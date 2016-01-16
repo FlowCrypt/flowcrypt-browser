cc7ba56b8c29e4b71e96fd993ecd8030396836e5
james <tom@holub.me>
2016-01-16 12:52:44 -0800

    allow cryptup only on one gmail account | #12 #9

--- src/js/inject_cryptup_into_gmail.js
+++ src/js/inject_cryptup_into_gmail.js
@@ -1,13 +1,20 @@
 'use strict';
 
 if (document.title.indexOf("Gmail") != -1 || document.title.indexOf("Mail") != -1) {
+  console.log(1);
+  var current_account = $("div.msg:contains('Loading '):contains('…')").text().replace('Loading ', '').replace('…', '');
+  console.log(current_account);;
+  chrome.storage.local.get(['primary_email'], function(storage){
+    if (typeof storage['primary_email'] === 'undefined'){
+      chrome.storage.local.set({primary_email: current_account}, inject_cryptup);
+    }
+    else if (storage['primary_email'] === current_account) {
+      inject_cryptup();
+    }
+  });
+}
 
-  // console.log(window.navigator.appVersion);
-  // console.log(window.navigator.platform);
-
-  var account = $("div.msg:contains('Loading '):contains('…')").text().replace('Loading ', '').replace('…', '');
-  chrome.storage.local.set({primary_email: account});
-
+function inject_cryptup() {
   $('body').append('<div class="cryptup_logo"></div>');
   $('body').append('<div class="T-I-KE T-I J-J5-Ji new_message_button">@</div>');
   $('body').append('<link rel="stylesheet" href="' + chrome.extension.getURL('css/gmail.css') + '" />');
@@ -17,12 +24,6 @@ if (document.title.indexOf("Gmail") != -1 || document.title.indexOf("Mail") != -
       $('body').append('<div class="new_message" id="new_message"><iframe scrolling="no" src="' + chrome.extension.getURL('chrome/gmail_elements/new_message.htm') + '"></iframe></div>');
     }
   });
-
-  // $('div.reply').click(function(){
-  //   if($('div.reply_message').length == 0) {
-  //     $('body').append('<div class="reply_message" id="reply_message"><iframe src="' + chrome.extension.getURL('elements/reply_message.htm') + '"></iframe></div>');
-  //   }
-  // });
 
   set_signal_listener('gmail_tab', {
     close_new_message: function(data, sender) {
@@ -35,5 +36,4 @@ if (document.title.indexOf("Gmail") != -1 || document.title.indexOf("Mail") != -
       $('iframe#' + sender).css(data);
     }
   });
-
 }
