e037f4919a3a469eb5a4d543ac2d0913a636724a
Tom James Holub <tom@holub.me>
2016-01-17 01:30:17 -0800

    connected to cryptup-keyserver | #7

--- src/js/pubkeys.js
+++ src/js/pubkeys.js
@@ -38,45 +38,27 @@ function pubkey_cache_get(email){
 }
 
 function get_pubkey(email, callback) { //add a callback here to do something when I have the info
-    var mit_db_url = 'https://pgp.mit.edu/pks/lookup';
-    email = email.trim();
-    search_email = fake_db_get_primary_email(email);
-    $.ajax({
-        url: mit_db_url,
-        data: {search: search_email, op: 'index', exact: 'on'},
-        type: 'GET',
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            callback(null);
-        },
-        success: function(text) {
-            var m = new RegExp(/pub[^<]+<\a href="([^"]+)">[A-Z0-9]+<\/\a>[^>]+>([^<]+)<\/\a>/g).exec(text);
-            if (m === null) {
-                callback(null);
-            } else {
-                search_query = m[1].split("search=");
-                name_email = m[2];
-                $.ajax({
-                    url: mit_db_url,
-                    data: {op: 'get', search: search_query[1]},
-                    type: 'GET',
-                    error: function(XMLHttpRequest, textStatus, errorThrown){
-                        console.log(mit_db_url + ' fetch resulted in 404 for ' + email);
-                        callback(null);
-                    },
-                    success: function(pubkey_text) {
-                        var pubkey_m = new RegExp(/-----BEGIN PGP PUBLIC KEY BLOCK-----[^-]+-----END PGP PUBLIC KEY BLOCK-----/g).exec(pubkey_text);
-                        if (pubkey_m !== null) {
-                            callback({'name': name_email, 'key': pubkey_m[0], 'email': email});
-                        } else {
-                            console.log([mit_db_url, {op: 'get', search: search_query[1]}])
-                            console.log(pubkey_text);
-                            console.log(pubkey_m);
-                            console.log('Found a match with corresponding email address for ' + email + ', but followup link did not contain a PGP public block');
-                            callback(null);
-                        }
-                    }
-                });
-            }
-        }
-    });
+  email = email.trim();
+  search_email = fake_db_get_primary_email(email);
+  $.ajax({
+    url: 'https://cryptup-keyserver.herokuapp.com/keys/find',
+    method: 'POST',
+    data: JSON.stringify({'email': search_email}),
+    dataType: 'json',
+    crossDomain: true,
+    contentType: 'application/json; charset=UTF-8',
+    async: true,
+    success: function(response) {
+      if(response.pubkey === null){
+        callback(null);
+      }
+      else{
+        callback({'name': null, 'key': response.pubkey, 'email': email});
+      }
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown){
+        console.log(['keyserver error', textStatus, errorThrown]);
+        callback(null);
+    },
+  });
 }
