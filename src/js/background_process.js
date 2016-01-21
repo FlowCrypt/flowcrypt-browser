601a2d4aca9b6f884d06237400457f7b436d440d
cryptup <tom@holub.me>
2016-01-20 23:51:23 -0800

    fixed gmail api 401 auth errors

--- src/js/background_process.js
+++ src/js/background_process.js
@@ -3,11 +3,12 @@ console.log('background_process.js starting');
 
 function gmail_auth(signal_data, gmail_auth_request_sender) {
   console.log('gmail_auth start');
-  var redirect_uri = 'https://nmelpmhpelannghfpkbmmpfggmildcmj.chromiumapp.org/google-auth-cb';
+  var registered_redirect_uri = chrome.identity.getRedirectURL('redirect');
+  console.log('regd: ' + registered_redirect_uri);
   var scope = 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send';
   var client_id = '717284730244-1ko46mlo9u0h9r16mlr6paine5u1qn7p.apps.googleusercontent.com'; //webapp
   var endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-  var url = endpoint + '?response_type=token&client_id=' + encodeURIComponent(client_id) + '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+  var url = endpoint + '?response_type=token&client_id=' + encodeURIComponent(client_id) + '&redirect_uri=' + encodeURIComponent(registered_redirect_uri) +
     '&scope=' + encodeURIComponent(scope) + '&login_hint=' + encodeURIComponent(signal_data.account);
   console.log('url:' + url);
   chrome.identity.launchWebAuthFlow({'url': url, 'interactive': true}, function(redirect_uri) {
