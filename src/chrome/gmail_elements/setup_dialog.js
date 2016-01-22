8c6700cdbb71e40383cde69c34c53141b2b1486b
Tom James Holub <tom@holub.me>
2016-01-22 17:42:37 +0800

    optional key submit on setup | close #18

--- src/chrome/gmail_elements/setup_dialog.js
+++ src/chrome/gmail_elements/setup_dialog.js
@@ -1,4 +1,23 @@
 
+function setup_dialog_set_done_and_close() {
+  chrome.storage.local.set({cryptup_setup_done: true}, function(){
+    send_signal('close_setup_dialog', 'setup_dialog', 'gmail_tab');
+  });
+}
+
+function setup_dialog_submit_pubkey(email, pubkey, callback) {
+  keyserver_keys_submit(email, pubkey, function(key_submitted, response){
+    if(key_submitted && response.saved === true) {
+      localStorage.master_public_key_submitted = true;
+    }
+    else{
+      //todo automatically resubmit later, make a notification if can't, etc
+      console.log('warning: pubkey not submitted');
+      console.log(respponse);
+    }
+    callback();
+  });
+}
 
 function create_save_submit_key_pair(email, email_name, passphrase){
   var user_id = email + ' <' + email_name + '>';
@@ -7,19 +26,7 @@ function create_save_submit_key_pair(email, email_name, passphrase){
     localStorage.master_public_key = keypair.publicKeyArmored;
     localStorage.master_public_key_submitted = false;
     localStorage.master_passphrase = '';
-    keyserver_keys_submit(email, keypair.publicKeyArmored, function(key_submitted, response){
-      if(key_submitted && response.saved === true) {
-        localStorage.master_public_key_submitted = true;
-      }
-      else{
-        //todo automatically resubmit later, make a notification if can't, etc
-        console.log('warning: pubkey not submitted')
-        console.log(respponse);
-      }
-      chrome.storage.local.set({cryptup_setup_done: true}, function(){
-        send_signal('close_setup_dialog', 'setup_dialog', 'gmail_tab');
-      });
-    });
+    setup_dialog_submit_pubkey(email, localStorage.master_public_key, setup_dialog_set_done_and_close);
   }).catch(function(error) {
     $('#step_2_easy_generating').html('Error, thnaks for discovering it!<br/><br/>This is an early development version.<br/><br/>Please press CTRL+SHIFT+J, click on CONSOLE.<br/><br/>Copy messages printed in red and send them to me.<br/><br/>tom@cryptup.org - thanks!');
     console.log('--- copy message below for debugging  ---')
@@ -49,7 +56,13 @@ $('div#btn_save_private').click(function(){
   localStorage.master_private_key = $('#input_private_key').val();
   localStorage.master_public_key = openpgp.key.readArmored($('#input_private_key').val()).keys[0].toPublic().armor();
   localStorage.master_passphrase = $('#input_passphrase').val();
-  chrome.storage.local.set({cryptup_setup_done: true}, function(){
-    send_signal('close_setup_dialog', 'setup_dialog', 'gmail_tab');
-  });
+  if($('#input_submit_key').prop('checked')) {
+    chrome.storage.local.get(['primary_email', 'primary_email_name'], function(storage) {
+      $('div#btn_save_private').html('<i class="fa fa-spinner fa-pulse"></i>');
+      setup_dialog_submit_pubkey(storage['primary_email'], localStorage.master_public_key, setup_dialog_set_done_and_close);
+    });
+  }
+  else {
+    setup_dialog_set_done_and_close();
+  }
 });
