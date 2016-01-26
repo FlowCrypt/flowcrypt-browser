48eb12f9022c55d4a21f8e9e485cad3d054fcf12
Tom James Holub <tom@holub.me>
2016-01-26 11:01:19 +0800

    exporting prv/pub keys | #16

--- src/chrome/plugin_button_popup.js
+++ src/chrome/plugin_button_popup.js
@@ -2,4 +2,17 @@
 
 $('#btn_flush_pubkey_cache').click(function(){
   localStorage.pubkey_cache = JSON.stringify({});
+  $('pre#key_container').text('Pubkey cache flushed.');
+});
+
+$('#btn_show_private_key').click(function(){
+  $('pre#key_container').text(localStorage.master_private_key);
+});
+
+$('#btn_show_public_key').click(function(){
+  $('pre#key_container').text(localStorage.master_public_key);
+});
+
+$('#btn_back_up_private_key').click(function(){
+
 });
