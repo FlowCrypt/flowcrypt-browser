41f6e939edc516c67c8ef4a639d2e27dc86c3e85
Tom James Holub <tom@holub.me>
2016-04-05 23:40:33 +0800

    fixed broken logo | close #111

--- src/js/content_scripts/gmail_tab/elements/inject.js
+++ src/js/content_scripts/gmail_tab/elements/inject.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var logo = 'iVBORw0KGgoAAAANSUhEUgAAABMAAAAOCAYAAADNGCeJAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4AMdAREakDr07QAAAFFJREFUOMtjVOpWYqAWYGFgYGC4W3L3PwMDA4NyjzIjTAKfGDag3KPMyMRARcBCjiZcrqWqywbem7giYnBFAM1cRjtv4kvhhCKD6jmAkZoZHQBF3hzwjZcuRAAAAABJRU5ErkJggg==';
+
 function inject_meta() {
   $('body').append('<link rel="stylesheet" href="' + chrome.extension.getURL('css/gmail.css') + '" />');
   $('body').append('<link rel="stylesheet" href="' + chrome.extension.getURL('css/font-awesome.min.css') + '" />');
@@ -7,7 +9,7 @@ function inject_meta() {
 }
 
 function inject_buttons(account_email, tab_id) {
-  $('body').append('<div class="T-I-KE T-I J-J5-Ji new_message_button"><img src="chrome-extension://nmelpmhpelannghfpkbmmpfggmildcmj/img/logo-19-14-white.png" /></div>');
+  $('body').append('<div class="T-I-KE T-I J-J5-Ji new_message_button"><img src="data:image/png;base64,' + logo + '" /></div>');
   $('div.new_message_button').click(function() {
     if($('div.new_message').length == 0) {
       var url = chrome.extension.getURL('chrome/gmail_elements/new_message.htm') +
