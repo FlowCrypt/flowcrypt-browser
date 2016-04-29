0fed270dc732fa9d6309fdf9b6db09bfb88747ad
Tom James Holub <tom@holub.me>
2016-04-28 18:14:19 -0700

    pubkey in-msg block add/update button renaming | close #127

--- src/chrome/gmail_elements/pgp_pubkey.js
+++ src/chrome/gmail_elements/pgp_pubkey.js
@@ -4,10 +4,13 @@ var url_params = get_url_params(['account_email', 'armored_pubkey', 'parent_tab_
 
 var pubkey = openpgp.key.readArmored(url_params.armored_pubkey).keys[0];
 
+var cache = pubkey_cache_retrieve();
+
 $('.pubkey').text(url_params.armored_pubkey);
 
 if(typeof pubkey !== 'undefined') {
   $('.input_email').val(trim_lower(pubkey.users[0].userId.userid));
+  set_button_text();
 } else {
   $('.add_pubkey').replaceWith('<div style="color: red;">This public key is invalid or has unknown format.</div>');
   send_resize_message();
@@ -32,5 +35,15 @@ function send_resize_message() {
     }
   });
 }
+
+function set_button_text() {
+  if(Object.keys(cache).indexOf($('.input_email').val()) === -1) {
+    $('.add_pubkey').text('add to contacts');
+  } else {
+    $('.add_pubkey').text('update contact');
+  }
+}
+
+$('.input_email').keyup(set_button_text);
 
 send_resize_message();
