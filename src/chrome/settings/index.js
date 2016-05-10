96e246488d54619a711272932bc5cf7355d54308
Tom James Holub <tom@holub.me>
2016-05-09 23:34:03 -0700

    added a note to setup index about gmail permissions | #62

--- src/chrome/settings/index.js
+++ src/chrome/settings/index.js
@@ -7,10 +7,12 @@ function refresh_account_list() {
   get_account_emails(function(account_emails) {
     account_storage_get(account_emails, ['setup_done'], function(account_storages) {
       var accounts_content = '';
+      var has_connected_account = false;
       $.each(account_emails, function(i, account_email) {
         var email_text = '<b>' + account_email.split('@')[0] + '</b>' + '@' + account_email.split('@')[1];
         if(account_storages[account_email]['setup_done'] === true) {
           accounts_content += '<div class="line"><a class="button green has_email" href="account.htm?account_email=' + encodeURIComponent(account_email) + '">' + email_text + '</a></div>';
+          var has_connected_account = true;
         } else {
           if(spinning !== account_email) {
             accounts_content += '<div class="line"><a class="button red action_auth has_email" href="#">' + email_text + '</a></div>';
@@ -19,12 +21,16 @@ function refresh_account_list() {
           }
         }
       });
+      if(!has_connected_account) {
+        $('#notes').css('display', 'block');
+      }
       if(accounts_content) {
         $('h1').text('Select Gmail Account');
         $('#accounts').html(accounts_content + '<div class="line"><a href="#" class="block action_auth">Add another account</a></div>');
       } else {
         $('h1').text('Set up CryptUP');
         $('#accounts').html('<div class="line"><a href="#" class="button long green action_auth">Connect to Gmail</a></div>');
+
       }
       $('a.action_auth').click(function() {
         if(/@/.test($(this).text())) {
