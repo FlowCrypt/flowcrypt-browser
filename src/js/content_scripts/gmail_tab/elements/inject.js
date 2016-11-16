0d6bf75366a47349acecccce781f0e64e05a0aee
Tom James Holub <tom@holub.me>
2016-11-16 13:06:21 -0800

    new secure compose button | close #202 close #204

--- src/js/content_scripts/gmail_tab/elements/inject.js
+++ src/js/content_scripts/gmail_tab/elements/inject.js
@@ -7,10 +7,14 @@ function inject_meta() {
 }
 
 function inject_buttons(account_email, tab_id) {
-  $('body').append('<div class="T-I-KE T-I J-J5-Ji new_message_button"><img src="' + get_logo_src(true) + '" /></div>');
-  $('div.new_message_button').click(function() {
-    if($('div.new_message').length == 0) {
-      $('body').append(compose_message_iframe(account_email, tab_id));
-    }
-  });
+  if($('div.aic').length === 0) { // don't inject too early
+    setInterval(inject_buttons, 100);
+  } else {
+    $('div.aic').prepend('<div class="z0"><div class="T-I J-J5-Ji T-I-KE L3 new_message_button" role="button" tabindex="0" style="user-select: none;background: #31A217;" gh="cm">SECURE COMPOSE</div></div>')
+    $('div.new_message_button').click(function() {
+      if($('div.new_message').length == 0) {
+        $('body').append(compose_message_iframe(account_email, tab_id));
+      }
+    });
+  }
 }
