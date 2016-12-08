d4758d2791a358da1ab663df8514d8c6273cb3ba
Tom James Holub <tom@holub.me>
2016-12-07 16:25:06 -0800

    allowing partial auth (dev) | #211

--- src/chrome/settings/modules/auth_denied.js
+++ src/chrome/settings/modules/auth_denied.js
@@ -3,8 +3,12 @@
 var url_params = get_url_params(['parent_tab_id']);
 
 $('.action_auth_proceed').click(function() {
+  chrome_message_send(url_params.parent_tab_id, 'open_google_auth_dialog');
+});
+
+$('.auth_action_limited').click(function() {
   chrome_message_send(url_params.parent_tab_id, 'open_google_auth_dialog', {
-    scope: undefined, //todo: make some permissions optional
+    omit_read_scope: true,
   });
 });
 
