e13eca8793eb262c12bbd268d3b46d1e78ba7d02
Tom James Holub <tom@holub.me>
2016-12-11 08:37:45 -0800

    preventing multiple secure compose buttons | close #229

--- src/js/content_scripts/gmail_tab/elements/inject.js
+++ src/js/content_scripts/gmail_tab/elements/inject.js
@@ -10,11 +10,13 @@ function inject_buttons(account_email, tab_id) {
   if($('div.aic').length === 0) { // don't inject too early
     setTimeout(function() {
       inject_buttons(account_email, tab_id);
-    }, 100);
+    }, 300);
   } else {
-    $('div.aic').prepend('<div class="z0"><div class="T-I J-J5-Ji T-I-KE L3 new_message_button" role="button" tabindex="0" style="user-select: none;background: #31A217;" gh="cm">SECURE COMPOSE</div></div>')
-    $('div.new_message_button').click(function() {
-      open_new_message(account_email, tab_id);
-    });
+    if($('div.new_message_button').length === 0) {
+      $('div.aic').prepend('<div class="z0"><div class="T-I J-J5-Ji T-I-KE L3 new_message_button" role="button" tabindex="0" style="user-select: none;background: #31A217;" gh="cm">SECURE COMPOSE</div></div>')
+      $('div.new_message_button').click(function() {
+        open_new_message(account_email, tab_id);
+      });
+    }
   }
 }
