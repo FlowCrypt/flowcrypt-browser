3bd743f09e1e6fdfb4eb9d84addb1c9444f3e687
Tom James Holub <tom@holub.me>
2016-12-27 20:52:50 -0800

    modules/add_key.htm dialog added | #184

---
+++ src/chrome/settings/modules/add_key.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var url_params = get_url_params(['account_email', 'parent_tab_id']);
+
+var private_keys = private_keys_get(url_params.account_email);
+var private_keys_long_ids = [];
+$.each(private_keys, function(i, keyinfo) {
+  private_keys_long_ids.push(key_longid(keyinfo.armored));
+});
+
+$('.action_add_private_key').click(prevent(doubleclick(), function() {
+  var new_key = openpgp.key.readArmored($('#step_2b_manual_enter .input_private_key').val()).keys[0];
+  if(typeof new_key === 'undefined') {
+    alert('Private key is not properly formatted. Please insert complete key, including "-----BEGIN PGP PRIVATE KEY BLOCK-----" and "-----END PGP PRIVATE KEY BLOCK-----"');
+  } else if(new_key.isPublic()) {
+    alert('This was a public key. Please insert a private key instead. It\'s a block of text starting with "-----BEGIN PGP PRIVATE KEY BLOCK-----"');
+  } else if(!key_longid(new_key)) {
+    alert('This key may not be compatible. Please write me at tom@cryptup.org and let me know which software created this key, so that I can fix it.\n\n(error: cannot get long_id)');
+  } else if(private_keys_long_ids.indexOf(key_longid(new_key)) !== -1) {
+    alert('This is one of your current keys.');
+  } else if(new_key.decrypt($('.input_passphrase').val()) === false) {
+    alert('The pass phrase does not match. Please try a different pass phrase.');
+  } else {
+    private_keys_add(url_params.account_email, $('#step_2b_manual_enter .input_private_key').val());
+    if($('#step_2b_manual_enter .input_passphrase_save').prop('checked')) {
+      save_passphrase('local', url_params.account_email, key_longid(new_key), $('.input_passphrase').val());
+    } else {
+      save_passphrase('session', url_params.account_email, key_longid(new_key), $('.input_passphrase').val());
+    }
+  }
+  chrome_message_send(url_params.parent_tab_id, 'reload', {
+    advanced: true,
+  });
+}));
