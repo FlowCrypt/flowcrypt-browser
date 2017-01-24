e8c4c00a767f19e11485559503597150a911c1bd
Tom James Holub <tom@holub.me>
2017-01-24 08:41:38 -0800

    easier access to public key + clearer instructions

--- src/chrome/settings/modules/my_key.js
+++ src/chrome/settings/modules/my_key.js
@@ -3,23 +3,39 @@
 'use strict';
 
 var url_params = get_url_params(['account_email', 'longid']);
+var keyinfo = undefined;
 
-var keyinfo = private_keys_get(url_params.account_email, url_params.longid);
+if(url_params.longid) {
+  keyinfo = private_keys_get(url_params.account_email, url_params.longid);
+} else {
+  $.each(private_keys_get(url_params.account_email), function(i, k) {
+    if(k.primary) {
+      keyinfo = k;
+    }
+  });
+}
 
 var key = openpgp.key.readArmored(keyinfo.armored).keys[0];
 
+$('.email').text(url_params.account_email);
 $('.key_dump').text(key.toPublic().armor());
 $('.key_fingerprint').text(key_fingerprint(key, 'spaced'));
 $('.key_words').text(mnemonic(keyinfo.longid));
+$('.show_when_showing_public').css('display', 'block');
+$('.show_when_showing_private').css('display', 'none');
 
 $('.action_show_other_type').click(function() {
   if($('.action_show_other_type').text().toLowerCase() === 'show private') {
     $('.key_dump').text(key.armor()).removeClass('good').addClass('bad');
     $('.action_show_other_type').text('show public').removeClass('bad').addClass('good');
-    $('.key_type').text('Master Private Key')
+    $('.key_type').text('Master Private Key');
+    $('.show_when_showing_public').css('display', 'none');
+    $('.show_when_showing_private').css('display', 'block');
   } else {
     $('.key_dump').text(key.toPublic().armor()).removeClass('bad').addClass('good');
     $('.action_show_other_type').text('show private').removeClass('good').addClass('bad');
-    $('.key_type').text('Master Public Key')
+    $('.key_type').text('Master Public Key Info');
+    $('.show_when_showing_public').css('display', 'block');
+    $('.show_when_showing_private').css('display', 'none');
   }
 });
