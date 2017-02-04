615800d1b030194d525131658bb0093c4140e5c7
Tom James Holub <tom@holub.me>
2017-02-04 12:31:46 -0800

    experimental: simulate code error | close #439

--- src/chrome/settings/modules/experimental.js
+++ src/chrome/settings/modules/experimental.js
@@ -7,3 +7,7 @@ var url_params = tool.env.url_params(['account_email', 'parent_tab_id']);
 $('.action_open_decrypt').click(function () {
   show_settings_page('/chrome/settings/modules/decrypt.htm');
 });
+
+$('.action_exception').click(function() {
+  purposefully_generated_exception();
+});
