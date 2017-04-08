deaf3895360c2a00ec8d0173666a086d4f86969e
Tom James Holub <tom@holub.me>
2017-04-07 19:25:42 -0700

    added comment to mnemonic | #689

--- firefox/js/common/mnemonic.js
+++ firefox/js/common/mnemonic.js
@@ -2070,5 +2070,5 @@ function mnemonic(hex) { // 4E1F799AA4FF2279
   var binary = hex.split('').map(function (h) { return leading_zeroes(parseInt(h, 16).toString(2), 4); }).join(''); // 0100111000011111011110011001101010100100111111110010001001111001
   var binary_chunks = string_chunks(binary, 11); // ["01001110000", "11111011110", "01100110101", "01001001111", "11110010001", "001111001"]
   var integers = binary_chunks.map(function (b) { return parseInt(b, 2) }); // [624, 2014, 821, 591, 1937, 121]
-  return integers.map(function (i) { return words[i].toUpperCase(); }).join(' '); // "word word word ..."
+  return integers.map(function (i) { return words[i].toUpperCase(); }).join(' '); // "EVOKE WINK GRIT ENEMY VENDOR AUGUST"
 }
