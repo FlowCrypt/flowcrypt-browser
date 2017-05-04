275704d8638f83834bdfe8231452381a1f9a223b
Tom James Holub <tom@holub.me>
2017-05-03 20:48:32 -0700

    fix google auth sign up loop | close  #757

--- chrome/js/content_scripts/oauth_window/google.js
+++ chrome/js/content_scripts/oauth_window/google.js
@@ -8,14 +8,20 @@ function api_google_auth_state_unpack(status_string) {
   return JSON.parse(status_string.replace(google_oauth2.state_header, '', 1));
 }
 
-if(tool.value(google_oauth2.state_header).in(document.title)) { // this is cryptup's google oauth - based on a &state= passed on in auth request
-  var parts = document.title.split(' ', 2);
-  var result = parts[0];
-  var params = tool.env.url_params(['code', 'state', 'error'], parts[1]);
-  var state_object = api_google_auth_state_unpack(params.state);
-  tool.browser.message.send('broadcast', 'google_auth_window_result', { result: result, params: params, state: state_object }, function () {
-    var close_title = 'Close this window';
-    $('title').text(close_title);
-    tool.browser.message.send(null, 'close_popup', {title: close_title});
-  });
-}
+var interval = setInterval(function () {
+  if(!document.title) {
+    return;
+  }
+  clearInterval(interval);
+  if(tool.value(google_oauth2.state_header).in(document.title)) { // this is cryptup's google oauth - based on a &state= passed on in auth request
+    var parts = document.title.split(' ', 2);
+    var result = parts[0];
+    var params = tool.env.url_params(['code', 'state', 'error'], parts[1]);
+    var state_object = api_google_auth_state_unpack(params.state);
+    tool.browser.message.send('broadcast', 'google_auth_window_result', { result: result, params: params, state: state_object }, function () {
+      var close_title = 'Close this window';
+      $('title').text(close_title);
+      tool.browser.message.send(null, 'close_popup', {title: close_title});
+    });
+  }
+}, 50);
