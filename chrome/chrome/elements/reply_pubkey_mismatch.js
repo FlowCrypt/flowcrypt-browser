267c3a90d9e49f5a167a28b4a6201b59ac81e89d
Tom James Holub <tom@holub.me>
2017-06-29 18:55:45 -0700

    fixing reply pubkey mismatch | #853

--- chrome/chrome/elements/reply_pubkey_mismatch.js
+++ chrome/chrome/elements/reply_pubkey_mismatch.js
@@ -2,11 +2,12 @@
 
 'use strict';
 
-var url_params = tool.env.url_params(['account_email', 'from', 'to', 'subject', 'frame_id', 'thread_id', 'thread_message_id', 'parent_tab_id', 'skip_click_prompt', 'ignore_draft']);
-var keyinfo = private_keys_get(url_params.account_email, 'primary');
-var attachment = tool.file.keyinfo_as_pubkey_attachment(keyinfo);
-var compose = init_shared_compose_js(url_params, null, {}, null, true);
-var additional_message_headers;
+const url_params = tool.env.url_params(['account_email', 'from', 'to', 'subject', 'frame_id', 'thread_id', 'thread_message_id', 'parent_tab_id', 'skip_click_prompt', 'ignore_draft']);
+const keyinfo = private_keys_get(url_params.account_email, 'primary');
+const attachment = tool.file.keyinfo_as_pubkey_attachment(keyinfo);
+let additional_message_headers;
+
+window.flowcrypt_compose.init({send_message_to_main_window: (channel, data) => tool.browser.message.send(url_params.parent_tab_id, channel, data)}, {is_reply_box: true, frame_id: url_params.frame_id});
 
 tool.each(url_params.to.split(','), function(i, to) {
   $('.recipients').append(tool.e('span', {text: to}));
@@ -14,15 +15,15 @@ tool.each(url_params.to.split(','), function(i, to) {
 
 // render
 $('.pubkey_file_name').text(attachment.name);
-compose.resize_reply_box();
+window.flowcrypt_compose.resize_reply_box();
 tool.browser.message.send(url_params.parent_tab_id, 'scroll', {selector: '.reply_message_iframe_container', repeat: [500]});
 $('#input_text').focus();
 
 // determine reply headers
 tool.api.gmail.thread_get(url_params.account_email, url_params.thread_id, 'full', function (success, thread) {
   if (success && thread.messages && thread.messages.length > 0) {
-    var thread_message_id_last = tool.api.gmail.find_header(thread.messages[thread.messages.length - 1], 'Message-ID') || '';
-    var thread_message_referrences_last = tool.api.gmail.find_header(thread.messages[thread.messages.length - 1], 'In-Reply-To') || '';
+    let thread_message_id_last = tool.api.gmail.find_header(thread.messages[thread.messages.length - 1], 'Message-ID') || '';
+    let thread_message_referrences_last = tool.api.gmail.find_header(thread.messages[thread.messages.length - 1], 'In-Reply-To') || '';
     additional_message_headers = { 'In-Reply-To': thread_message_id_last, 'References': thread_message_referrences_last + ' ' + thread_message_id_last };
   }
 });
@@ -30,18 +31,17 @@ tool.api.gmail.thread_get(url_params.account_email, url_params.thread_id, 'full'
 // send
 $('#send_btn').click(tool.ui.event.prevent(tool.ui.event.double(), function (self) {
   $('#send_btn').text('sending..');
-  var message = tool.api.common.message(url_params.account_email, url_params.from, url_params.to, url_params.subject, $('#input_text').get(0).innerText, [attachment], url_params.thread_id);
+  let message = tool.api.common.message(url_params.account_email, url_params.from, url_params.to, url_params.subject, $('#input_text').get(0).innerText, [attachment], url_params.thread_id);
   tool.each(additional_message_headers, function (k, h) {
     message.headers[k] = h;
   });
   tool.api.gmail.message_send(url_params.account_email, message, function (success, response) {
     if(success) {
       tool.browser.message.send(url_params.parent_tab_id, 'notification_show', { notification: 'Message sent.' });
-      $('#reply_message_table_container').text('Message sent. The other person should use this information to send a new message.');
+      $('#compose').replaceWith('Message sent. The other person should use this information to send a new message.');
     } else {
       $('#send_btn').text('send response');
       alert('There was an error sending message, please try again');
     }
   });
 }));
-
