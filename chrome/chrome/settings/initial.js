4560ef8ca608dcff661071c04239a1b0affb2168
Tom James Holub <tom@holub.me>
2017-10-13 18:16:18 -0700

    FlowCrypt name notification | #922

--- chrome/chrome/settings/initial.js
+++ chrome/chrome/settings/initial.js
@@ -8,3 +8,6 @@ localStorage.resolved_naked_key_vulnerability = true;
 // plugins first installed as version 4.3.1 and up: will not use master_public_key and master_private_key in local storage
 // instead, all keys in each account in one array
 localStorage.uses_account_keys_array = true;
+
+// let people know that name was changed to FlowCrypt only if they installed before v 5.0.0
+window.flowcrypt_storage.set(null, { namechange_flowcrypt_notified: true });
