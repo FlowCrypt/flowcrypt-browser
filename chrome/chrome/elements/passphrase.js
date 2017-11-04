42b57abb7d6e92196c58e8c286baf580ea61bbb7
Tom James Holub <tom@holub.me>
2017-11-03 21:54:52 -0700

    fix signing w/ session pass phrase | #934

--- chrome/chrome/elements/passphrase.js
+++ chrome/chrome/elements/passphrase.js
@@ -19,17 +19,19 @@ if(url_params.type === 'embedded') {
 tool.ui.passphrase_toggle(['passphrase']);
 
 window.flowcrypt_storage.keys_get(url_params.account_email).then(all_private_keys => {
-
-  let private_keys = url_params.longids ? all_private_keys.filter(ki => tool.value(ki.longid).in(url_params.longids.split(','))) : all_private_keys;
-
+  let selected_private_keys = all_private_keys;
+  if(url_params.longids) {
+    let longids = url_params.longids.split(',');
+    selected_private_keys = all_private_keys.filter(ki => tool.value(ki.longid).in(longids) || (ki.primary && tool.value('primary').in(longids)));
+  }
   if(all_private_keys.length > 1) {
     let html;
-    if(private_keys.length === 1) {
-      html = 'For the following key: <span class="good">' + mnemonic(private_keys[0].longid) + '</span> (KeyWords)';
+    if(selected_private_keys.length === 1) {
+      html = 'For the following key: <span class="good">' + mnemonic(selected_private_keys[0].longid) + '</span> (KeyWords)';
     } else {
       html = 'Pass phrase needed for any of the following keys:';
-      tool.each(private_keys, function (i, keyinfo) {
-        html += 'KeyWords ' + String(i + 1) + ': <div class="good">' + mnemonic(private_keys[i].longid) + '</div>';
+      tool.each(selected_private_keys, function (i, keyinfo) {
+        html += 'KeyWords ' + String(i + 1) + ': <div class="good">' + mnemonic(selected_private_keys[i].longid) + '</div>';
       });
     }
     $('.which_key').html(html);
@@ -57,7 +59,7 @@ window.flowcrypt_storage.keys_get(url_params.account_email).then(all_private_key
   $('.action_ok').click(tool.ui.event.prevent(tool.ui.event.double(), function () {
     let pass = $('#passphrase').val();
     let is_correct = false;
-    tool.each(private_keys, function (i, keyinfo) { // if passphrase matches more keys, it will save them all
+    tool.each(selected_private_keys, function (i, keyinfo) { // if passphrase matches more keys, it will save them all
       let prv = openpgp.key.readArmored(keyinfo.private).keys[0];
       if(tool.crypto.key.decrypt(prv, pass).success) {
         is_correct = true;
