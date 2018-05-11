7ab6b20b3f9a6592a4410f074fcd1c5808910605
Tom James Holub <tom@holub.me>
2018-05-12 04:26:46 +0800

    stripe.ts | #1081

--- chrome/js/content_scripts/checkout/stripe.ts
+++ chrome/js/content_scripts/checkout/stripe.ts
@@ -2,9 +2,8 @@
 
 'use strict';
 
-var url_params = tool.env.url_params(['parent_tab_id']);
+let url_params = tool.env.url_params(['parent_tab_id']);
 
 document.addEventListener('cryptup_stripe_result', catcher.try(() => {
-  tool.browser.message.send(url_params.parent_tab_id, 'stripe_result', { token: $('#stripe_result').text() });
+  tool.browser.message.send(url_params.parent_tab_id as string, 'stripe_result', { token: $('#stripe_result').text() });
 }));
-
