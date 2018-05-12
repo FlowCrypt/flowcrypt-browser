7267db1975795a6d0c68c599bf0bf2da90b0c991
Tom James Holub <tom@holub.me>
2018-05-12 17:17:01 +0800

    pupups + fixed bug that may cause several settings windows | #1081

---
+++ chrome/chrome/popups/select_account.ts
@@ -0,0 +1,34 @@
+/* © 2016-2018 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com */
+
+'use strict';
+
+tool.catch.try(() => {
+
+  let url_params = tool.env.url_params(['action']);
+
+  let page: string|null = null;
+  if(url_params.action === 'new_message') {
+    $('#title').text('Choose account for new message');
+    page = '/chrome/elements/compose.htm';
+  } else if(url_params.action === 'settings') {
+    $('#title').text('Select account to open settings')
+  } else {
+    throw new Error('unknown action: ' + url_params.action);
+  }
+  
+  (window as FlowCryptWindow).flowcrypt_storage.account_emails_get((account_emails) => {
+    (window as FlowCryptWindow).flowcrypt_storage.get(account_emails, ['setup_done'], (account_storages) => {
+      let ul_emails = '';
+      tool.each(account_storages, (email: string, storage) => {
+        if(storage.setup_done === true) {
+          ul_emails += `<li><a class="button gray2 long" href="#" email="${tool.str.html_escape(email)}">${tool.str.html_escape(email)}</a></li>`;
+        }
+      });
+      $('ul.emails').html(ul_emails).find('a').click(function () {
+        tool.browser.message.send(null, 'settings', { account_email: $(this).attr('email'), page: page }, () => window.close());
+      });
+      $('html, body').css('height', $('.content').height()! + (tool.env.browser().name === 'firefox' ? 40 : 0)); // .content is in template
+    });
+  });  
+
+})();
