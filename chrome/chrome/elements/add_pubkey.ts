921a02bb109b4104f8785efa53fb79c09e8d6973
Tom James Holub <tom@holub.me>
2018-05-17 00:14:14 +0800

    add_pubkey refac | #1081

--- chrome/chrome/elements/add_pubkey.ts
+++ chrome/chrome/elements/add_pubkey.ts
@@ -2,7 +2,7 @@
 
 'use strict';
 
-tool.catch.try(() => {
+tool.catch.try(async () => {
 
   tool.ui.event.protect();
 
@@ -12,42 +12,40 @@ tool.catch.try(() => {
     $('select.email').append('<option value="' + email + '">' + email + '</option>');
   }
   
-  Store.db_contact_search(null, { has_pgp: true }).then(contacts => {
+  let contacts = await Store.db_contact_search(null, {has_pgp: true});
   
-    $('select.copy_from_email').append('<option value=""></option>');
-    for(let contact of contacts) {
-      $('select.copy_from_email').append('<option value="' + contact.email + '">' + contact.email + '</option>');
+  $('select.copy_from_email').append('<option value=""></option>');
+  for(let contact of contacts) {
+    $('select.copy_from_email').append('<option value="' + contact.email + '">' + contact.email + '</option>');
+  }
+
+  $('select.copy_from_email').change(async function () {
+    if($(this).val()) {
+      let [contact] = await Store.db_contact_get(null, [$(this).val() as string]);
+      if(contact && contact.pubkey) {
+        $('.pubkey').val(contact.pubkey).prop('disabled', true);
+      } else {
+        alert('Contact not found.');
+      }
+    } else {
+      $('.pubkey').val('').prop('disabled', false);
     }
-  
-    $('select.copy_from_email').change(async function () {
-      if($(this).val()) {
-        let [contact] = await Store.db_contact_get(null, [$(this).val() as string]);
-        if(contact && contact.pubkey) {
-          $('.pubkey').val(contact.pubkey).prop('disabled', true);
-        } else {
-          alert('Contact not found.');
-        }
-      } else {
-        $('.pubkey').val('').prop('disabled', false);
-      }
-    });
-  
-    $('.action_ok').click(tool.ui.event.prevent(tool.ui.event.double(), async () => {
-      let armored = tool.crypto.key.normalize(tool.crypto.armor.strip($('.pubkey').val() as string)); // .pubkey is a textarea
-      if(!armored || !tool.crypto.key.fingerprint(armored)) {
-        alert('Could not recognize the format, please try again.');
-        $('.pubkey').val('').focus();
-      } else if (!tool.crypto.key.usable(armored)) {
-        alert('This public key looks correctly formatted, but cannot be used for encryption. Please write me at human@flowcrypt.com so that I can see if there is a way to fix it.');
-        $('.pubkey').val('').focus();
-      } else {
-        await Store.db_contact_save(null, Store.db_contact_object($('select.email').val() as string, null, 'pgp', armored, null, false, Date.now()));
-        close_dialog();
-      }
-    }));
-  
   });
-  
+
+  $('.action_ok').click(tool.ui.event.prevent(tool.ui.event.double(), async () => {
+    let armored = tool.crypto.key.normalize(tool.crypto.armor.strip($('.pubkey').val() as string)); // .pubkey is a textarea
+    if(!armored || !tool.crypto.key.fingerprint(armored)) {
+      alert('Could not recognize the format, please try again.');
+      $('.pubkey').val('').focus();
+    } else if (!tool.crypto.key.usable(armored)) {
+      alert('This public key looks correctly formatted, but cannot be used for encryption. Please write me at human@flowcrypt.com so that I can see if there is a way to fix it.');
+      $('.pubkey').val('').focus();
+    } else {
+      await Store.db_contact_save(null, Store.db_contact_object($('select.email').val() as string, null, 'pgp', armored, null, false, Date.now()));
+      close_dialog();
+    }
+  }));
+
   if(url_params.placement !== 'settings') {
     $('.action_settings').click(tool.ui.event.prevent(tool.ui.event.double(), function () {
       tool.browser.message.send(null, 'settings', {
@@ -69,6 +67,6 @@ tool.catch.try(() => {
       window.close();
     }
   
-  }  
+  }
 
 })();
