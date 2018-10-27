fb7966dec386d263c5fd97280111c21ccc56b029
Tom J <tom@holub.me>
2018-10-27 11:04:37 +0000

    automated xss comment check - should fail | #1293

---
+++ test/source/patterns.ts
@@ -0,0 +1,57 @@
+
+import {readdirSync, statSync, readFileSync, symlinkSync} from 'fs';
+import * as path from 'path';
+
+let errors_found = 0;
+
+const get_all_files_in_directory = (dir: string, file_pattern: RegExp): string[] => {
+  let all: string[] = [];
+  const files_in_dir = readdirSync(dir);
+  for (const file_in_dir of files_in_dir) {
+    const file_path = path.join(dir, file_in_dir);
+    const stat = statSync(file_path);
+    if (stat.isDirectory()) {
+      all = all.concat(get_all_files_in_directory(file_path, file_pattern));
+    } else if(file_pattern.test(file_path)) {
+      all.push(file_path);
+    }
+  }
+  return all;
+};
+
+const has_xss_comment = (line: string) => {
+  return /\/\/ xss-(known-source|direct|escaped|safe-factory|safe-value|sanitized|none|reinsert|dangerous-function)/.test(line);
+};
+
+const validate_line = (line: string, location: string) => {
+
+  if(line.match(/\.(innerHTML|outerHTML) ?= ?/) && !has_xss_comment(line)) {
+    console.error(`unchecked xss in ${location}:\n${line}\n`);
+    errors_found++;
+  }
+
+  if(line.match(/\.(html|append|prepend|replaceWith)\([^)]/) && !has_xss_comment(line)) {
+    console.error(`unchecked xss in ${location}:\n${line}\n`);
+    errors_found++;
+  }
+
+  if(line.match(/DANGEROUS/i) && !has_xss_comment(line)) {
+    console.error(`unchecked xss in ${location}:\n${line}\n`);
+    errors_found++;
+  }
+
+};
+
+const source_file_paths = get_all_files_in_directory('./chrome', /\.ts$/);
+
+for(const source_file_path of source_file_paths) {
+  const lines = readFileSync(source_file_path).toString().split('\n');
+  for(let line_i = 0; line_i < lines.length; line_i++) {
+    validate_line(lines[line_i], `${source_file_path}:${line_i + 1}`);
+  }
+}
+
+if(errors_found) {
+  console.error(`patterns.ts: Found ${errors_found} unhandled xss patterns, exiting\n`);
+  process.exit(1);
+}
