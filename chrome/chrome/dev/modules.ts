1eadab318d71dcc4f498f488c5c2738e27b582aa
Tom J <tom@holub.me>
2018-10-31 17:02:33 +0000

    extended module tests | #1306

--- chrome/chrome/dev/modules.ts
+++ chrome/chrome/dev/modules.ts
@@ -3,14 +3,37 @@
 import {return_this_worked} from './test_module_for_import.js';
 import * as DOMPurify from 'dompurify';
 
-$('pre').text('hello from jquery');
+// results:
+//
+// general code:    works
+//
+// background:      will work if defined as "page" instead of script, then use type=module in script tags (not tested)
+//
+// content scripts: will have to be compiled as a single content script file with --out=file and module:?? and target: es5 (should work)
+//                  this means that there would be tsconfig.extension.json and tsconfig.content.json as per https://github.com/Microsoft/TypeScript/issues/3645
 
 try {
   $('pre').text(`
-    OpenPGP hash: ${openpgp.crypto.hash.digest(openpgp.enums.hash.sha256, 'something to hash')}
-    return_this_worked: ${return_this_worked()}
-    Purify result: ${DOMPurify.sanitize('<div>something sanitized <script>inside script</script></div>')}
-    anchorme: ${anchorme('hello text https://flowcrypt.com yay', {})}
+OpenPGP hash:
+${openpgp.crypto.hash.digest(openpgp.enums.hash.sha256, 'something to hash')}
+
+return_this_worked:
+${return_this_worked()}
+
+Purify result:
+${DOMPurify.sanitize('<div>something sanitized <script>inside script</script></div>')}
+
+anchorme:
+${anchorme('hello text https://flowcrypt.com yay', {})}
+
+Mime encode:
+${(() => {
+  let MimeBuilder = (window as BrowserWidnow)['emailjs-mime-builder'];
+  let root_node = new MimeBuilder();
+  root_node.addHeader('random-header-1', 'RANDOM HEADER A');
+  root_node.addHeader('random-header-2', 'RANDOM HEADER B');
+  return root_node.build();
+})()}
   `);
 } catch (e) {
   $('pre').text(e.stack);
