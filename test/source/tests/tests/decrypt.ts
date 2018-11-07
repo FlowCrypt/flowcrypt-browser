e6649d651bad9447d4112008780050f3937af5a1
Tom J <tom@holub.me>
2018-11-07 22:38:15 +0000

    fix decrypt tests missing frameId | #1323

--- test/source/tests/tests/decrypt.ts
+++ test/source/tests/tests/decrypt.ts
@@ -3,9 +3,9 @@ import * as ava from 'ava';
 import { Util, Config } from '../../util';
 import { BrowserRecipe } from '../browser_recipe';
 
-export let defineDecryptTests = (testWithNewBrowser: TestWithBrowser, testWithSemaphoredBrowser: TestWithGlobalBrowser) => {
+export const defineDecryptTests = (testWithNewBrowser: TestWithBrowser, testWithSemaphoredBrowser: TestWithGlobalBrowser) => {
 
-  for (let m of Config.tests.messages) {
+  for (const m of Config.tests.messages) {
     ava.test(`decrypt[global] - ${m.name}`, testWithSemaphoredBrowser('compatibility', async (browser, t) => {
       await BrowserRecipe.pgpBlockVerifyDecryptedContent(browser, `chrome/elements/pgp_block.htm${m.params}`, m.content, m.password);
     }));
