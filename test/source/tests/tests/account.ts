164d5c17113b551fedc6ed8bb0193e3d4cd90823
Tom J <tom@holub.me>
2018-11-08 10:06:54 +0000

    tests const | #1323

--- test/source/tests/tests/account.ts
+++ test/source/tests/tests/account.ts
@@ -8,25 +8,25 @@ import * as ava from 'ava';
 import { Util, Config } from '../../util';
 import { expect } from 'chai';
 
-export let defineAcctTests = (testWithNewBrowser: TestWithBrowser, testWithSemaphoredGlobalBrowser: TestWithGlobalBrowser) => {
+export const defineAcctTests = (testWithNewBrowser: TestWithBrowser, testWithSemaphoredGlobalBrowser: TestWithGlobalBrowser) => {
 
   ava.test('compose > large file > subscribe > trial > attach again', testWithSemaphoredGlobalBrowser('trial', async (browser, t) => {
     // set up acct and open compose page
-    let settingsPage = await BrowserRecipe.openSettingsLoginApprove(browser, Config.secrets.ci_dev_account);
+    const settingsPage = await BrowserRecipe.openSettingsLoginApprove(browser, Config.secrets.ci_dev_account);
     await SetupPageRecipe.recover(settingsPage, 'flowcrypt.test.trial', { hasRecoverMore: false });
     await browser.closeAllPages();
-    let gmailPage = await BrowserRecipe.openGmailPageAndVerifyComposeBtnPresent(browser);
+    const gmailPage = await BrowserRecipe.openGmailPageAndVerifyComposeBtnPresent(browser);
     await GmailPageRecipe.closeInitialSetupNotif(gmailPage);
-    let composePage = await GmailPageRecipe.openSecureCompose(gmailPage, browser);
+    const composePage = await GmailPageRecipe.openSecureCompose(gmailPage, browser);
     await ComposePageRecipe.fillMsg(composePage, 'human@flowcrypt.com', 'a large file to trigger trial');
     // add a large file
     let fileInput = await composePage.target.$('input[type=file]');
-    let subscriptionNeededAlert = await composePage.triggerAndWaitNewAlert(async () => await fileInput!.uploadFile('test/samples/large.jpg'));
+    const subscriptionNeededAlert = await composePage.triggerAndWaitNewAlert(async () => await fileInput!.uploadFile('test/samples/large.jpg'));
     expect(await subscriptionNeededAlert.message()).contains('The files are over 5 MB');
     await subscriptionNeededAlert.accept();
     // get a trial
-    let subscribePage = await GmailPageRecipe.getSubscribeDialog(gmailPage, browser);
-    let subscribedAlert = await composePage.triggerAndWaitNewAlert(async () => await subscribePage.waitAndClick('@action-get-trial', { delay: 1 }));
+    const subscribePage = await GmailPageRecipe.getSubscribeDialog(gmailPage, browser);
+    const subscribedAlert = await composePage.triggerAndWaitNewAlert(async () => await subscribePage.waitAndClick('@action-get-trial', { delay: 1 }));
     expect(await subscribedAlert.message()).contains('now you can add your file again');
     await subscribedAlert.accept();
     await subscribePage.close();
