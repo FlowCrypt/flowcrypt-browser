c674e5aef9aec332974d9e32ee784de7c6bfb0a9
Tom J <tom@holub.me>
2018-12-26 12:56:13 +0000

    chrome.windows deprecation notice when missing #1418

--- extension/js/common/api/chrome.ts
+++ extension/js/common/api/chrome.ts
@@ -2,8 +2,12 @@
 
 'use strict';
 
-// could be replaced by a shim from mozilla
-
 export const tabsQuery = (q: chrome.tabs.QueryInfo): Promise<chrome.tabs.Tab[]> => new Promise(resolve => chrome.tabs.query(q, resolve));
 
-export const windowsCreate = (q: chrome.windows.CreateData): Promise<chrome.windows.Window | undefined> => new Promise(resolve => chrome.windows.create(q, resolve));
+export const windowsCreate = (q: chrome.windows.CreateData): Promise<chrome.windows.Window | undefined> => new Promise(resolve => {
+  if (typeof chrome.windows !== 'undefined') {
+    chrome.windows.create(q, resolve);
+  } else {
+    alert('Your platform is not supported: browser does not support extension windows');
+  }
+});
