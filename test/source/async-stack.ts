aa63a2d132df4a5599390b6a240c053bdf1c3ce7
Tom J <tom@holub.me>
2019-01-01 13:23:02 +0000

    less verbose async stack js | #1449

--- test/source/async-stack.ts
+++ test/source/async-stack.ts
@@ -1,77 +1,110 @@
 
 // tslint:disable:only-arrow-functions
 
-const wait = () => new Promise(resolve => setTimeout(resolve, 100));
+(() => {
 
-const thisWillFail = () => {
-  throw new Error('this failed');
-};
+  type Type = 'error' | 'object';
 
-function func() {
-  thisWillFail();
-}
+  const wait = () => new Promise(resolve => setTimeout(resolve, 100));
 
-class Class {
-  static staticConstAttr = () => {
-    func();
-  }
-  static staticFunc() {
-    Class.staticConstAttr();
-  }
-}
-
-const asyncArrowConst = async () => {
-  await wait();
-  Class.staticFunc();
-  await wait();
-};
-
-async function asyncFunc() {
-  await wait();
-  await asyncArrowConst();
-  await wait();
-}
-
-class ClassAsync {
-  static staticConstAttrAsync = async () => {
-    await wait();
-    await asyncFunc();
-    await wait();
-  }
-  static async staticAsyncFunc() {
-    await wait();
-    await ClassAsync.staticConstAttrAsync();
-    await wait();
-  }
-}
-
-(async () => {
-
-  try {
-    await ClassAsync.staticAsyncFunc();
-  } catch (e) {
-    const expectedStackStatements = [
-      'Error: this failed',
-      ' at thisWillFail ',
-      ' at func ',
-      ' at Function.Class.staticConstAttr ',
-      ' at Function.staticFunc ',
-      ' at asyncArrowConst ',
-      ' at async asyncArrowConst? ',
-      ' at async asyncFunc ',
-      ' at async staticConstAttrAsync? ',
-      ' at async staticAsyncFunc ',
-    ];
-    for (const statement of expectedStackStatements) {
-      if (e instanceof Error && (e.stack || '').indexOf(statement) === -1) {
-        console.error(`Unexpected stack format:\n${e.stack}\n\n\nExpected to include:\n${expectedStackStatements.join('\n')}`);
-        process.exit(1);
-      }
+  const thisWillFail = (type: Type) => {
+    if (type === 'error') {
+      throw new Error('this failed');
+    } else if (type === 'object') {
+      throw { nonsense: 'yes' };
     }
-    process.exit(0);
+  };
+
+  function func(type: Type) {
+    thisWillFail(type);
   }
 
-  console.error(`Fail - expected Error to be thrown`);
-  process.exit(1);
+  class Class {
+    static staticConstAttr = (type: Type) => {
+      func(type);
+    }
+    static staticFunc(type: Type) {
+      Class.staticConstAttr(type);
+    }
+  }
+
+  const asyncArrowConst = async (type: Type) => {
+    await wait();
+    Class.staticFunc(type);
+    await wait();
+  };
+
+  async function asyncFunc(type: Type) {
+    await wait();
+    await asyncArrowConst(type);
+    await wait();
+  }
+
+  class ClassAsync {
+    static staticConstAttrAsync = async (type: Type) => {
+      await wait();
+      await asyncFunc(type);
+      await wait();
+    }
+    static async staticAsyncFunc(type: Type) {
+      await wait();
+      await ClassAsync.staticConstAttrAsync(type);
+      await wait();
+    }
+  }
+
+  (async () => {
+
+    const doTestWith = async (type: Type, expectedStackStatements: string[]) => {
+      try {
+        await ClassAsync.staticAsyncFunc(type);
+      } catch (e) {
+        if (!(e instanceof Error)) {
+          console.error(`Thrown was unexpectedly not an error for type ${type}`, e);
+          return process.exit(1);
+        }
+        for (const statement of expectedStackStatements) {
+          if ((e.stack || '').indexOf(statement) === -1) {
+            console.error(`Unexpected stack format for type ${type}:\n${e.stack}\n\n\nExpected to include:\n${expectedStackStatements.join('\n')}`);
+            process.exit(1);
+          }
+        }
+        if (type === 'object' && JSON.stringify((e as any).thrown) !== '{"nonsense":"yes"}') {
+          console.error(`Unexpected e.throw for type ${type}:\n${JSON.stringify((e as any).thrown)}`);
+          process.exit(1);
+        }
+        return;
+      }
+      console.error(`Fail - expected Error to be thrown for type ${type}`);
+      return process.exit(1);
+    };
+
+    try {
+      await doTestWith('error', [
+        'Error: this failed',
+        ' at thisWillFail ',
+        ' at func ',
+        ' at Function.Class.staticConstAttr ',
+        ' at Function.staticFunc ',
+        ' at asyncArrowConst ',
+        ' at <async> asyncArrowConst ',
+        ' at <async> asyncFunc ',
+        ' at <async> staticConstAttrAsync ',
+        ' at <async> staticAsyncFunc ',
+      ]);
+      await doTestWith('object', [
+        'Error: Thrown[object][object Object]',
+        ' at asyncArrowConst ',
+        ' at <async> asyncFunc ',
+        ' at <async> staticConstAttrAsync ',
+        ' at <async> staticAsyncFunc ',
+      ]);
+      process.exit(0);
+    } catch (e) {
+      console.error(e);
+      return process.exit(1);
+    }
+
+  })();
 
 })();
