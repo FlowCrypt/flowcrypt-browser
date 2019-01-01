0fcb399bbff1a9139edee807a0e1a16146a16283
Tom J <tom@holub.me>
2019-01-01 11:44:43 +0000

    tests code style | #1447

--- test/source/tests/tests/settings.ts
+++ test/source/tests/tests/settings.ts
@@ -1,5 +1,5 @@
 import { TestWithBrowser, TestWithGlobalBrowser } from '..';
-import { PageRecipe, SettingsPageRecipe } from '../page_recipe';
+import { SettingsPageRecipe } from '../page_recipe';
 import { Url } from '../../browser';
 import * as ava from 'ava';
 import { Util, Config } from '../../util';
@@ -8,9 +8,9 @@ import { expect } from 'chai';
 export let defineSettingsTests = (testWithNewBrowser: TestWithBrowser, testWithSemaphoredGlobalBrowser: TestWithGlobalBrowser) => {
 
   ava.test('settings[global] - my own emails show as contacts', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await SettingsPageRecipe.toggleScreen(settingsPage, 'additional');
-    let comtactsFrame = await SettingsPageRecipe.awaitNewPageFrame(settingsPage, '@action-open-contacts-page', ['contacts.htm', 'placement=settings']);
+    const comtactsFrame = await SettingsPageRecipe.awaitNewPageFrame(settingsPage, '@action-open-contacts-page', ['contacts.htm', 'placement=settings']);
     await comtactsFrame.waitAll('@page-contacts');
     await Util.sleep(1);
     expect(await comtactsFrame.read('@page-contacts')).to.contain('flowcrypt.compatibility@gmail.com');
@@ -20,9 +20,9 @@ export let defineSettingsTests = (testWithNewBrowser: TestWithBrowser, testWithS
   }));
 
   ava.test('settings[global] - attester shows my emails', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await SettingsPageRecipe.toggleScreen(settingsPage, 'additional');
-    let attesterFrame = await SettingsPageRecipe.awaitNewPageFrame(settingsPage, '@action-open-attester-page', ['keyserver.htm', 'placement=settings']);
+    const attesterFrame = await SettingsPageRecipe.awaitNewPageFrame(settingsPage, '@action-open-attester-page', ['keyserver.htm', 'placement=settings']);
     await attesterFrame.waitAll('@page-attester');
     await Util.sleep(1);
     await attesterFrame.waitTillGone('@spinner');
@@ -34,12 +34,12 @@ export let defineSettingsTests = (testWithNewBrowser: TestWithBrowser, testWithS
   }));
 
   ava.test('settings[global] - verify key presense 1pp1', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await SettingsPageRecipe.verifyMyKeyPage(settingsPage, 'flowcrypt.compatibility.1pp1', 'button');
   }));
 
   ava.test('settings[global] - test pass phrase', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await SettingsPageRecipe.passphraseTest(settingsPage, Config.key('flowcrypt.wrong.passphrase').passphrase, false);
     await SettingsPageRecipe.passphraseTest(settingsPage, Config.key('flowcrypt.compatibility.1pp1').passphrase, true);
   }));
@@ -48,19 +48,19 @@ export let defineSettingsTests = (testWithNewBrowser: TestWithBrowser, testWithS
   // await tests.settings_my_key_tests(settingsPage, 'flowcrypt.compatibility.2pp1', 'link');
 
   ava.test('settings[global] - feedback form', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await settingsPage.waitAndClick('@action-open-modules-help');
     await settingsPage.waitAll('@dialog');
-    let helpFrame = await settingsPage.getFrame(['help.htm']);
+    const helpFrame = await settingsPage.getFrame(['help.htm']);
     await helpFrame.waitAndType('@input-feedback-message', 'automated puppeteer test: help form from settings footer');
-    let dialog = await settingsPage.triggerAndWaitNewAlert(() => helpFrame.waitAndClick('@action-feedback-send'));
+    const dialog = await settingsPage.triggerAndWaitNewAlert(() => helpFrame.waitAndClick('@action-feedback-send'));
     await dialog.accept();
   }));
 
   ava.test('settings[global] - view contact public key', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await SettingsPageRecipe.toggleScreen(settingsPage, 'additional');
-    let contactsFrame = await SettingsPageRecipe.awaitNewPageFrame(settingsPage, '@action-open-contacts-page', ['contacts.htm', 'placement=settings']);
+    const contactsFrame = await SettingsPageRecipe.awaitNewPageFrame(settingsPage, '@action-open-contacts-page', ['contacts.htm', 'placement=settings']);
     await contactsFrame.waitAll('@page-contacts');
     await Util.sleep(1);
     await contactsFrame.waitAndClick('@action-show-pubkey', { confirmGone: true });
@@ -78,7 +78,7 @@ export let defineSettingsTests = (testWithNewBrowser: TestWithBrowser, testWithS
   }));
 
   ava.test('settings[global] - my key page - primary + secondary', testWithSemaphoredGlobalBrowser('compatibility', async (browser, t) => {
-    let settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
+    const settingsPage = await browser.newPage(Url.extensionSettings('flowcrypt.compatibility@gmail.com'));
     await SettingsPageRecipe.verifyMyKeyPage(settingsPage, 'flowcrypt.compatibility.1pp1', 'link', 0);
     await SettingsPageRecipe.verifyMyKeyPage(settingsPage, 'flowcrypt.compatibility.2pp1', 'link', 1);
   }));
