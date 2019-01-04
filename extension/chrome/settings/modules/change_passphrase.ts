90e6293194ff4ca7bd955ae87df6c6b627ee533e
Tom J <tom@holub.me>
2019-01-04 13:52:14 +0000

    fix change passphrase dialog | #1456

--- extension/chrome/settings/modules/change_passphrase.ts
+++ extension/chrome/settings/modules/change_passphrase.ts
@@ -20,87 +20,79 @@ Catch.try(async () => {
 
   const privateKeys = await Store.keysGet(acctEmail);
   if (privateKeys.length > 1) {
-    $('#step_0_enter .sentence').text('Enter the current passphrase for your primary key');
-    $('#step_0_enter #original_password').attr('placeholder', 'Current primary key pass phrase');
-    $('#step_1_password #password').attr('placeholder', 'Enter a new primary key pass phrase');
+    $('#step_0_enter_current .sentence').text('Enter the current passphrase for your primary key');
+    $('#step_0_enter_current #original_password').attr('placeholder', 'Current primary key pass phrase');
+    $('#step_1_enter_new #password').attr('placeholder', 'Enter a new primary key pass phrase');
   }
 
   const [primaryKi] = await Store.keysGet(acctEmail, ['primary']);
   Settings.abortAndRenderErrorIfKeyinfoEmpty(primaryKi);
 
-  let origPassphrase = await Store.passphraseGet(acctEmail, primaryKi.longid);
+  const storedOrSessionPp = await Store.passphraseGet(acctEmail, primaryKi.longid);
+  let { keys: [primaryPrv] } = await openpgp.key.readArmored(primaryKi.private);
 
   const displayBlock = (name: string) => {
-    const blocks = ['step_0_enter', 'step_1_password', 'step_2_confirm', 'step_3_done'];
+    const blocks = ['step_0_enter_current', 'step_1_enter_new', 'step_2_confirm_new', 'step_3_done'];
     for (const block of blocks) {
-      $('#' + block).css('display', 'none');
+      $(`#${block}`).css('display', 'none');
     }
-    $('#' + name).css('display', 'block');
+    $(`#${name}`).css('display', 'block');
   };
 
-  if (origPassphrase) {
-    displayBlock('step_0_enter');
+  if (primaryPrv.isDecrypted() || (storedOrSessionPp && await Pgp.key.decrypt(primaryPrv, [storedOrSessionPp]))) {
+    displayBlock('step_1_enter_new'); // current pp is already known
   } else {
-    if (origPassphrase === '') {
-      $('h1').text('Set a pass phrase');
-    } else {
-      $('h1').text('Change your pass phrase');
-    }
-    displayBlock('step_1_password');
+    displayBlock('step_0_enter_current');
   }
 
-  $('.action_enter').click(Ui.event.handle(async () => {
+  $('#step_0_enter_current .action_test_current_passphrase').click(Ui.event.handle(async () => {
     const { keys: [prv] } = await openpgp.key.readArmored(primaryKi.private);
     if (await Pgp.key.decrypt(prv, [String($('#original_password').val())]) === true) {
-      origPassphrase = String($('#original_password').val());
-      displayBlock('step_1_password');
+      primaryPrv = prv;
+      displayBlock('step_1_enter_new');
     } else {
       alert('Pass phrase did not match, please try again.');
       $('#original_password').val('').focus();
     }
   }));
 
-  $('#password').on('keyup', Ui.event.prevent('spree', () => Settings.renderPasswordStrength('#step_1_password', '#password', '.action_password')));
+  $('#password').on('keyup', Ui.event.prevent('spree', () => Settings.renderPasswordStrength('#step_1_enter_new', '#password', '.action_set_pass_phrase')));
 
-  $('.action_password').click(Ui.event.handle(target => {
+  $('#step_1_enter_new .action_set_pass_phrase').click(Ui.event.handle(target => {
     if ($(target).hasClass('green')) {
-      displayBlock('step_2_confirm');
+      displayBlock('step_2_confirm_new');
     } else {
       alert('Please select a stronger pass phrase. Combinations of 4 to 5 uncommon words are the best.');
     }
   }));
 
-  $('.action_reset_password').click(Ui.event.handle(() => {
+  $('#step_2_confirm_new .action_use_another').click(Ui.event.handle(() => {
     $('#password').val('');
     $('#password2').val('');
-    displayBlock('step_1_password');
-    Settings.renderPasswordStrength('#step_1_password', '#password', '.action_password');
+    displayBlock('step_1_enter_new');
+    Settings.renderPasswordStrength('#step_1_enter_new', '#password', '.action_set_pass_phrase');
     $('#password').focus();
   }));
 
-  $('.action_change').click(Ui.event.prevent('double', async self => {
-    const newPassphrase = String($('#password').val());
-    if (newPassphrase !== $('#password2').val()) {
+  $('#step_2_confirm_new .action_change').click(Ui.event.prevent('double', async self => {
+    const newPp = String($('#password').val());
+    if (newPp !== $('#password2').val()) {
       alert('The two pass phrases do not match, please try again.');
       $('#password2').val('');
       $('#password2').focus();
+      return;
+    }
+    await Settings.openpgpKeyEncrypt(primaryPrv, newPp);
+    await Store.keysAdd(acctEmail, primaryPrv.armor());
+    const persistentlyStoredPp = await Store.passphraseGet(acctEmail, primaryKi.longid, true);
+    await Store.passphraseSave('local', acctEmail, primaryKi.longid, typeof persistentlyStoredPp === 'undefined' ? undefined : newPp);
+    await Store.passphraseSave('session', acctEmail, primaryKi.longid, typeof persistentlyStoredPp === 'undefined' ? newPp : undefined);
+    const { setup_simple } = await Store.getAcct(acctEmail, ['setup_simple']);
+    if (setup_simple) {
+      Settings.redirectSubPage(acctEmail, parentTabId, '/chrome/settings/modules/backup.htm', '&action=passphrase_change_gmail_backup');
     } else {
-      const { keys: [prv] } = await openpgp.key.readArmored(primaryKi.private);
-      if (!prv.isDecrypted()) {
-        await Pgp.key.decrypt(prv, [origPassphrase!]); // !null because we checked for this above, and user entry cannot be null
-      }
-      await Settings.openpgpKeyEncrypt(prv, newPassphrase);
-      const storedPassphrase = await Store.passphraseGet(acctEmail, primaryKi.longid, true);
-      await Store.keysAdd(acctEmail, prv.armor());
-      await Store.passphraseSave('local', acctEmail, primaryKi.longid, typeof storedPassphrase !== 'undefined' ? newPassphrase : undefined);
-      await Store.passphraseSave('session', acctEmail, primaryKi.longid, typeof storedPassphrase !== 'undefined' ? undefined : newPassphrase);
-      const { setup_simple } = await Store.getAcct(acctEmail, ['setup_simple']);
-      if (setup_simple) {
-        Settings.redirectSubPage(acctEmail, parentTabId, '/chrome/settings/modules/backup.htm', '&action=passphrase_change_gmail_backup');
-      } else {
-        alert('Now that you changed your pass phrase, you should back up your key. New backup will be protected with new passphrase.');
-        Settings.redirectSubPage(acctEmail, parentTabId, '/chrome/settings/modules/backup.htm', '&action=options');
-      }
+      alert('Now that you changed your pass phrase, you should back up your key. New backup will be protected with new passphrase.');
+      Settings.redirectSubPage(acctEmail, parentTabId, '/chrome/settings/modules/backup.htm', '&action=options');
     }
   }));
 
