f67a5c3eb9f48c68f7743c980a9d39fbffd3f848
Tom J <tom@holub.me>
2019-01-09 12:38:38 +0000

    proxy fail detection | #1480

--- extension/js/common/api/api.ts
+++ extension/js/common/api/api.ts
@@ -266,7 +266,18 @@ export class Api {
     isBadReq: (e: any): e is AjaxError => e instanceof AjaxError && e.status === 400,
     isReqTooLarge: (e: any): e is AjaxError => e instanceof AjaxError && e.status === 413,
     isServerErr: (e: any): e is AjaxError => e instanceof AjaxError && e.status >= 500,
-    isBlockedByProxy: (e: any): e is AjaxError => e instanceof AjaxError && e.status === 200 && e.responseText.indexOf('Access to this site is blocked') !== -1,
+    isBlockedByProxy: (e: any): e is AjaxError => {
+      if (!(e instanceof AjaxError)) {
+        return false;
+      }
+      if (e.status === 200 && e.responseText.indexOf('Access to this site is blocked') !== -1) {
+        return true;
+      }
+      if (e.status === 403 && e.responseText.indexOf('Your system policy has denied access to the requested site.') !== -1) {
+        return true; // Symantec proxy or firewall
+      }
+      return false;
+    },
     isSignificant: (e: any) => {
       return !Api.err.isNetErr(e) && !Api.err.isServerErr(e) && !Api.err.isNotFound(e) && !Api.err.isMailOrAcctDisabled(e) && !Api.err.isAuthErr(e)
         && !Api.err.isBlockedByProxy(e);
