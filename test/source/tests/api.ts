094e68aeed7aa5a18fd240859e8e4f7184c5ea73
Tom J <tom@holub.me>
2019-01-18 15:33:49 +0000

    ci initialize | #1173

--- test/source/tests/api.ts
+++ test/source/tests/api.ts
@@ -14,17 +14,17 @@ class ApiErrorResponse extends Error {
 
 export class FlowCryptApi {
 
-  private static call = async (path: string, values: { [k: string]: any }) => {
-    const r = await request.post({ url: `https://flowcrypt.com/api${path}`, json: values, headers: { 'api-version': 3 } });
+  private static call = async (url: string, values: { [k: string]: any }) => {
+    const r = await request.post({ url, json: values, headers: { 'api-version': 3 } });
     if (r.body.error) {
-      throw new ApiErrorResponse(`FlowCryptApi ${path} returned an error: ${r.body.error.message}`, r);
+      throw new ApiErrorResponse(`FlowCryptApi ${url} returned an error: ${r.body.error.message}`, r);
     }
     return r;
   }
 
   public static hookCiAcctDelete = async (email: string) => {
     try {
-      await FlowCryptApi.call('/hook/ci_account_delete', { email, ci_admin_token });
+      await FlowCryptApi.call('https://flowcrypt.com/api/hook/ci_account_delete', { ci_admin_token, email });
     } catch (e) {
       if (e.message instanceof ApiErrorResponse && e.response.body.error.message === 'Unknown account email') {
         throw e;
@@ -33,15 +33,29 @@ export class FlowCryptApi {
   }
 
   public static hookCiSubscriptionExpire = async (email: string) => {
-    await FlowCryptApi.call('/hook/ci_subscription_expire', { email, ci_admin_token });
+    await FlowCryptApi.call('https://flowcrypt.com/api/hook/ci_subscription_expire', { ci_admin_token, email });
   }
 
   public static hookCiSubscriptionReset = async (email: string) => {
-    await FlowCryptApi.call('/hook/ci_subscription_reset', { email, ci_admin_token });
+    await FlowCryptApi.call('https://flowcrypt.com/api/hook/ci_subscription_reset', { ci_admin_token, email });
   }
 
   public static hookCiDebugEmail = async (debug_title: string, debug_html_content: string) => { // tslint:disable-line:variable-name
-    await FlowCryptApi.call('/hook/ci_debug_email', { ci_admin_token, debug_title, debug_html_content });
+    await FlowCryptApi.call('https://flowcrypt.com/api/hook/ci_debug_email', { ci_admin_token, debug_title, debug_html_content });
+  }
+
+  public static ciInitialize = async (acct: string, pwd: string, backup: string) => {
+    const r = await FlowCryptApi.call('https://cron.flowcrypt.com/ci_initialize', { ci_admin_token, acct, pwd, backup });
+    if (!r.body.success) {
+      if (r.body.errHtml) {
+        try {
+          await FlowCryptApi.hookCiDebugEmail('ci_browser: ci_initialize', r.body.errHtml);
+        } catch (e) {
+          console.error('error calling ciInitialize', r.body);
+          console.error('error calling hookDebugEmail about it:', String(e));
+        }
+      }
+    }
   }
 
 }
