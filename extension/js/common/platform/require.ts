bcce7658825f5dba1da7b259de255ab5385a7a76
Tom J <tom@holub.me>
2019-02-06 13:15:45 +0000

    clarify imports |  #1520

--- extension/js/common/platform/require.ts
+++ extension/js/common/platform/require.ts
@@ -2,6 +2,25 @@
 
 'use strict';
 
+/**
+ * If you are here to add a dependency to the project, you may be at a wrong place. But since you are here..
+ *
+ * This file is here because we reuse common/core in different environments: the web, chrome extension and Node.js
+ *
+ * External libraries in JS tend to have a thousand ways to import them, and they may expect to be imported differently in each environment.
+ *
+ * _IF_ you are adding a dependency that we use across environments, _AND_ such dependency expects to be imported differently in each, _THEN_ this may be the right place to put it.
+ *
+ * In such case you should ping @tomholub for guidance.
+ *
+ * For all other imports:
+ *  - see tsconfig.json and mimic the style, including the COMMENT flag
+ *  - add type definitions from node_modules/xxx/xxx.d.ts to extension/types/ and check it into git
+ *  - add dest js dep file into extension/lib and check it into git
+ *  - import in .ts files by using: import { Dep } from 'depname'; // this is only used in ts, import will be commented out during build, and that's why you must:
+ *  - add < script > tags to appropriate .htm files pointing to the js using ABSOLUTE path, unless this dep can be imported as es6 module tag, in which case use RELATIVE path
+ */
+
 /// <reference path="../../../types/openpgp.d.ts" />
 
 declare const openpgp: typeof OpenPGP;
