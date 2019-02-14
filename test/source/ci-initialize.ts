5cdaf9250fba459b2beb88044ab0c15fea81e907
Tom J <tom@holub.me>
2019-02-14 13:42:37 +0000

    init one by one | #1539

--- test/source/ci-initialize.ts
+++ test/source/ci-initialize.ts
@@ -3,12 +3,17 @@
 import { Config } from './util';
 import { FlowCryptApi } from './tests/api';
 
-for (const acct of ['flowcrypt.compatibility@gmail.com', 'test.ci.compose@org.flowcrypt.com']) {
-  const { email, password, backup } = Config.secrets.auth.google.filter(a => a.email === acct)[0];
-  FlowCryptApi.ciInitialize(email, password, backup).then(() => {
-    console.info(`Successfully initialized CI for ${acct}`);
-  }).catch(e => {
-    console.error(`Failed to initialize CI for ${acct}: ${String(e)}`);
-    // do not fail whole process - the rest of tests may work without this
-  });
-}
+(async () => {
+
+  for (const acct of ['flowcrypt.compatibility@gmail.com', 'test.ci.compose@org.flowcrypt.com']) {
+    const { email, password, backup } = Config.secrets.auth.google.filter(a => a.email === acct)[0];
+    try {
+      console.info(`Initializing CI for ${acct}`);
+      await FlowCryptApi.ciInitialize(email, password, backup);
+      console.info(`Successfully initialized CI for ${acct}`);
+    } catch (e) { // do not fail whole process - the rest of tests may work without this
+      console.error(`Failed to initialize CI for ${acct}: ${String(e)}`);
+    }
+  }
+
+})().catch(console.error);
