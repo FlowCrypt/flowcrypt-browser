f4498bf94d02dac7976aaecfb29aaa68a76c7ae2
Tom J <tom@holub.me>
2019-02-19 13:53:04 +0000

    retest again | #1557

--- test/source/ci-initialize.ts
+++ test/source/ci-initialize.ts
@@ -6,12 +6,12 @@ import { FlowCryptApi } from './tests/api';
 (async () => {
 
   for (const { email, password, backup } of Config.secrets.auth.google) {
+    const e = email.replace(/gmail|flowcrypt|test|com|@|\./, '');
     try {
-      console.info(`[${email}] Initializing CI`);
+      console.info(`[${e}] Initializing CI`);
       await FlowCryptApi.ciInitialize(email, password, backup);
-      console.info(`[${email}] Successfully initialized CI`);
     } catch (e) { // do not fail whole process - the rest of tests may work without this
-      console.error(`[${email}] Failed to initialize CI: ${String(e)}`);
+      console.error(`[${e}] ${String(e)}`);
     }
   }
 
