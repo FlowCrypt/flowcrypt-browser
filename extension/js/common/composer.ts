2e502c94b93014a578633e84f2f3549034d43c1e
Evan Hellmuth <evan.hellmuth@gmail.com>
2019-02-22 16:13:52 +0200

    Reload reply iframe instead of removing it

--- extension/js/common/composer.ts
+++ extension/js/common/composer.ts
@@ -299,7 +299,9 @@ export class Composer {
     }, this.getErrHandlers(`change footer`)));
     $('.delete_draft').click(Ui.event.handle(async () => {
       await this.draftDelete();
-      this.app.closeMsg();
+
+      // Reload iframe so we don't leave users without a reply UI.
+      window.location.reload();
     }, this.getErrHandlers('delete draft')));
     this.S.cached('body').bind({ drop: Ui.event.stop(), dragover: Ui.event.stop() }); // prevents files dropped out of the intended drop area to screw up the page
     this.S.cached('icon_sign').click(Ui.event.handle(() => this.toggleSignIcon(), this.getErrHandlers(`enable/disable signing`)));
