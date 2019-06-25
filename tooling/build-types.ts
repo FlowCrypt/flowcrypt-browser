e3860b284fc8c231cb3ec584a7a392d4cdb5fc9b
Tom J <tom@holub.me>
2019-06-25 11:44:31 +0000

    fix enterprise mock host

--- tooling/build-types.ts
+++ tooling/build-types.ts
@@ -3,7 +3,7 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const CHROME_CONSUMER = 'chrome-consumer';
 const CHROME_ENTERPRISE = 'chrome-enterprise';
-const MOCK_HOST = 'http://localhost:8001';
+const MOCK_HOST: { [buildType: string]: string } = { 'chrome-consumer': 'http://localhost:8001', 'chrome-enterprise': 'http://google.mock.flowcrypt.com:8001' };
 
 const buildDir = (buildType: string) => `./build/${buildType}`;
 
@@ -14,7 +14,7 @@ const edit = (filepath: string, editor: (content: string) => string) => {
 const makeMockBuild = (buildType: string) => {
   const mockBuildType = `${buildType}-mock`;
   exec(`cp -r ${buildDir(buildType)} ${buildDir(mockBuildType)}`);
-  const editor = (code: string) => code.replace(/const (GOOGLE_API_HOST|GOOGLE_OAUTH_SCREEN_HOST) = [^;]+;/g, `const $1 = '${MOCK_HOST}';`);
+  const editor = (code: string) => code.replace(/const (GOOGLE_API_HOST|GOOGLE_OAUTH_SCREEN_HOST) = [^;]+;/g, `const $1 = '${MOCK_HOST[buildType]}';`);
   edit(`${buildDir(mockBuildType)}/js/common/core/const.js`, editor);
   edit(`${buildDir(mockBuildType)}/js/content_scripts/webmail_bundle.js`, editor);
 };
