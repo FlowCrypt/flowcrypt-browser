53ec2f5d4640df5d14197cfc4165f365d77e1f0a
Tom J <tom@holub.me>
2019-07-05 11:16:35 +0000

    build concurrently again

--- build.sh
+++ build.sh
@@ -7,11 +7,17 @@ set -euxo pipefail
 
 shopt -s globstar
 
-# prebuild
+# prebuild tools
 rm -rf ./build
 npx tsc --project ./conf/tsconfig.tooling.json
 mkdir -p $OUTDIR
 
+# build concurrently
+node ./build/tooling/tsc-compiler --project ./tsconfig.json & pids+=($!)
+node ./build/tooling/tsc-compiler --project ./conf/tsconfig.content_scripts.json & pids+=($!)
+for pid in "${pids[@]}"; do wait "$pid" || exit 1; done
+
+# build sequentially
 node ./build/tooling/tsc-compiler --project ./tsconfig.json
 node ./build/tooling/tsc-compiler --project ./conf/tsconfig.content_scripts.json
 ( cd $SRCDIR && cp -r --parents ./**/*.{js,htm,css,ttf,woff2,png,svg,txt} ./{.web-extension-id,manifest.json} ../$OUTDIR )
