833854712d36f0d4d8f66c8e65b68e15d7204e80
michael-volynets <michael.volynets@startupsoft.us>
2019-07-08 17:47:01 +0300

    Corrected encrypted/text email text

--- test/source/tests/tests/compose.ts
+++ test/source/tests/tests/compose.ts
@@ -190,9 +190,15 @@ export const defineComposeTests = (testVariant: TestVariant, testWithNewBrowser:
       const appendUrl = 'isReplyBox=___cu_true___&threadId=16b8e09ebf6c5f54&skipClickPrompt=___cu_false___&ignoreDraft=___cu_false___' +
         '&threadMsgId=16b8e09ebf6c5f54&to=Human%20at%20FlowCrypt%20%3Chuman%40flowcrypt.com%3E&from=test.ci.compose%40org.flowcrypt.com ' +
         '&subject=Re%3A%20do%20not%20delete%3A%20plain%20message%20so%20that%20human%20shows%20in%20contacts';
+      const messageBeginingText =
+        [
+          'On 2019-06-25 at 09:48, human@flowcrypt.com wrote:',
+          '> --',
+          '> Human at FlowCrypt. Try our new Android app'
+        ].join('\n');
       const composePage = await ComposePageRecipe.openStandalone(t, browser, 'compose', { appendUrl, hasReplyPrompt: true });
       await composePage.waitAndClick('@action-accept-reply-prompt', { delay: 1 });
-      await baseQuotingTest(composePage, 'On 2019-06-25 at 09:48, human@flowcrypt.com wrote:');
+      await baseQuotingTest(composePage, messageBeginingText);
     }));
 
     ava.test('compose[global:compatibility] - standalone - quote - can load quote from plain/text email', testWithSemaphoredGlobalBrowser('compatibility', async (t, browser) => {
@@ -228,7 +234,7 @@ export const defineComposeTests = (testVariant: TestVariant, testWithNewBrowser:
       await baseQuotingTest(composePage, messageBeginingText);
     }));
 
-    ava.test.only('compose[global:compatibility] - reply - can load quote from encrypted/html email', testWithSemaphoredGlobalBrowser('compatibility', async (t, browser) => {
+    ava.test('compose[global:compatibility] - reply - can load quote from encrypted/html email', testWithSemaphoredGlobalBrowser('compatibility', async (t, browser) => {
       const appendUrl = 'isReplyBox=___cu_true___&threadId=1663a65bbd73ce1a&skipClickPrompt=___cu_false___&ignoreDraft=___cu_false___' +
         '&threadMsgId=1663a65bbd73ce1a&to=Henry%20Electrum%20%3Ccensored%40email.com%3E&from=flowcrypt.compatibility%40gmail.com' +
         '&subject=Re%3A%20Encrypted%20Message';
