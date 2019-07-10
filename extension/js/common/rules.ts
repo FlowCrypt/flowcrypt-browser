c96bd5c85e52fb7ca879db732fdf52d11912ee0b
Tom J <tom@holub.me>
2019-07-10 17:15:40 +0000

    adding ALLOW_CUSTOM_KEYSERVER rule

--- extension/js/common/rules.ts
+++ extension/js/common/rules.ts
@@ -5,7 +5,7 @@
 import { Str, Dict } from './core/common.js';
 import { Pgp } from './core/pgp.js';
 
-export type DomainRule = { flags: ('NO_PRV_CREATE' | 'NO_PRV_BACKUP' | 'STRICT_GDPR')[] };
+export type DomainRule = { flags: ('NO_PRV_CREATE' | 'NO_PRV_BACKUP' | 'STRICT_GDPR' | 'ALLOW_CUSTOM_KEYSERVER')[] };
 
 export class Rules {
 
@@ -14,6 +14,7 @@ export class Rules {
   private rules: Dict<DomainRule> = {
     '745126dcac9a94a1931a3a5e03f02be3820f51d1': { flags: ['NO_PRV_CREATE', 'NO_PRV_BACKUP', 'STRICT_GDPR'] }, // n
     '77754b18ecb3f2f7c59bf20cfe06afac2a6458ec': { flags: ['NO_PRV_CREATE', 'NO_PRV_BACKUP', 'STRICT_GDPR'] }, // v
+    'e308e274e602f710349f5fe178cef094fa01c32b': { flags: ['NO_PRV_BACKUP', 'ALLOW_CUSTOM_KEYSERVER'] }, // h
     [this.other]: { flags: [] },
   };
 
@@ -36,5 +37,7 @@ export class Rules {
   canBackupKeys = () => !this.rules[this.domainHash].flags.includes('NO_PRV_BACKUP');
 
   hasStrictGdpr = () => this.rules[this.domainHash].flags.includes('STRICT_GDPR');
+
+  canUseCustomKeyserver = () => this.rules[this.domainHash].flags.includes('ALLOW_CUSTOM_KEYSERVER');
 
 }
