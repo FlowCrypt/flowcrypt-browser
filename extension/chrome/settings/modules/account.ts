2ccf772361adee39ec6c0fea4ad2fe722a321aee
Tom J <tom@holub.me>
2019-12-01 21:59:55 +0000

    AccountView

--- extension/chrome/settings/modules/account.ts
+++ extension/chrome/settings/modules/account.ts
@@ -2,7 +2,6 @@
 
 'use strict';
 
-import { Catch } from '../../../js/common/platform/catch.js';
 import { Store, Subscription } from '../../../js/common/platform/store.js';
 import { Ui } from '../../../js/common/browser.js';
 import { Settings } from '../../../js/common/settings.js';
@@ -11,49 +10,61 @@ import { Assert } from '../../../js/common/assert.js';
 import { Xss } from '../../../js/common/platform/xss.js';
 import { Api } from '../../../js/common/api/api.js';
 import { Url } from '../../../js/common/core/common.js';
+import { View } from '../../../js/common/view.js';
 
-Catch.try(async () => {
+// todo - this this page should be removed, link from settings should point to flowcrypt.com/account once available
 
-  const uncheckedUrlParams = Url.parse(['acctEmail', 'parentTabId']);
-  const acctEmail = Assert.urlParamRequire.string(uncheckedUrlParams, 'acctEmail');
-  const parentTabId = Assert.urlParamRequire.string(uncheckedUrlParams, 'parentTabId');
+View.run(class AccountView extends View {
 
-  Xss.sanitizeRender('.loading', Ui.spinner('green', 'large_spinner'));
+  private acctEmail: string;
+  private parentTabId: string;
 
-  const authInfo = await Store.authInfo(acctEmail);
-  let subscription = await Store.subscription(acctEmail);
-  try {
-    const r = await Backend.getSubscriptionWithoutLogin(acctEmail);
-    subscription = new Subscription(r.subscription);
-    await Backend.accountGet(authInfo); // here to test auth
-  } catch (e) {
-    if (Api.err.isAuthErr(e) && subscription.level) {
-      Settings.offerToLoginWithPopupShowModalOnErr(acctEmail, () => window.location.reload());
-      return;
+  constructor() {
+    super();
+    const uncheckedUrlParams = Url.parse(['acctEmail', 'parentTabId']);
+    this.acctEmail = Assert.urlParamRequire.string(uncheckedUrlParams, 'acctEmail');
+    this.parentTabId = Assert.urlParamRequire.string(uncheckedUrlParams, 'parentTabId');
+  }
+
+  async render() {
+    Xss.sanitizeRender('.loading', Ui.spinner('green', 'large_spinner'));
+    const authInfo = await Store.authInfo(this.acctEmail);
+    let subscription = await Store.subscription(this.acctEmail);
+    try {
+      const r = await Backend.getSubscriptionWithoutLogin(this.acctEmail);
+      subscription = new Subscription(r.subscription);
+      await Backend.accountGet(authInfo); // here to test auth
+    } catch (e) {
+      if (Api.err.isAuthErr(e) && subscription.level) {
+        Settings.offerToLoginWithPopupShowModalOnErr(this.acctEmail, () => window.location.reload());
+        return;
+      }
     }
+    $('.email').text(authInfo.account);
+    $('.level').text('advanced');
+    $('.expire').text(subscription.expire ? subscription.expire.split(' ')[0] : 'lifetime');
+    if (subscription.method === 'stripe') {
+      $('.line.cancel').css('display', 'block');
+      $('.expire_label').text('Renews on');
+      $('.price').text('$5 monthly');
+      $('.method').text('Credit Card (processed by Stripe Payments)');
+    } else if (subscription.method === 'group') {
+      $('.price').text('Group billing');
+      $('.hide_if_group_billing').css('display', 'none');
+    } else {
+      $('.expire_label').text('Until');
+      $('.price').text('free');
+      Xss.sanitizeRender('.method', 'trial <a href="#" class="action_go_subscription">upgrade</a>');
+    }
+    if (subscription.method !== 'group') {
+      $('.get_group_billing').css('display', 'block');
+    }
+    $('.loading').text(' ');
+    $('.list_table').css('display', 'block');
   }
 
-  $('.email').text(authInfo.account);
-  $('.level').text('advanced');
-  $('.expire').text(subscription.expire ? subscription.expire.split(' ')[0] : 'lifetime');
-  if (subscription.method === 'stripe') {
-    $('.line.cancel').css('display', 'block');
-    $('.expire_label').text('Renews on');
-    $('.price').text('$5 monthly');
-    $('.method').text('Credit Card (processed by Stripe Payments)');
-  } else if (subscription.method === 'group') {
-    $('.price').text('Group billing');
-    $('.hide_if_group_billing').css('display', 'none');
-  } else {
-    $('.expire_label').text('Until');
-    $('.price').text('free');
-    Xss.sanitizeRender('.method', 'trial <a href="#" class="action_go_subscription">upgrade</a>');
-    $('.action_go_subscription').click(Ui.event.handle(() => Settings.redirectSubPage(acctEmail, parentTabId, '/chrome/elements/subscribe.htm', '&placement=settings')));
+  setHandlers() {
+    $('.action_go_subscription').click(this.setHandler(() => Settings.redirectSubPage(this.acctEmail, this.parentTabId, '/chrome/elements/subscribe.htm', '&placement=settings')));
   }
-  if (subscription.method !== 'group') {
-    $('.get_group_billing').css('display', 'block');
-  }
-  $('.loading').text(' ');
-  $('.list_table').css('display', 'block');
 
-})();
+});
