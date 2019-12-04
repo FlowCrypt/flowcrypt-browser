396d3b47cd24c1e50149bc6d83cdd98b5473cba2
michael-volynets <michael.volynets@startupsoft.co>
2019-12-04 15:58:56 +0200

    Issue #2231, refactored add_key.ts file

--- extension/chrome/settings/modules/add_key.ts
+++ extension/chrome/settings/modules/add_key.ts
@@ -14,63 +14,83 @@ import { Assert } from '../../../js/common/assert.js';
 import { KeyImportUi, UserAlert, KeyCanBeFixed } from '../../../js/common/ui/key_import_ui.js';
 import { initPassphraseToggle } from '../../../js/common/ui/passphrase_ui.js';
 import { Xss } from '../../../js/common/platform/xss.js';
+import { View } from '../../../js/common/view.js';
 
-Catch.try(async () => {
+View.run(class AddKeyView extends View {
 
-  const uncheckedUrlParams = Url.parse(['acctEmail', 'parentTabId']);
-  const acctEmail = Assert.urlParamRequire.string(uncheckedUrlParams, 'acctEmail');
-  const parentTabId = Assert.urlParamRequire.string(uncheckedUrlParams, 'parentTabId');
+  private readonly acctEmail: string;
+  private readonly parentTabId: string;
+  private readonly keyImportUi = new KeyImportUi({ rejectKnown: true });
 
-  await initPassphraseToggle(['input_passphrase']);
-  const keyImportUi = new KeyImportUi({ rejectKnown: true });
-  keyImportUi.initPrvImportSrcForm(acctEmail, parentTabId);
-
-  Xss.sanitizeRender('#spinner_container', Ui.spinner('green') + ' loading..');
-
-  const keyinfos = await Store.keysGet(acctEmail);
-  const privateKeysLongIds = keyinfos.map(ki => ki.longid);
-  let keyBackups;
-
-  try {
-    keyBackups = await Google.gmail.fetchKeyBackups(acctEmail);
-    if (keyBackups.length) {
-      const notImportedBackupLongids: string[] = [];
-      for (const longid of Value.arr.unique(await Promise.all(keyBackups.map(Pgp.key.longid)))) {
-        if (longid && !privateKeysLongIds.includes(longid)) {
-          notImportedBackupLongids.push(longid);
-        }
-      }
-      if (notImportedBackupLongids.length) {
-        $('label[for=source_backup]').text('Load from backup (' + notImportedBackupLongids.length + ' new to import)');
-      } else {
-        $('label[for=source_backup]').text('Load from backup (already loaded)').css('color', '#AAA');
-        $('#source_backup').prop('disabled', true);
-      }
-    } else {
-      $('label[for=source_backup]').text('Load from backup (no backups found)').css('color', '#AAA');
-      $('#source_backup').prop('disabled', true);
-    }
-  } catch (e) {
-    if (Api.err.isAuthPopupNeeded(e)) {
-      BrowserMsg.send.notificationShowAuthPopupNeeded(parentTabId, { acctEmail });
-    }
-    $('label[for=source_backup]').text('Load from backup (error checking backups)').css('color', '#AAA');
-    $('#source_backup').prop('disabled', true);
+  constructor() {
+    super();
+    const uncheckedUrlParams = Url.parse(['acctEmail', 'parentTabId']);
+    this.acctEmail = Assert.urlParamRequire.string(uncheckedUrlParams, 'acctEmail');
+    this.parentTabId = Assert.urlParamRequire.string(uncheckedUrlParams, 'parentTabId');
   }
 
-  $('.source_selector').css('display', 'block');
-  $('#spinner_container').text('');
-  $('.action_add_private_key').click(Ui.event.prevent('double', async (e) => {
-    if (e.className.includes('gray')) {
+  async render() {
+    await initPassphraseToggle(['input_passphrase']);
+    this.keyImportUi.initPrvImportSrcForm(this.acctEmail, this.parentTabId);
+    Xss.sanitizeRender('#spinner_container', Ui.spinner('green') + ' loading..');
+    await this.loadAndRenderKeyBackupsOrRenderError();
+    $('.source_selector').css('display', 'block');
+    $('#spinner_container').text('');
+  }
+
+  setHandlers() {
+    $('.action_add_private_key').click(Ui.event.prevent('double', this.addPrivateKeyHandler));
+    $('#input_passphrase').keydown(this.setHandler((el, ev) => {
+      if (ev.which === 13) {
+        this.addPrivateKeyHandler(el).catch(Catch.reportErr);
+      }
+    }));
+  }
+
+  private async loadAndRenderKeyBackupsOrRenderError() {
+    const keyInfos = await Store.keysGet(this.acctEmail);
+    const privateKeysLongIds = keyInfos.map(ki => ki.longid);
+    let keyBackups: OpenPGP.key.Key[] | undefined;
+    try {
+      keyBackups = await Google.gmail.fetchKeyBackups(this.acctEmail);
+      if (keyBackups.length) {
+        const notImportedBackupLongids: string[] = [];
+        for (const longid of Value.arr.unique(await Promise.all(keyBackups.map(Pgp.key.longid)))) {
+          if (longid && !privateKeysLongIds.includes(longid)) {
+            notImportedBackupLongids.push(longid);
+          }
+        }
+        if (notImportedBackupLongids.length) {
+          $('label[for=source_backup]').text('Load from backup (' + notImportedBackupLongids.length + ' new to import)');
+        } else {
+          $('label[for=source_backup]').text('Load from backup (already loaded)').css('color', '#AAA');
+          $('#source_backup').prop('disabled', true);
+        }
+      } else {
+        $('label[for=source_backup]').text('Load from backup (no backups found)').css('color', '#AAA');
+        $('#source_backup').prop('disabled', true);
+      }
+    } catch (e) {
+      if (Api.err.isAuthPopupNeeded(e)) {
+        BrowserMsg.send.notificationShowAuthPopupNeeded(this.parentTabId, { acctEmail: this.acctEmail });
+      }
+      $('label[for=source_backup]').text('Load from backup (error checking backups)').css('color', '#AAA');
+      $('#source_backup').prop('disabled', true);
+    }
+  }
+
+  private addPrivateKeyHandler = async (submitBtn: HTMLElement) => {
+    if (submitBtn.className.includes('gray')) {
       await Ui.modal.warning('Please double check the pass phrase input field for any issues.');
       return;
     }
     try {
-      const checked = await keyImportUi.checkPrv(acctEmail, String($('.input_private_key').val()), String($('.input_passphrase').val()));
+      const checked = await this.keyImportUi.checkPrv(this.acctEmail, String($('.input_private_key').val()), String($('.input_passphrase').val()));
       if (checked) {
-        await Store.keysAdd(acctEmail, checked.normalized); // resulting new_key checked above
-        await Store.passphraseSave($('.input_passphrase_save').prop('checked') ? 'local' : 'session', acctEmail, checked.longid, checked.passphrase);
-        BrowserMsg.send.reload(parentTabId, { advanced: true });
+        await Store.keysAdd(this.acctEmail, checked.normalized); // resulting new_key checked above
+        await Store.passphraseSave($('.input_passphrase_save').prop('checked') ? 'local' : 'session', this.acctEmail,
+          checked.longid, checked.passphrase);
+        BrowserMsg.send.reload(this.parentTabId, { advanced: true });
       }
     } catch (e) {
       if (e instanceof UserAlert) {
@@ -82,6 +102,5 @@ Catch.try(async () => {
         return await Ui.modal.error(`An error happened when processing the key: ${String(e)}\nPlease write at human@flowcrypt.com`);
       }
     }
-  }));
-
-})();
+  }
+});
