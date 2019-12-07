d044311ebfe43c0abf986763451f92d861f8aeae
Tom J <tom@holub.me>
2019-12-07 08:56:34 +0000

    handle storage undefined in storageLocalGetAll

--- extension/js/common/api/chrome.ts
+++ extension/js/common/api/chrome.ts
@@ -55,8 +55,12 @@ export const storageLocalGet = (keys: string[]): Promise<Object> => new Promise(
   }
 });
 
-export const storageLocalGetAll = (): Promise<{ [key: string]: any }> => new Promise((resolve, reject) => {
-  chrome.storage.local.get(resolve);
+export const storageLocalGetAll = (): Promise<{ [key: string]: any }> => new Promise((resolve) => {
+  if (typeof chrome.storage === 'undefined') {
+    handleFatalErr('storage_undefined', new Error('storage is undefined'));
+  } else {
+    chrome.storage.local.get(resolve);
+  }
 });
 
 export const storageLocalSet = (values: Object): Promise<void> => new Promise((resolve) => { // tslint:disable-line:ban-types
