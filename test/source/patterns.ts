ec379602f9414f84d901004ea736c61d6b948ef4
Tom J <tom@holub.me>
2019-12-10 15:11:25 +0000

    fix regex style

--- test/source/patterns.ts
+++ test/source/patterns.ts
@@ -44,11 +44,11 @@ const validateLine = (line: string, location: string) => {
     console.error(`errors not handled in ${location} (make sure to use Catch.setHandledTimeout or Catch.setHandledInterval):\n${line}\n`);
     errsFound++;
   }
-  if (line.match(/^  (public |private |protected |static |async )*((?!constructor)[a-z][a-zA-Z0-9]+)\([^;]+[^>] \{$/)) {
+  if (line.match(/^ {2}(public |private |protected |static |async )*((?!constructor)[a-z][a-zA-Z0-9]+)\([^;]+[^>] \{$/)) {
     console.error(`wrongly using class method, which can cause binding loss (use fat arrow method properties instead):\n${line}\n`);
     errsFound++;
   }
-  if (line.match(/^  (public |private |protected |static )+?[a-z][a-zA-Z0-9]+ = (async )?\(.+\)(: .+)? => .+;$/)) {
+  if (line.match(/^ {2}(public |private |protected |static )+?[a-z][a-zA-Z0-9]+ = (async )?\(.+\)(: .+)? => .+;$/)) {
     console.error(`don't use single-line "method = (arg) => result" class methods, give them a method body and a return statement "method = (arg) => { return result; }":\n${line}\n`);
     errsFound++;
   }
