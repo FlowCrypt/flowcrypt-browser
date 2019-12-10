72977fbfb10c81bde118055d6eef1ee2937f28be
Tom J <tom@holub.me>
2019-12-10 22:35:13 +0000

    isInsufficientPermission to use fat arrow

--- extension/js/common/api/error/api-error.ts
+++ extension/js/common/api/error/api-error.ts
@@ -126,7 +126,7 @@ export class ApiErr {
     return e instanceof AjaxErr && e.status === 400;
   }
 
-  static isInsufficientPermission(e: any): e is AjaxErr {
+  static isInsufficientPermission = (e: any): e is AjaxErr => {
     return e instanceof AjaxErr && e.status === 403 && e.responseText.indexOf('insufficientPermissions') !== -1;
   }
 
