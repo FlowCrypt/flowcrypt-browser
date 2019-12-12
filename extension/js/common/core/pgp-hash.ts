8929aa72b55817c41610f2caddf3176bf99c1827
Tom J <human@flowcrypt.com>
2019-12-12 15:29:58 +0000

    style

--- extension/js/common/core/pgp-hash.ts
+++ extension/js/common/core/pgp-hash.ts
@@ -6,9 +6,11 @@ import { openpgp } from './pgp.js';
 import { Buf } from './buf.js';
 
 export class PgpHash {
+
   static sha1UtfStr = async (string: string): Promise<string> => {
     return openpgp.util.Uint8Array_to_hex(await openpgp.crypto.hash.digest(openpgp.enums.hash.sha1, Buf.fromUtfStr(string)));
   }
+
   static sha256UtfStr = async (string: string) => {
     return openpgp.util.Uint8Array_to_hex(await openpgp.crypto.hash.digest(openpgp.enums.hash.sha256, Buf.fromUtfStr(string)));
   }
@@ -16,13 +18,16 @@ export class PgpHash {
   static doubleSha1Upper = async (string: string) => {
     return (await PgpHash.sha1UtfStr(await PgpHash.sha1UtfStr(string))).toUpperCase();
   }
+
   static challengeAnswer = async (answer: string) => {
     return await PgpHash.cryptoHashSha256Loop(answer);
   }
+
   private static cryptoHashSha256Loop = async (string: string, times = 100000) => {
     for (let i = 0; i < times; i++) {
       string = await PgpHash.sha256UtfStr(string);
     }
     return string;
   }
+
 }
