d102503c81e47336098484191b3fe3eb56cbde15
Michael <49104848+michael-volynets@users.noreply.github.com>
2019-12-17 18:40:34 +0200

    Issue #2048, check using attester and add to DB (#2385)
    * Issue #2048, check using attester and add to DB
    
    * Send requests to Attester parallelly
    
    * Do not send requests if failed before

--- extension/chrome/elements/composer/composer-recipients.ts
+++ extension/chrome/elements/composer/composer-recipients.ts
@@ -22,6 +22,8 @@ import { ApiErr } from '../../../js/common/api/error/api-error.js';
 import { PgpKey, Contact } from '../../../js/common/core/pgp-key.js';
 
 export class ComposerRecipients extends ComposerComponent {
+  private readonly failedLookupEmails: string[] = [];
+
   private addedRecipients: RecipientElement[] = [];
   private BTN_LOADING = 'Loading..';
 
@@ -492,17 +494,22 @@ export class ComposerRecipients extends ComposerComponent {
 
   private renderAndAddToDBAPILoadedContacts = async (input: JQuery<HTMLElement>, contacts: Contact[]) => {
     if (contacts.length) {
+      const updatePromises: Promise<void>[] = [];
       for (const contact of contacts) {
         const [inDb] = await Store.dbContactGet(undefined, [contact.email]);
-        if (!inDb) {
-          await Store.dbContactSave(undefined, await Store.dbContactObj({
-            email: contact.email, name: contact.name, pendingLookup: true, lastUse: contact.last_use
-          }));
-        } else if (!inDb.name && contact.name) {
+        if (!inDb && !this.failedLookupEmails.includes(contact.email)) {
+          updatePromises.push((async () => {
+            const lookupRes = await this.composer.storage.lookupPubkeyFromDbOrKeyserverAndUpdateDbIfneeded(contact.email);
+            if (lookupRes === 'fail') {
+              this.failedLookupEmails.push(contact.email);
+            }
+          })());
+        } else if (!inDb?.name && contact.name) {
           const toUpdate = { name: contact.name };
           await Store.dbContactUpdate(undefined, contact.email, toUpdate);
         }
       }
+      await Promise.all(updatePromises);
       await this.searchContacts(input, true);
     }
   }
