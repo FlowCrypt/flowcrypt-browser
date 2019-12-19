41f5ba01cb94b79e9de53fc78d65c15375fa0208
Tom J <tom@holub.me>
2019-12-19 09:01:58 +0000

    fix pretest

--- test/source/platform/xss.ts
+++ test/source/platform/xss.ts
@@ -7,7 +7,7 @@ export class Xss {
   /**
    * used whenever untrusted remote content (eg html email) is rendered, but we still want to preserve html
    */
-  public static htmlSanitizeKeepBasicTags = (dirtyHtml: string): string => {
+  public static htmlSanitizeKeepBasicTags = (dirtyHtml: string, imgHandling: 'IMG-DEL' | 'IMG-KEEP' | 'IMG-TO-LINK'): string => {
     throw new Error('not implemented / not needed by tests yet');
   }
 
