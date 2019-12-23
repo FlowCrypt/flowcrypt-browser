c27e1815688032ccfe7b951b37450701e0219e71
michael-volynets <michael.volynets@startupsoft.co>
2019-12-23 20:12:11 +0200

    Corrected test build

--- test/source/platform/util.ts
+++ test/source/platform/util.ts
@@ -15,3 +15,8 @@ export const base64encode = (binary: string): string => {
 export const base64decode = (b64tr: string): string => {
   return Buffer.from(b64tr, 'base64').toString('binary');
 };
+
+export const iso2022jpToUtf = (content: Buffer) => {
+  const decoder = new TextDecoder();
+  return decoder.decode(content);
+};
