7109cbdfa6599aebcf47769837db238664ef91c9
Tom J <tom@holub.me>
2020-01-27 19:12:06 +0000

    code style fix

--- extension/js/common/api/chrome.ts
+++ extension/js/common/api/chrome.ts
@@ -30,52 +30,64 @@ export const handleFatalErr = (reason: 'storage_undefined', error: Error) => {
   }
 };
 
-export const tabsQuery = (q: chrome.tabs.QueryInfo): Promise<chrome.tabs.Tab[]> => new Promise(resolve => chrome.tabs.query(q, resolve));
+export const tabsQuery = (q: chrome.tabs.QueryInfo): Promise<chrome.tabs.Tab[]> => {
+  return new Promise(resolve => chrome.tabs.query(q, resolve));
+};
 
-export const windowsCreate = (q: chrome.windows.CreateData): Promise<chrome.windows.Window | undefined> => new Promise(resolve => {
-  if (typeof chrome.windows !== 'undefined') {
-    chrome.windows.create(q, resolve);
-  } else {
-    Ui.modal.error('Your platform is not supported: browser does not support extension windows').catch(Catch.reportErr);
-  }
-});
+export const windowsCreate = (q: chrome.windows.CreateData): Promise<chrome.windows.Window | undefined> => {
+  return new Promise(resolve => {
+    if (typeof chrome.windows !== 'undefined') {
+      chrome.windows.create(q, resolve);
+    } else {
+      Ui.modal.error('Your platform is not supported: browser does not support extension windows').catch(Catch.reportErr);
+    }
+  });
+};
 
-export const storageLocalGet = (keys: string[]): Promise<Object> => new Promise((resolve, reject) => { // tslint:disable-line:ban-types
-  if (typeof chrome.storage === 'undefined') {
-    handleFatalErr('storage_undefined', new Error('storage is undefined'));
-  } else {
-    chrome.storage.local.get(keys, result => {
-      if (typeof result !== 'undefined') {
-        resolve(result);
-      } else if (chrome.runtime.lastError) {
-        reject(Store.errCategorize(chrome.runtime.lastError));
-      } else {
-        reject(new Error(`storageLocalGet(${keys.join(',')}) produced undefined result without an error`));
-      }
-    });
-  }
-});
+export const storageLocalGet = (keys: string[]): Promise<Object> => {
+  return new Promise((resolve, reject) => { // tslint:disable-line:ban-types
+    if (typeof chrome.storage === 'undefined') {
+      handleFatalErr('storage_undefined', new Error('storage is undefined'));
+    } else {
+      chrome.storage.local.get(keys, result => {
+        if (typeof result !== 'undefined') {
+          resolve(result);
+        } else if (chrome.runtime.lastError) {
+          reject(Store.errCategorize(chrome.runtime.lastError));
+        } else {
+          reject(new Error(`storageLocalGet(${keys.join(',')}) produced undefined result without an error`));
+        }
+      });
+    }
+  });
+};
 
-export const storageLocalGetAll = (): Promise<{ [key: string]: any }> => new Promise((resolve) => {
-  if (typeof chrome.storage === 'undefined') {
-    handleFatalErr('storage_undefined', new Error('storage is undefined'));
-  } else {
-    chrome.storage.local.get(resolve);
-  }
-});
+export const storageLocalGetAll = (): Promise<{ [key: string]: any }> => {
+  return new Promise((resolve) => {
+    if (typeof chrome.storage === 'undefined') {
+      handleFatalErr('storage_undefined', new Error('storage is undefined'));
+    } else {
+      chrome.storage.local.get(resolve);
+    }
+  });
+};
 
-export const storageLocalSet = (values: Object): Promise<void> => new Promise((resolve) => { // tslint:disable-line:ban-types
-  if (typeof chrome.storage === 'undefined') {
-    handleFatalErr('storage_undefined', new Error('storage is undefined'));
-  } else {
-    chrome.storage.local.set(values, resolve);
-  }
-});
+export const storageLocalSet = (values: Object): Promise<void> => {
+  return new Promise((resolve) => { // tslint:disable-line:ban-types
+    if (typeof chrome.storage === 'undefined') {
+      handleFatalErr('storage_undefined', new Error('storage is undefined'));
+    } else {
+      chrome.storage.local.set(values, resolve);
+    }
+  });
+};
 
-export const storageLocalRemove = (keys: string[]): Promise<void> => new Promise((resolve) => { // tslint:disable-line:ban-types
-  if (typeof chrome.storage === 'undefined') {
-    handleFatalErr('storage_undefined', new Error('storage is undefined'));
-  } else {
-    chrome.storage.local.remove(keys, resolve);
-  }
-});
+export const storageLocalRemove = (keys: string[]): Promise<void> => {
+  return new Promise((resolve) => { // tslint:disable-line:ban-types
+    if (typeof chrome.storage === 'undefined') {
+      handleFatalErr('storage_undefined', new Error('storage is undefined'));
+    } else {
+      chrome.storage.local.remove(keys, resolve);
+    }
+  });
+};
