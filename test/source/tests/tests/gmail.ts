47010c4b731831385937ff6b16c1e2ff2aea9d79
Tom J <tom@holub.me>
2020-01-28 11:40:19 +0000

    remove dated comments | close #2505

--- test/source/tests/tests/gmail.ts
+++ test/source/tests/tests/gmail.ts
@@ -147,21 +147,5 @@ export const defineGmailTests = (testVariant: TestVariant, testWithBrowser: Test
       expect(content).to.contain('STONE NEED REMAIN SLIDE DEPOSIT BRICK');
     }));
 
-    // const compose_frame = await gmail_page.get_frame(['compose.htm']);
-    // Task.compose_fill_message(compose_frame, 'human@flowcrypt.com', 'message from gmail');
-    // await compose_frame.wait_and_click('@action-send', {delay: 0.5});
-    // await gmail_page.wait_till_gone('@container-new-message');
-    // await gmail_page.wait_all('@webmail-notification'); // message sent
-    // assert(await gmail_page.read('@webmail-notification'), 'Your encrypted message has been sent.', 'gmail notifiaction message');
-    // await gmail_page.click('@webmail-notification');
-    // await gmail_page.wait_till_gone('@webmail-notification');
-    // log('tests:gmail:secure compose works from gmail + compose frame disappears + notification shows + notification disappears');
-
-    // google inbox - need to hover over the button first
-    // await gmail_page.goto('https://inbox.google.com');
-    // await gmail_page.wait_and_click('@action-secure-compose', 1);
-    // await gmail_page.wait('@container-new-message');
-    // log('gmail:tests:secure compose button (inbox.google.com)');
-
   }
 };
