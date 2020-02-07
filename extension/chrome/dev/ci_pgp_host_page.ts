6d60d20497af352b7018971301a5f625c34ea0b0
Tom J <tom@holub.me>
2020-02-07 22:09:58 +0000

    try dynamic tabId for ci

--- extension/chrome/dev/ci_pgp_host_page.ts
+++ extension/chrome/dev/ci_pgp_host_page.ts
@@ -5,11 +5,18 @@
 import { BrowserMsg } from '../../js/common/browser/browser-msg.js';
 import { Catch } from '../../js/common/platform/catch.js';
 import { PgpMsg } from '../../js/common/core/pgp-msg.js';
+import { Env } from '../../js/common/browser/env.js';
+
+/* eslint-disable max-len */
 
 Catch.try(async () => {
   const tabId = await BrowserMsg.requiredTabId();
-  console.log(tabId);
 
   BrowserMsg.addListener('pgpMsgDiagnosePubkeys', PgpMsg.diagnosePubkeys);
   BrowserMsg.listen(tabId);
+
+  let src = Env.getBaseUrl();
+  src += `/chrome/elements/pgp_block.htm?account_email=flowcrypt.compatibility%40gmail.com&frame_id=frame_yVMKFLRDiY&message=&has_password=___cu_false___&message_id=162275c819bcbf9b&sender_email=human%40flowcrypt.com&is_outgoing=___cu_false___`;
+  src += `&parentTabId=${encodeURIComponent(tabId)}`;
+  $('body').append(`<iframe src="${src}" frameborder="0"></iframe>`);
 })();
