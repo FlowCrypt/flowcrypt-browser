5ee0fa50be1c9d7529f704b887b17ce379cfe4c2
Tom J <human@flowcrypt.com>
2020-02-08 22:58:14 +0800

    issue #2486 rm old global store (#2543)

--- extension/js/background_page/migrations.ts
+++ extension/js/background_page/migrations.ts
@@ -2,16 +2,6 @@
 
 'use strict';
 
-import { Store } from '../common/platform/store.js';
-
 export const migrateGlobal = async () => {
-  const s = await Store.getGlobal(['cryptup_account_email', 'cryptup_account_uuid', 'cryptup_account_subscription']);
-  if (s.cryptup_account_email) {
-    // this migration should stay here until March 2019
-    // there used to be one global account, as of Dec 2019 each is treated separately
-    // if there is any account info stored globally, assign it to the appropriate account, then delete it from global storage
-    const acctEmail = s.cryptup_account_email.toLowerCase().trim();
-    await Store.setAcct(acctEmail, { uuid: s.cryptup_account_uuid || undefined, subscription: s.cryptup_account_subscription || undefined });
-    await Store.removeGlobal(['cryptup_account_email', 'cryptup_account_uuid', 'cryptup_account_subscription']);
-  }
+  // noop
 };
