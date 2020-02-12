bdd077d05b3201c6218b2caee7efbebb4f61903f
Tom J <human@flowcrypt.com>
2020-02-12 21:59:06 +0000

    actually escape the passed string to reflect comment

--- extension/chrome/dev/ci_pgp_host_page.ts
+++ extension/chrome/dev/ci_pgp_host_page.ts
@@ -4,6 +4,7 @@
 
 import { BrowserMsg } from '../../js/common/browser/browser-msg.js';
 import { Catch } from '../../js/common/platform/catch.js';
+import { Xss } from '../../js/common/platform/xss.js';
 import { PgpMsg } from '../../js/common/core/pgp-msg.js';
 import { Env } from '../../js/common/browser/env.js';
 
@@ -18,5 +19,5 @@ Catch.try(async () => {
   let src = Env.getBaseUrl();
   src += `/chrome/elements/pgp_block.htm?${location.search}`;
   src += `&parentTabId=${encodeURIComponent(tabId)}`;
-  $('body').append(`<iframe src="${src}" frameborder="0"></iframe>`); // xss-escaped
+  $('body').append(`<iframe src="${Xss.escape(src)}" frameborder="0"></iframe>`); // xss-escaped
 })();
