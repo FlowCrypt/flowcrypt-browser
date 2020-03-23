905c47e68aae72a51666837fbeba37853c7d710f
Limon Monte <limon.monte@gmail.com>
2020-03-24 00:10:41 +0200

    add data-test attrs

--- extension/js/common/browser/ui.ts
+++ extension/js/common/browser/ui.ts
@@ -212,6 +212,8 @@ export class Ui {
           iframe.onload = () => {
             iframe.focus();
           };
+          $(Swal.getContent()).data('test', 'dialog');
+          $(Swal.getCloseButton()).data('test', 'dialog-close');
         },
         onClose: () => {
           const urlWithoutPageParam = Url.removeParamsFromUrl(window.location.href, ['page']);
