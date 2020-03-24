1bd8085075a64ff23e49defcc3a6ffe9c27f5675
Tom J <tom@holub.me>
2020-03-24 16:26:00 +0000

    mini fp refactor

--- extension/chrome/settings/index.ts
+++ extension/chrome/settings/index.ts
@@ -400,7 +400,7 @@ View.run(class SettingsView extends View {
       const escapedPrimaryOrRm = (ki.primary) ? '(primary)' : `(<a href="#" class="action_remove_key" fingerprint="${escapedFp}">remove</a>)`;
       const escapedEmail = Xss.escape(Str.parseEmail(prv.users[0].userId ? prv.users[0].userId!.userid : '').email || '');
       const escapedLink = `<a href="#" data-test="action-show-key-${i}" class="action_show_key" page="modules/my_key.htm" addurltext="&fingerprint=${escapedFp}">${escapedEmail}</a>`;
-      const fpHtml = `fingerprint:&nbsp;<span class="good">${Xss.escape(Str.spaced(ki.fingerprint))}</span>`;
+      const fpHtml = `fingerprint:&nbsp;<span class="good">${Str.spaced(escapedFp)}</span>`;
       const space = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`;
       html += `<div class="row key-content-row key_${escapedFp}">`;
       html += `  <div class="col-sm-12">${escapedLink} from ${Xss.escape(date)}${space}${fpHtml}${space}${escapedPrimaryOrRm}</div>`;
