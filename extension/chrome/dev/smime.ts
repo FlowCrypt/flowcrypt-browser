0494f270c47c1c33190e9fecf74331b901d13658
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2020-03-26 13:16:48 +0100

    Clean up e-mail creating code

--- extension/chrome/dev/smime.ts
+++ extension/chrome/dev/smime.ts
@@ -6,6 +6,12 @@ import { Catch } from '../../js/common/platform/catch.js';
 
 import * as forge from 'node-forge';
 
+const HEADERS = `MIME-Version: 1.0
+Content-Type: application/pkcs7-mime; name="smime.p7m"; smime-type=enveloped-data
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename="smime.p7m"
+Content-Description: S/MIME Encrypted Message`;
+
 Catch.try(async () => {
   function wrap(string: string) {
     var lines = [];
@@ -24,22 +30,13 @@ Catch.try(async () => {
 
     const headers = $('#headers').val();
 
-    p7.content = forge.util.createBuffer(`${headers}
-
-    ${$('#content').val()}`);
+    p7.content = forge.util.createBuffer(headers + '\r\n\r\n' + $('#content').val());
 
     p7.encrypt();
 
     const derBuffer = forge.asn1.toDer(p7.toAsn1()).getBytes();
 
-    $('#email').val(`${headers}
-MIME-Version: 1.0
-Content-Type: application/pkcs7-mime; name="smime.p7m"; smime-type=enveloped-data
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment; filename="smime.p7m"
-Content-Description: S/MIME Encrypted Message
-
-${wrap(btoa(derBuffer))}`);
+    $('#email').val(headers + '\r\n' + HEADERS + '\r\n\r\n' + wrap(btoa(derBuffer)));
   }
 
   $("#encrypt").click(encrypt);
