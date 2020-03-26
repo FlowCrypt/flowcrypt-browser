aec301bf062b27544557a7091aed4d8c8e9b2643
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2020-03-26 14:40:56 +0100

    Use email-js-codec for wrapping lines; style fixes

--- extension/chrome/dev/smime.ts
+++ extension/chrome/dev/smime.ts
@@ -6,6 +6,8 @@ import { Catch } from '../../js/common/platform/catch.js';
 
 import * as forge from 'node-forge';
 
+import { BrowserWindow } from '../../js/common/browser/browser-window';
+
 const HEADERS = `MIME-Version: 1.0
 Content-Type: application/pkcs7-mime; name="smime.p7m"; smime-type=enveloped-data
 Content-Transfer-Encoding: base64
@@ -13,19 +15,14 @@ Content-Disposition: attachment; filename="smime.p7m"
 Content-Description: S/MIME Encrypted Message`;
 
 Catch.try(async () => {
-  function wrap(string: string) {
-    var lines = [];
-    var index = 0;
-    var length = string.length;
-    while (index < length) {
-      lines.push(string.slice(index, index += 76));
-    }
-    return lines.join('\r\n');
-  }
+  const mimeCodec: {
+    foldLines(text: string, maxLength: number, afterSpace: boolean): string;
+  } = (window as unknown as BrowserWindow)['emailjs-mime-codec']; // tslint:disable-line:no-unsafe-any
+  const wrap = (text: string) => mimeCodec.foldLines(text, 76, true);
 
-  function encrypt() {
-    var p7 = forge.pkcs7.createEnvelopedData();
-    var cert = forge.pki.certificateFromPem(String($('#cert').val()));
+  const encrypt = () => {
+    const p7 = forge.pkcs7.createEnvelopedData();
+    const cert = forge.pki.certificateFromPem(String($('#cert').val()));
     p7.addRecipient(cert);
 
     const headers = $('#headers').val();
@@ -37,7 +34,7 @@ Catch.try(async () => {
     const derBuffer = forge.asn1.toDer(p7.toAsn1()).getBytes();
 
     $('#email').val(headers + '\r\n' + HEADERS + '\r\n\r\n' + wrap(btoa(derBuffer)));
-  }
+  };
 
   $("#encrypt").click(encrypt);
 
