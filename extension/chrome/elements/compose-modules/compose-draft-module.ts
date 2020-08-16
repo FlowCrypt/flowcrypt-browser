cba02a304e3338dd46d4cb970adf4ccb204e98b8
Limon Monte <limon.monte@gmail.com>
2020-08-16 17:00:41 +0300

    the fix doesnt seem to be correct, undo it

--- extension/chrome/elements/compose-modules/compose-draft-module.ts
+++ extension/chrome/elements/compose-modules/compose-draft-module.ts
@@ -119,10 +119,8 @@ export class ComposeDraftModule extends ViewModule<ComposeView> {
         this.view.S.cached('send_btn_note').text('Saving');
         if (this.view.threadId) { // reply draft
           sendable.body['text/plain'] = `[flowcrypt:link:draft_reply:${this.view.threadId}]\n\n${sendable.body['text/plain'] || ''}`;
-          delete sendable.body['encrypted/buf'];
-        } else if (this.view.draftId) { // new message compose draft with known draftid
+        } else if (this.view.draftId) { // new message compose draft with known draftId
           sendable.body['text/plain'] = `[flowcrypt:link:draft_compose:${this.view.draftId}]\n\n${sendable.body['text/plain'] || ''}`;
-          delete sendable.body['encrypted/buf'];
         }
         const mimeMsg = await sendable.toMime();
         // If a draft was loaded from the local storage, once a user is back online, the local draft will be moved to the email provider
