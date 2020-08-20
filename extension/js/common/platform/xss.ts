163a4bd92eeb74bdf4465f9e666e61deb26cb1b4
seisvelas <alej.alvarado94@gmail.com>
2020-08-20 18:15:28 -0500

    deleted fingerprint from array by accident, readding

--- extension/js/common/platform/xss.ts
+++ extension/js/common/platform/xss.ts
@@ -17,7 +17,7 @@ export class Xss {
 
   private static ALLOWED_HTML_TAGS = ['p', 'div', 'br', 'u', 'i', 'em', 'b', 'ol', 'ul', 'pre', 'li', 'table', 'tr', 'td', 'th', 'img', 'h1', 'h2', 'h3', 'h4', 'h5',
     'h6', 'hr', 'address', 'blockquote', 'dl', 'fieldset', 'a', 'font'];
-  private static ADD_ATTR = ['email', 'page', 'addurltext', 'longid', 'index', 'target'];
+  private static ADD_ATTR = ['email', 'page', 'addurltext', 'longid', 'index', 'target', 'fingerprint'];
   private static FORBID_ATTR = ['background'];
   private static HREF_REGEX_CACHE: RegExp | undefined;
 
