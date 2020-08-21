198d14dcde9d18b8bcad9c9da57f1dbefa745c75
Alex Vazquez <44826516+seisvelas@users.noreply.github.com>
2020-08-21 05:35:47 -0500

    issue #2910 Prevent tabnabs in PGP MIME emails with Xss class (#2910)
    * Block tabnabbing in XSS class for PGP MIME messages
    
    * deleted fingerprint from array by accident, readding
    
    * Block tabnabbing in XSS class for PGP MIME messages
    
    * trailing whitespace

--- extension/js/common/platform/xss.ts
+++ extension/js/common/platform/xss.ts
@@ -106,7 +106,9 @@ export class Xss {
         }
       }
       if ('target' in node) { // open links in new window
-        (node as Element).setAttribute('target', '_blank');
+        (node as Element).setAttribute('target','_blank');
+        // prevents https://www.owasp.org/index.php/Reverse_Tabnabbing
+        (node as Element).setAttribute('rel', 'noopener noreferrer');
       }
     });
     const cleanHtml = DOMPurify.sanitize(dirtyHtml, {
