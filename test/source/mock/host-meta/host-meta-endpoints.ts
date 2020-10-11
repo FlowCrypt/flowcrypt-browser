c79201d860e656376c098b8e6fad197d8b0f2b90
Tom J <tom@holub.me>
2020-10-11 20:49:59 +0000

    status404

--- test/source/mock/host-meta/host-meta-endpoints.ts
+++ test/source/mock/host-meta/host-meta-endpoints.ts
@@ -4,7 +4,10 @@ import { HttpClientErr } from '../lib/api';
 import { HandlersDefinition } from '../all-apis-mock';
 
 export const mockWellKnownHostMetaEndpoints: HandlersDefinition = {
-  '/.well-known/host-meta.json?local=err500': async ({ body }, req) => {
+  '/.well-known/host-meta.json?local=status500': async () => {
     throw new Error(`Intentional host meta 500 - ignored on consumer but noticed by enterprise`);
+  },
+  '/.well-known/host-meta.json?local=status404': async () => {
+    throw new HttpClientErr(`Not Found`, 404);
   },
 };
