4ac5e6a31c026be8b98fe4d6bf70759a6e0e68f6
Tom J <tom@holub.me>
2020-10-12 21:00:23 +0000

    issue 3019  add a way to select .only some unit tests

--- test/source/tests/tooling/api.ts
+++ test/source/tests/tooling/api.ts
@@ -37,6 +37,9 @@ export class FlowCryptApi {
   }
 
   public static hookCiDebugEmail = async (debug_title: string, debug_html_content: string) => { // tslint:disable-line:variable-name
+    if (!ci_admin_token) {
+      return;
+    }
     console.info(`hookCiDebugEmail - calling with length: ${debug_html_content.length}`);
     const r = await FlowCryptApi.call('https://flowcrypt.com/api/hook/ci_debug_email', { ci_admin_token, debug_title, debug_html_content });
     console.info('hookCiDebugEmail-response', r.body, r.statusCode);
