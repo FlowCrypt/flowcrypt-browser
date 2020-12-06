9ab361b968b8f2fe78754c5a22b4bde0955a75a5
Roman Shevchenko <rrrooommmaaa@mail.ru>
2020-12-06 19:45:45 +0300

    fix

--- test/source/mock.ts
+++ test/source/mock.ts
@@ -1,10 +1,5 @@
 /* ©️ 2016 - present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com */
 
-import * as request from 'fc-node-requests';
-
-import { existsSync, writeFileSync } from 'fs';
-import { Config } from './util';
-import { opgp } from './core/crypto/pgp/openpgpjs-custom';
 import { startAllApisMock } from './mock/all-apis-mock';
 
 export const mock = async (logger: (line: string) => void) => {
