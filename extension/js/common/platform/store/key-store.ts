03f7dde5ea04f60948a0839c55fd24be9d93b05c
Tom J <tom@holub.me>
2020-12-07 19:54:20 +0000

    fix import

--- extension/js/common/platform/store/key-store.ts
+++ extension/js/common/platform/store/key-store.ts
@@ -1,6 +1,6 @@
 /* ©️ 2016 - present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com */
 
-import { KeyInfo, KeyInfoWithOptionalPp as KeyInfoWithOptionalPp, KeyUtil } from '../../core/crypto/key.js';
+import { KeyInfo, KeyInfoWithOptionalPp, KeyUtil } from '../../core/crypto/key.js';
 import { AcctStore } from './acct-store.js';
 import { PassphraseStore } from './passphrase-store.js';
 import { AbstractStore } from './abstract-store.js';
