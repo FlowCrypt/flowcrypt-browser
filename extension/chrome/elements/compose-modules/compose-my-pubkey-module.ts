a989eadee84e1fc76f568fa806e73dac718ae4b8
Limon Monte <limon.monte@protonmail.com>
2020-12-23 01:18:44 +0200

    rm unused catch

--- extension/chrome/elements/compose-modules/compose-my-pubkey-module.ts
+++ extension/chrome/elements/compose-modules/compose-my-pubkey-module.ts
@@ -3,7 +3,6 @@
 'use strict';
 
 import { ApiErr } from '../../../js/common/api/shared/api-error.js';
-import { Catch } from '../../../js/common/platform/catch.js';
 import { KeyInfo } from '../../../js/common/core/crypto/key.js';
 import { Lang } from '../../../js/common/lang.js';
 import { Ui } from '../../../js/common/browser/ui.js';
