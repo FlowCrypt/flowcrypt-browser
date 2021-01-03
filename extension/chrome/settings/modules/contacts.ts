4deb81c99a315f71e81e559f057901023dd711bb
Roman Shevchenko <rrrooommmaaa@mail.ru>
2021-01-03 09:18:16 -0500

    Export all pubkeys now ignores filters

--- extension/chrome/settings/modules/contacts.ts
+++ extension/chrome/settings/modules/contacts.ts
@@ -109,8 +109,7 @@ View.run(class ContactsView extends View {
   }
 
   private actionExportAllKeysHandler = async () => {
-    const allArmoredPublicKeys = (await ContactStore.searchPubkeys(undefined, { has_pgp: true, limit: 500, substring: String($('.input-search-contacts').val()) })).
-      map(a => a!.trim()).join('\n');
+    const allArmoredPublicKeys = (await ContactStore.searchPubkeys(undefined, { has_pgp: true })).map(a => a!.trim()).join('\n');
     const exportFile = new Attachment({ name: 'public-keys-export.asc', type: 'application/pgp-keys', data: Buf.fromUtfStr(allArmoredPublicKeys) });
     Browser.saveToDownloads(exportFile);
   }
