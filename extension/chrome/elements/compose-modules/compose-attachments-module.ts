bc0fa03ab22bd33211e9de72ef0ed5a7e8318631
Alex Vazquez <44826516+seisvelas@users.noreply.github.com>
2021-03-10 16:24:02 -0700

    Remove 5mb limit notice (leave only 25mb notice)

--- extension/chrome/elements/compose-modules/compose-attachments-module.ts
+++ extension/chrome/elements/compose-modules/compose-attachments-module.ts
@@ -30,46 +30,15 @@ export class ComposeAttachmentsModule extends ViewModule<ComposeView> {
   }
 
   private getMaxAttachmentSizeAndOversizeNotice = async (): Promise<AttachmentLimits> => {
-    const subscription = await AcctStore.getSubscription(this.view.acctEmail);
-    if (!subscription.active && !OrgRules.isPublicEmailProviderDomain(this.view.senderModule.getSender())) {
-      return {
-        sizeMb: 5,
-        size: 5 * 1024 * 1024,
-        count: 10,
-        oversize: async () => {
-          let getAdvanced = 'The files are over 5 MB. Advanced users can send files up to 25 MB.';
-          if (!subscription.method) {
-            getAdvanced += '\n\nTry it free for 30 days.';
-          } else if (subscription.method === 'trial') {
-            getAdvanced += '\n\nYour trial has expired, please consider supporting our efforts by upgrading.';
-          } else if (subscription.method === 'group') {
-            getAdvanced += '\n\nGroup billing is due for renewal. Please check with your leadership.';
-          } else if (subscription.method === 'stripe') {
-            getAdvanced += '\n\nPlease renew your subscription to continue sending large files.';
-          } else {
-            getAdvanced += '\n\nClick ok to see subscribe options.';
-          }
-          if (subscription.method === 'group') {
-            await Ui.modal.info(getAdvanced);
-          } else {
-            if (await Ui.modal.confirm(getAdvanced)) {
-              BrowserMsg.send.subscribeDialog(this.view.parentTabId, {});
-            }
-          }
-          return;
-        },
-      };
-    } else {
-      const sizeMb = 25;
-      return {
-        sizeMb,
-        size: sizeMb * 1024 * 1024,
-        count: 10,
-        oversize: async (combinedSize: number) => {
-          await Ui.modal.warning('Combined attachment size is limited to 25 MB. The last file brings it to ' + Math.ceil(combinedSize / (1024 * 1024)) + ' MB.');
-        },
-      };
-    }
+    const sizeMb = 25;
+    return {
+      sizeMb,
+      size: sizeMb * 1024 * 1024,
+      count: 10,
+      oversize: async (combinedSize: number) => {
+        await Ui.modal.warning('Combined attachment size is limited to 25 MB. The last file brings it to ' + Math.ceil(combinedSize / (1024 * 1024)) + ' MB.');
+      },
+    };
   }
 
 }
