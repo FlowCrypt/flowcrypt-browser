9b98db2976334e42838d21587fc193135e245b6b
Limon Monte <limon.monte@gmail.com>
2021-04-28 16:39:01 +0300

    3s sleep is enough

--- test/source/tests/decrypt.ts
+++ test/source/tests/decrypt.ts
@@ -329,12 +329,12 @@ export const defineDecryptTests = (testVariant: TestVariant, testWithBrowser: Te
       await BrowserRecipe.pgpBlockVerifyDecryptedContent(t, browser, { params: url, content: ['1234'], signature });
     }));
 
-    ava.default('decrypt - unsigned encrypted message', testWithBrowser('compatibility', async (t, browser) => {
+    ava.default.only('decrypt - unsigned encrypted message', testWithBrowser('compatibility', async (t, browser) => {
       const threadId = '17918a9d7ca2fbac';
       const acctEmail = 'flowcrypt.compatibility@gmail.com';
       const inboxPage = await browser.newPage(t, TestUrls.extension(`chrome/settings/inbox/inbox.htm?acctEmail=${acctEmail}&threadId=${threadId}`));
       await inboxPage.waitAll('iframe', { timeout: 2 });
-      const urls = await inboxPage.getFramesUrls(['/chrome/elements/pgp_block.htm'], { sleep: 10, appearIn: 20 });
+      const urls = await inboxPage.getFramesUrls(['/chrome/elements/pgp_block.htm'], { sleep: 3 });
       expect(urls.length).to.equal(1);
       const url = urls[0].split('/chrome/elements/pgp_block.htm')[1];
       const signature = ['Message Not Signed'];
