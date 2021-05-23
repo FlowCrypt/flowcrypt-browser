3a6773cd61be36ab5e54ba7435fd215422cd2821
Tom J <6306961+tomholub@users.noreply.github.com>
2021-05-23 19:41:36 +0800

    issue #3596 refactored ekm integration code (#3670)
    * issue 3596 refactored ekm integration code
    
    * renamed method
    
    Co-authored-by: Tom J <tom@holub.me>

--- extension/chrome/settings/setup/setup-key-manager-autogen.ts
+++ extension/chrome/settings/setup/setup-key-manager-autogen.ts
@@ -14,59 +14,30 @@ import { Settings } from '../../../js/common/settings.js';
 import { KeyUtil } from '../../../js/common/core/crypto/key.js';
 import { OpenPGPKey } from '../../../js/common/core/crypto/pgp/openpgp-key.js';
 
-export class SetupKeyManagerAutogenModule {
+export class SetupWithEmailKeyManagerModule {
 
   constructor(private view: SetupView) {
   }
 
-  public getKeyFromKeyManagerOrAutogenAndStoreItThenRenderSetupDone = async () => {
+  public setupWithEkmThenRenderSetupDone = async () => {
     if (!this.view.orgRules.mustAutogenPassPhraseQuietly()) {
       const notSupportedErr = 'Combination of org rules not yet supported: PRV_AUTOIMPORT_OR_AUTOGEN cannot yet be used without PASS_PHRASE_QUIET_AUTOGEN.';
       await Ui.modal.error(`${notSupportedErr}\n\nPlease write human@flowcrypt.com to add support.`);
       window.location.href = Url.create('index.htm', { acctEmail: this.view.acctEmail });
       return;
     }
-    const keygenAlgo = this.view.orgRules.getEnforcedKeygenAlgo();
-    if (!keygenAlgo) {
-      const notSupportedErr = 'Combination of org rules not yet supported: PRV_AUTOIMPORT_OR_AUTOGEN cannot yet be used without enforce_keygen_algo.';
-      await Ui.modal.error(`${notSupportedErr}\n\nPlease write human@flowcrypt.com to add support.`);
-      window.location.href = Url.create('index.htm', { acctEmail: this.view.acctEmail });
-      return;
-    }
     const passphrase = PgpPwd.random(); // mustAutogenPassPhraseQuietly
-    const opts: SetupOptions = { passphrase_save: true, submit_main: this.view.orgRules.canSubmitPubToAttester(), submit_all: false, passphrase };
+    const setupOptions: SetupOptions = { passphrase_save: true, submit_main: this.view.orgRules.canSubmitPubToAttester(), submit_all: false, passphrase };
     try {
       const { privateKeys } = await this.view.keyManager!.getPrivateKeys(this.view.idToken!);
-      if (privateKeys.length) { // keys already exist on keyserver, auto-import
-        const { keys } = await KeyUtil.readMany(Buf.fromUtfStr(privateKeys.map(pk => pk.decryptedPrivateKey).join('\n')));
-        if (!keys.length) {
-          throw new Error(`Could not parse any valid keys from Key Manager response for user ${this.view.acctEmail}`);
-        }
-        for (const prv of keys) {
-          if (!prv.isPrivate) {
-            throw new Error(`Key ${prv.id} for user ${this.view.acctEmail} is not a private key`);
-          }
-          if (!prv.fullyDecrypted) {
-            throw new Error(`Key ${prv.id} for user ${this.view.acctEmail} from FlowCrypt Email Key Manager is not fully decrypted`);
-          }
-          await KeyUtil.encrypt(prv, passphrase);
-        }
-        await this.view.saveKeysAndPassPhrase(keys, opts);
-      } else { // generate keys and store them on key manager
-        const { full_name } = await AcctStore.get(this.view.acctEmail, ['full_name']);
-        const expireInMonths = this.view.orgRules.getEnforcedKeygenExpirationMonths();
-        const pgpUids = [{ name: full_name || '', email: this.view.acctEmail }];
-        const generated = await OpenPGPKey.create(pgpUids, keygenAlgo, passphrase, expireInMonths);
-        const decryptablePrv = await KeyUtil.parse(generated.private);
-        if (! await KeyUtil.decrypt(decryptablePrv, passphrase)) {
-          throw new Error('Unexpectedly cannot decrypt newly generated key');
-        }
-        const pubArmor = KeyUtil.armor(await KeyUtil.asPublicKey(decryptablePrv));
-        const storePrvOnKm = async () => this.view.keyManager!.storePrivateKey(this.view.idToken!, KeyUtil.armor(decryptablePrv), pubArmor);
-        await Settings.retryUntilSuccessful(storePrvOnKm, 'Failed to store newly generated key on FlowCrypt Email Key Manager');
-        await this.view.saveKeysAndPassPhrase([await KeyUtil.parse(generated.private)], opts); // store encrypted key + pass phrase locally
+      if (privateKeys.length) {
+        // keys already exist on keyserver, auto-import
+        await this.processAndStoreKeysFromEkmLocally(privateKeys, setupOptions);
+      } else {
+        // generate keys on client and store them on key manager
+        await this.autoGenerateKeyAndStoreBothLocallyAndToEkm(setupOptions);
       }
-      await this.view.submitPublicKeysAndFinalizeSetup(opts);
+      await this.view.submitPublicKeysAndFinalizeSetup(setupOptions);
       await this.view.setupRender.renderSetupDone();
     } catch (e) {
       if (ApiErr.isNetErr(e) && await Api.isInternetAccessible()) { // frendly message when key manager is down, helpful during initial infrastructure setup
@@ -74,6 +45,45 @@ export class SetupKeyManagerAutogenModule {
       }
       throw e;
     }
+  }
+
+  private processAndStoreKeysFromEkmLocally = async (privateKeys: { decryptedPrivateKey: string }[], setupOptions: SetupOptions) => {
+    const { keys } = await KeyUtil.readMany(Buf.fromUtfStr(privateKeys.map(pk => pk.decryptedPrivateKey).join('\n')));
+    if (!keys.length) {
+      throw new Error(`Could not parse any valid keys from Key Manager response for user ${this.view.acctEmail}`);
+    }
+    for (const prv of keys) {
+      if (!prv.isPrivate) {
+        throw new Error(`Key ${prv.id} for user ${this.view.acctEmail} is not a private key`);
+      }
+      if (!prv.fullyDecrypted) {
+        throw new Error(`Key ${prv.id} for user ${this.view.acctEmail} from FlowCrypt Email Key Manager is not fully decrypted`);
+      }
+      await KeyUtil.encrypt(prv, setupOptions.passphrase);
+    }
+    await this.view.saveKeysAndPassPhrase(keys, setupOptions);
+  }
+
+  private autoGenerateKeyAndStoreBothLocallyAndToEkm = async (setupOptions: SetupOptions) => {
+    const keygenAlgo = this.view.orgRules.getEnforcedKeygenAlgo();
+    if (!keygenAlgo) {
+      const notSupportedErr = 'Combination of org rules not yet supported: PRV_AUTOIMPORT_OR_AUTOGEN cannot yet be used without enforce_keygen_algo.';
+      await Ui.modal.error(`${notSupportedErr}\n\nPlease write human@flowcrypt.com to add support.`);
+      window.location.href = Url.create('index.htm', { acctEmail: this.view.acctEmail });
+      return;
+    }
+    const { full_name } = await AcctStore.get(this.view.acctEmail, ['full_name']);
+    const expireInMonths = this.view.orgRules.getEnforcedKeygenExpirationMonths();
+    const pgpUids = [{ name: full_name || '', email: this.view.acctEmail }];
+    const generated = await OpenPGPKey.create(pgpUids, keygenAlgo, setupOptions.passphrase, expireInMonths);
+    const decryptablePrv = await KeyUtil.parse(generated.private);
+    if (! await KeyUtil.decrypt(decryptablePrv, setupOptions.passphrase)) {
+      throw new Error('Unexpectedly cannot decrypt newly generated key');
+    }
+    const pubArmor = KeyUtil.armor(await KeyUtil.asPublicKey(decryptablePrv));
+    const storePrvOnKm = async () => this.view.keyManager!.storePrivateKey(this.view.idToken!, KeyUtil.armor(decryptablePrv), pubArmor);
+    await Settings.retryUntilSuccessful(storePrvOnKm, 'Failed to store newly generated key on FlowCrypt Email Key Manager');
+    await this.view.saveKeysAndPassPhrase([await KeyUtil.parse(generated.private)], setupOptions); // store encrypted key + pass phrase locally
   }
 
 }
