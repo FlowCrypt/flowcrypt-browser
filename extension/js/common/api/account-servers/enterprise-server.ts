846e403fc9e1c7f7ce05c020d948018284954f9e
martgil <mart.robles.it@gmail.com>
2021-07-29 16:55:49 +0800

    added the previous FES comment

--- extension/js/common/api/account-servers/enterprise-server.ts
+++ extension/js/common/api/account-servers/enterprise-server.ts
@@ -21,6 +21,11 @@ export namespace FesRes {
   export type ServiceInfo = { vendor: string, service: string, orgId: string, version: string, apiVersion: string }
 }
 
+/**
+ * FlowCrypt Enterprise Server (FES) may be deployed on-prem by enterprise customers.
+ * This gives them more control. All OrgRules, log collectors, etc (as implemented) would then be handled by the FES.
+ * Once fully integrated, this will allow customers to be fully independent of flowcrypt.com/api
+ */
 // ts-prune-ignore-next
 export class EnterpriseServer extends Api {
 
