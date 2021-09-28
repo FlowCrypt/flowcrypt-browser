fcb096549625ca5302cd353b0e506a2bc3200804
Tom J <6306961+tomholub@users.noreply.github.com>
2021-09-28 21:30:37 +0800

    issue #4018 generic InMemoryStore (#4019)
    * issue 4018 generic InMemoryStore
    
    * fix typo
    
    * add passphrase_ to in-memory index
    
    Co-authored-by: Tom J <tom@holub.me>

--- extension/js/background_page/background_page.ts
+++ extension/js/background_page/background_page.ts
@@ -25,8 +25,8 @@ opgp.initWorker({ path: '/lib/openpgp.worker.js' });
 
   let db: IDBDatabase;
   let storage: GlobalStoreDict;
-  const inMemoryPassPhrases = new ExpirationCache(4 * 60 * 60 * 1000); // 4 hours
-  Catch.setHandledInterval(() => inMemoryPassPhrases.deleteExpired(), 60000); // each minute
+  const inMemoryStore = new ExpirationCache(4 * 60 * 60 * 1000); // 4 hours
+  Catch.setHandledInterval(() => inMemoryStore.deleteExpired(), 60000); // each minute
 
   try {
     await migrateGlobal();
@@ -54,8 +54,8 @@ opgp.initWorker({ path: '/lib/openpgp.worker.js' });
 
   // storage related handlers
   BrowserMsg.bgAddListener('db', (r: Bm.Db) => BgHandlers.dbOperationHandler(db, r));
-  BrowserMsg.bgAddListener('session_passphrase_set', async (r: Bm.StoreSessionSet) => inMemoryPassPhrases.set(emailKeyIndex(r.acctEmail, r.key), r.value));
-  BrowserMsg.bgAddListener('session_passphrase_get', async (r: Bm.StoreSessionGet) => inMemoryPassPhrases.get(emailKeyIndex(r.acctEmail, r.key)));
+  BrowserMsg.bgAddListener('inMemoryStoreSet', async (r: Bm.InMemoryStoreSet) => inMemoryStore.set(emailKeyIndex(r.acctEmail, r.key), r.value));
+  BrowserMsg.bgAddListener('inMemoryStoreGet', async (r: Bm.InMemoryStoreGet) => inMemoryStore.get(emailKeyIndex(r.acctEmail, r.key)));
   BrowserMsg.bgAddListener('storeGlobalGet', (r: Bm.StoreGlobalGet) => GlobalStore.get(r.keys));
   BrowserMsg.bgAddListener('storeGlobalSet', (r: Bm.StoreGlobalSet) => GlobalStore.set(r.values));
   BrowserMsg.bgAddListener('storeAcctGet', (r: Bm.StoreAcctGet) => AcctStore.get(r.acctEmail, r.keys));
