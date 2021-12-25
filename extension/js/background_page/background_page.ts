5d4cb360dbe9b3c7d01b512224792723a50ccbb8
Roman Shevchenko <rrrooommmaaa@mail.ru>
2021-12-25 15:50:15 +0000

    added migration procedure to update searchables in indexedDB

--- extension/js/background_page/background_page.ts
+++ extension/js/background_page/background_page.ts
@@ -9,7 +9,7 @@ import { Catch } from '../common/platform/catch.js';
 import { GoogleAuth } from '../common/api/email-provider/gmail/google-auth.js';
 import { VERSION } from '../common/core/const.js';
 import { injectFcIntoWebmail } from './inject.js';
-import { migrateGlobal, moveContactsToEmailsAndPubkeys, updateOpgpRevocations, updateX509FingerprintsAndLongids } from './migrations.js';
+import { extendSearchables, migrateGlobal, moveContactsToEmailsAndPubkeys, updateOpgpRevocations, updateX509FingerprintsAndLongids } from './migrations.js';
 import { opgp } from '../common/core/crypto/pgp/openpgpjs-custom.js';
 import { GlobalStoreDict, GlobalStore } from '../common/platform/store/global-store.js';
 import { ContactStore } from '../common/platform/store/contact-store.js';
@@ -46,6 +46,7 @@ opgp.initWorker({ path: '/lib/openpgp.worker.js' });
     db = await ContactStore.dbOpen(); // takes 4-10 ms first time
     await updateOpgpRevocations(db);
     await updateX509FingerprintsAndLongids(db);
+    await extendSearchables(db);
     await moveContactsToEmailsAndPubkeys(db);
   } catch (e) {
     await BgUtils.handleStoreErr(e);
