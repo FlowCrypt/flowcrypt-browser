451e2b12d689d9be94b91bace1abb8e48669f7c6
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2022-02-17 23:38:01 +0200

    update test

--- extension/chrome/elements/add_pubkey.ts
+++ extension/chrome/elements/add_pubkey.ts
@@ -81,12 +81,14 @@ View.run(class AddPubkeyView extends View {
       const [contact] = await ContactStore.get(undefined, [String($(fromSelect).val())]);
       if (contact?.pubkey) {
         $('.pubkey').val('').prop('disabled', true).prop('style', 'display: none;');
+        $('#manual-import-warning').prop('style', 'display: none;');
       } else {
         Catch.report('Contact unexpectedly not found when copying pubkey by email in add_pubkey.htm');
         await Ui.modal.error('Contact not found.');
       }
     } else {
       $('.pubkey').val('').prop('disabled', false).prop('style', 'display: inline;');
+      $('#manual-import-warning').prop('style', 'display: inline;');
     }
   };
 
