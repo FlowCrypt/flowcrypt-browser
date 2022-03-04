663fc4bbaf0c8e98db57c1088b1034b7dd83a67f
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2022-03-05 01:03:57 +0200

    wip

--- test/source/tests/browser-unit-tests/unit-ContactStore.js
+++ test/source/tests/browser-unit-tests/unit-ContactStore.js
@@ -286,7 +286,7 @@ BROWSER_UNIT_TEST_NAME(`ContactStore gets a valid pubkey by e-mail and all pubke
   return 'pass';
 })();
 
-// This ine gives error:
+// This one gives error:
 // Error: Failed to extract pubkey 16BB407403A3ADC55E1E0E4AF93EEC8FB187C923
 // no idea why
 BROWSER_UNIT_TEST_NAME(`ContactStore stores postfixed fingerprint internally for X.509 certificate`);
