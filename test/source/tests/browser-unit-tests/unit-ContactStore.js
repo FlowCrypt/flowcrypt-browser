36077b4d6dabeb181bf4cd546c5b411eba4e8957
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2022-03-08 00:19:40 +0200

    wip

--- test/source/tests/browser-unit-tests/unit-ContactStore.js
+++ test/source/tests/browser-unit-tests/unit-ContactStore.js
@@ -259,6 +259,21 @@ BROWSER_UNIT_TEST_NAME(`ContactStore.update tests`);
   return 'pass';
 })();
 
+BROWSER_UNIT_TEST_NAME(`ContactStore saves and returns dates as numbers`);
+(async () => {
+  // we'll use background operation to make sure the date isn't transformed on its way
+  const email = 'test@expired.com';
+  const pubkeyLastCheck = Date.now();
+  const lastUse = pubkeyLastCheck + 1000;
+  await ContactStore.update(undefined, email, { pubkey: testConstants.expiredPub, pubkeyLastCheck, lastUse });
+  const loaded = await ContactStore.getOneWithAllPubkeys(undefined, email);
+  if (typeof loaded.sortedPubkeys[0].lastCheck !== 'number') {
+    throw Error(
+      'pubkeyLastCheck was expected to be a number, ' +
+      `but got ${typeof loaded.sortedPubkeys[0].lastCheck}`);
+  }
+})();
+
 BROWSER_UNIT_TEST_NAME(`ContactStore gets a valid pubkey by e-mail and all pubkeys with getOneWithAllPubkeys()`);
 (async () => {
   // Note 1: email differs from pubkey id
