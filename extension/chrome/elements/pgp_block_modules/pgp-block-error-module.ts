59784e54dc8fe0791da984b8a414094746210f5a
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2022-04-18 13:01:54 +0300

    wip

--- extension/chrome/elements/pgp_block_modules/pgp-block-error-module.ts
+++ extension/chrome/elements/pgp_block_modules/pgp-block-error-module.ts
@@ -54,8 +54,8 @@ export class PgpBlockViewErrorModule {
       BrowserMsg.send.notificationShowAuthPopupNeeded(this.view.parentTabId, { acctEmail: this.view.acctEmail });
       await this.renderErr(`Could not load message due to missing auth. ${Ui.retryLink()}`, undefined);
     } else if (e instanceof FormatError) {
-      await this.renderErr(Lang.pgpBlock.cantOpen + Lang.pgpBlock.badFormat + `Details: ${e.message}. `
-        + Lang.pgpBlock.dontKnowHowOpen(!!this.view.fesUrl), e.data);
+      await this.renderErr(Lang.pgpBlock.cantOpen + Lang.pgpBlock.badFormat + Lang.pgpBlock.details
+        + e.message  + ' ' + Lang.pgpBlock.dontKnowHowOpen(!!this.view.fesUrl), e.data);
     } else if (ApiErr.isInPrivateMode(e)) {
       await this.renderErr(`FlowCrypt does not work in a Firefox Private Window (or when Firefox Containers are used). Please try in a standard window.`, undefined);
     } else {
