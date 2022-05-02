0fcc7c0003184b9fdc4d8947edaeba447007bcb3
martgil <46025304+martgil@users.noreply.github.com>
2022-05-02 18:56:29 +0800

    update

--- extension/js/common/api/email-provider/gmail/gmail.ts
+++ extension/js/common/api/email-provider/gmail/gmail.ts
@@ -23,7 +23,6 @@ import { PgpArmor } from '../../../core/crypto/pgp/pgp-armor.js';
 import { SendableMsg } from '../sendable-msg.js';
 import { Xss } from '../../../platform/xss.js';
 import { KeyStore } from '../../../platform/store/key-store.js';
-import { base64decode } from '../../../../../js/common/platform/util.js';
 
 export type GmailResponseFormat = 'raw' | 'full' | 'metadata';
 
@@ -305,7 +304,7 @@ export class Gmail extends EmailProviderApi implements EmailProviderInterface {
       if (plaintext) {
         return { armored: '', plaintext, subject, isPwdMsg };
       }
-      throw new FormatError('Armored message not found', base64decode(textBody || htmlBody));
+      throw new FormatError('Armored message not found', textBody || htmlBody);
     } else { // format === raw
       const mimeMsg = Buf.fromBase64UrlStr(gmailMsg.raw!);
       const decoded = await Mime.decode(mimeMsg);
