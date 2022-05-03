d2fa2e93e3921ee08cbe4cdf0b3a10a2675fce1a
Tom J <tom@holub.me>
2022-05-03 18:34:09 +0000

    fix

--- tooling/fill-values.ts
+++ tooling/fill-values.ts
@@ -16,7 +16,7 @@ const replaceables: { needle: RegExp, val: string }[] = [
   { needle: /\[BUILD_REPLACEABLE_FLAVOR\]/g, val: 'consumer' },
   { needle: /\[BUILD_REPLACEABLE_OAUTH_GOOGLE_API_HOST\]/g, val: 'https://www.googleapis.com' },
   { needle: /\[BUILD_REPLACEABLE_GMAIL_GOOGLE_API_HOST\]/g, val: 'https://www.googleapis.com' },
-  { needle: /\[BUILD_REPLACEABLE_PEOPLE_API_HOST\]/g, val: 'https://www.googleapis.com' },
+  { needle: /\[BUILD_REPLACEABLE_PEOPLE_GOOGLE_API_HOST\]/g, val: 'https://www.googleapis.com' },
   { needle: /\[BUILD_REPLACEABLE_GOOGLE_OAUTH_SCREEN_HOST\]/g, val: 'https://accounts.google.com' },
   { needle: /\[BUILD_REPLACEABLE_BACKEND_API_HOST\]/g, val: 'https://flowcrypt.com/api/' },
   { needle: /\[BUILD_REPLACEABLE_ATTESTER_API_HOST\]/g, val: 'https://flowcrypt.com/attester/' },
