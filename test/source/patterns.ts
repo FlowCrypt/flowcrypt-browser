e418f741a6dc0863e2eb7a1459de6ba3eec17c89
tomholub <6306961+tomholub@users.noreply.github.com>
2022-05-03 21:09:41 +0200

    fix tests

--- test/source/patterns.ts
+++ test/source/patterns.ts
@@ -78,9 +78,13 @@ for (const srcFilePath of getAllFilesInDir('./extension', /\.ts$/)) {
  * check for problems in manifest file (because dynamically generated)
  * https://github.com/FlowCrypt/flowcrypt-browser/issues/2934
  */
-const expectedPermissions = ["storage", "tabs", "https://*.google.com/*", "https://www.googleapis.com/*", "https://flowcrypt.com/*", "unlimitedStorage"];
+const expectedConsumerPermissions = ["storage", "tabs", "https://*.google.com/*", "https://www.googleapis.com/*", "https://flowcrypt.com/*", "unlimitedStorage"];
+const expectedEnterprisePermissions = ["storage", "tabs", "https://*.google.com/*", "https://*.googleapis.com/*", "https://flowcrypt.com/*", "unlimitedStorage"];
 for (const buildType of ['chrome-consumer', 'chrome-enterprise', 'firefox-consumer']) {
   const manifest = JSON.parse(readFileSync(`./build/${buildType}/manifest.json`).toString());
+  const expectedPermissions = buildType.includes('consumer')
+    ? expectedConsumerPermissions
+    : expectedEnterprisePermissions;
   for (const expectedPermission of expectedPermissions) {
     if (!manifest.permissions.includes(expectedPermission)) {
       if (!(expectedPermission === 'unlimitedStorage' && buildType === 'firefox-consumer')) {
