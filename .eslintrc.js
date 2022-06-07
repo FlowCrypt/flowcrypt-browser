10ecea11d19bf4c7b2fa48a3e35b3f503162c0d9
Ioan Moldovan <mobilestar108@outlook.com>
2022-06-07 08:05:08 -0400

    fix: use single case

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/semi': ['error'],
-    "@typescript-eslint/no-explicit-any": ["warn"],
+    "@typescript-eslint/no-explicit-any": ['warn'],
     'indent': ['error', 2, { SwitchCase: 1 }],
     'max-len': ['error', { code: 190 }],
     'no-constant-condition': 0,
