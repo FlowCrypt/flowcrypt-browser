42b817316ca8df684c2b63e72065f99df07b7fb8
Ioan Moldovan <77344191+ioanmo226@users.noreply.github.com>
2022-07-13 17:07:27 +0300

    issue #2663 Refactor backup & setup create key logic (#4534)
    * feat: refactor setup create key and backup module
    
    * fix: typescript issue
    
    * fix: test pattern issue
    
    * fix: ui test
    
    * fix: ui test
    
    * (WIP) refactor BackupUi
    
    * fix: readonly
    
    * fix: ui tests
    
    * fix: beautify
    
    * fix: only
    
    * fix: pattern
    
    * fix: pr reviews

---
+++ extension/js/common/ui/backup-ui/backup-ui-automatic-module.ts
@@ -0,0 +1,49 @@
+/* ©️ 2016 - present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com */
+
+'use strict';
+
+import { Lang } from '../../lang.js';
+import { Settings } from '../../settings.js';
+import { UnreportableError } from '../../platform/catch.js';
+import { Ui } from '../../browser/ui.js';
+import { ApiErr } from '../../api/shared/api-error.js';
+import { GoogleAuth } from '../../api/email-provider/gmail/google-auth.js';
+import { KeyStore } from '../../platform/store/key-store.js';
+import { KeyStoreUtil } from "../../core/crypto/key-store-util.js";
+import { BackupUi } from './backup-ui.js';
+import { BackupUiModule } from './backup-ui-module.js';
+
+export class BackupUiAutomaticModule extends BackupUiModule<BackupUi> {
+
+  public simpleSetupAutoBackupRetryUntilSuccessful = async () => {
+    try {
+      await this.setupCreateSimpleAutomaticInboxBackup();
+    } catch (e) {
+      return await Settings.promptToRetry(e, Lang.setup.failedToBackUpKey, this.setupCreateSimpleAutomaticInboxBackup,
+        Lang.general.contactIfNeedAssistance(!!this.ui.fesUrl));
+    }
+  };
+
+  private setupCreateSimpleAutomaticInboxBackup = async () => {
+    const prvs = await KeyStoreUtil.parse(await KeyStore.getRequired(this.ui.acctEmail));
+    if (prvs.find(prv => !prv.key.fullyEncrypted)) {
+      await Ui.modal.warning('Key not protected with a pass phrase, skipping');
+      throw new UnreportableError('Key not protected with a pass phrase, skipping');
+    }
+    try {
+      await this.ui.manualModule.doBackupOnEmailProvider(prvs.map(prv => prv.keyInfo));
+      await this.ui.onBackedUpFinished();
+    } catch (e) {
+      if (ApiErr.isAuthErr(e)) {
+        await Ui.modal.info("Authorization Error. FlowCrypt needs to reconnect your Gmail account");
+        const connectResult = await GoogleAuth.newAuthPopup({ acctEmail: this.ui.acctEmail });
+        if (!connectResult.error) {
+          await this.setupCreateSimpleAutomaticInboxBackup();
+        } else {
+          throw e;
+        }
+      }
+    }
+  };
+
+}
