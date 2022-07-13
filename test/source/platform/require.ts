12c70335e66701b8a0980e4fa2a972163212ce79
martgil <46025304+martgil@users.noreply.github.com>
2022-07-13 18:03:39 +0800

    revert require.ts

--- test/source/platform/require.ts
+++ test/source/platform/require.ts
@@ -8,18 +8,18 @@ export const requireOpenpgp = (): typeof OpenPGP => {
   return require('openpgp') as unknown as typeof OpenPGP;
 };
 
-export const requireMimeParser = (): unknown => {
+export const requireMimeParser = (): any => {
   // const MimeParser = (window as any)['emailjs-mime-parser'](); // tslint:disable-line:no-unsafe-any
   // return require('../../../../../extension/lib/emailjs/emailjs-mime-parser'); // todo
   return undefined; // the above does not work, would have to import directly from npm, but we have made custom edits to the lib so not feasible now
 };
 
-export const requireMimeBuilder = (): unknown => {
+export const requireMimeBuilder = (): any => {
   // const MimeBuilder = (window as any)['emailjs-mime-builder']; // tslint:disable-line:variable-name
   return undefined; // todo
 };
 
-export const requireIso88592 = (): unknown => {
+export const requireIso88592 = (): any => {
   // (window as any).iso88592
   return undefined; // todo
 };
