1ad000a72ce376a4546cc8a6daae20a787ecbe3c
martgil <mart.robles.it@gmail.com>
2022-08-01 16:04:37 +0800

    Fix oauth popup issue

--- extension/js/common/api/email-provider/gmail/google-auth.ts
+++ extension/js/common/api/email-provider/gmail/google-auth.ts
@@ -121,7 +121,11 @@ export class GoogleAuth {
     }
     const authRequest: AuthReq = { acctEmail, scopes, csrfToken: `csrf-${Api.randomFortyHexChars()}` };
     const url = GoogleAuth.apiGoogleAuthCodeUrl(authRequest);
-    const oauthWin = await windowsCreate({ url, left: 100, top: 50, height: 800, width: 550, type: 'popup' });
+    const adaptiveHeight = Math.floor((window.innerHeight || window.screen.height) * 0.9);
+    const adaptiveWidth = Math.floor((window.innerWidth || window.screen.width) * 0.4);
+    const leftOffset = Math.floor((window.screen.width / 2) - (adaptiveWidth / 2) + (window as any).screen.availLeft);
+    const topOffset = Math.floor((window.innerHeight / 2) - (adaptiveHeight / 2) + (window as any).screen.availTop);
+    const oauthWin = await windowsCreate({ url, left: leftOffset, top: topOffset, height: adaptiveHeight, width: adaptiveWidth, type: 'popup' });
     if (!oauthWin || !oauthWin.tabs || !oauthWin.tabs.length || !oauthWin.id) {
       return { result: 'Error', error: 'No oauth window returned after initiating it', acctEmail, id_token: undefined };
     }
