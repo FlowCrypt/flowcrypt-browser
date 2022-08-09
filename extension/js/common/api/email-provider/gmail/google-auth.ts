e8066e9ba530ca13beae90a916534d579252b655
Roman Shevchenko <rrrooommmaaa@mail.ru>
2022-08-09 09:10:30 +0000

    minor fixes

--- extension/js/common/api/email-provider/gmail/google-auth.ts
+++ extension/js/common/api/email-provider/gmail/google-auth.ts
@@ -123,12 +123,15 @@ export class GoogleAuth {
     const url = GoogleAuth.apiGoogleAuthCodeUrl(authRequest);
     const screenWidth = (window.screen.width || window.innerWidth);
     const screenHeight = (window.screen.height || window.innerHeight);
+    // non-standard but supported by most of the browsers
     const { availLeft, availTop } = (window.screen as unknown as { availLeft?: number, availTop?: number });
     let adaptiveWidth = Math.floor(screenWidth * 0.4);
+    if (adaptiveWidth < 550) {
+      adaptiveWidth = Math.min(550, Math.floor(screenWidth * 0.9));
+    }
     const adaptiveHeight = Math.floor(screenHeight * 0.9);
-    adaptiveWidth = adaptiveWidth < 550 ? adaptiveWidth + Math.floor(screenWidth * 0.5) : adaptiveWidth;
-    const leftOffset = Math.floor((screenWidth / 2) - (adaptiveWidth / 2) + availLeft!);
-    const topOffset = Math.floor((screenHeight / 2) - (adaptiveHeight / 2) + availTop!);
+    const leftOffset = Math.floor((screenWidth / 2) - (adaptiveWidth / 2) + (availLeft || 0));
+    const topOffset = Math.floor((screenHeight / 2) - (adaptiveHeight / 2) + (availTop || 0));
     const oauthWin = await windowsCreate({ url, left: leftOffset, top: topOffset, height: adaptiveHeight, width: adaptiveWidth, type: 'popup' });
     if (!oauthWin || !oauthWin.tabs || !oauthWin.tabs.length || !oauthWin.id) {
       return { result: 'Error', error: 'No oauth window returned after initiating it', acctEmail, id_token: undefined };
