40596fa2dab1f5dd5b7d0a83a40e0bafcd85640b
Tom J <tom@holub.me>
2022-08-23 16:41:13 +0000

    fix

--- extension/js/common/core/common.ts
+++ extension/js/common/core/common.ts
@@ -84,6 +84,7 @@ export class Str {
     if (email.indexOf(' ') !== -1) {
       return false;
     }
+    email = email.replace(/\:8001$/, ''); // for MOCK tests until https://github.com/FlowCrypt/flowcrypt-browser/issues/4631
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(email);
   };
 
