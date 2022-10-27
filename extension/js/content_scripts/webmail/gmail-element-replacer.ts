6dc900644c5d7ebd6599590be4b3c421c0da8d2f
Ioan Moldovan <mobilestar108@outlook.com>
2022-10-27 08:04:22 -0400

    fix: switch to encrypted reply

--- extension/js/content_scripts/webmail/gmail-element-replacer.ts
+++ extension/js/content_scripts/webmail/gmail-element-replacer.ts
@@ -633,9 +633,17 @@ export class GmailElementReplacer implements WebmailElementReplacer {
           }
           if (!midConvoDraft) { // either is a draft in the middle, or the convo already had (last) box replaced: should also be useless draft
             const isReplyButtonView = replyBoxEl.className.includes('nr');
-            const secureReplyBoxXssSafe = `<div class="remove_borders reply_message_iframe_container">${this.factory.embeddedReply(replyParams, this.shouldShowEditableSecureReply)}</div>`;
+            const replyBoxes = document.querySelectorAll('iframe.reply_message');
+            const alreadyHasSecureReplyBox = replyBoxes.length > 0;
+            console.log(`alreadyHasSecureReplyBox: ${alreadyHasSecureReplyBox}`);
+            console.log(`this.shouldShowEditableSecureReply: ${this.shouldShowEditableSecureReply}`);
+            const secureReplyBoxXssSafe = `
+              <div class="remove_borders reply_message_iframe_container">
+                ${this.factory.embeddedReply(replyParams, this.shouldShowEditableSecureReply || alreadyHasSecureReplyBox)}
+              </div>
+            `;
             this.shouldShowEditableSecureReply = !isReplyButtonView;
-            if (hasDraft) {
+            if (hasDraft || alreadyHasSecureReplyBox) {
               replyBox.addClass('reply_message_evaluated remove_borders').parent().append(secureReplyBoxXssSafe); // xss-safe-factory
               replyBox.hide();
             } else if (isReplyButtonView) {
