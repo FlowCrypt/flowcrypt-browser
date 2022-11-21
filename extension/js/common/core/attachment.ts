08d323d9d00314aea20879c9ff82fffd040ee76b
Ioan Moldovan <mobilestar108@outlook.com>
2022-11-21 10:32:52 -0400

    fix: ts issue

--- extension/js/common/core/attachment.ts
+++ extension/js/common/core/attachment.ts
@@ -118,7 +118,7 @@ export class Attachment {
       return 'hidden'; // mail.ch does this - although it looks like encrypted msg, it will just contain PGP version eg "Version: 1"
     } else if (Attachment.encryptedMsgNames.includes(this.name)) {
       return 'encryptedMsg';
-    } else if (this.name === 'message' && (this.type === 'application/pgp-encrypted' || this.)) {
+    } else if (this.name === 'message' && (this.type === 'application/pgp-encrypted')) {
       // treat message as encryptedMsg when type=application/pgp-encrypted or empty body for the 'message' attachment
       return 'encryptedMsg';
     } else if (this.name.match(/(\.pgp$)|(\.gpg$)|(\.[a-zA-Z0-9]{3,4}\.asc$)/g)) { // ends with one of .gpg, .pgp, .???.asc, .????.asc
