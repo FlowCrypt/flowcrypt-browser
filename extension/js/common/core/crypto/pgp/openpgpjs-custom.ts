936f94d4bcee907e7acea36f1081c01066ded77d
Roman Shevchenko <roman@flowcrypt.com>
2023-01-10 19:38:59 +0000

    restore allow missing MDC config option

--- extension/js/common/core/crypto/pgp/openpgpjs-custom.ts
+++ extension/js/common/core/crypto/pgp/openpgpjs-custom.ts
@@ -12,7 +12,7 @@ if (typeof opgp !== 'undefined') {
   // in certain environments, eg pgp_block.htm, openpgp is not included
   opgp.config.versionString = `FlowCrypt Email Encryption ${VERSION}`;
   opgp.config.commentString = 'Seamlessly send and receive encrypted email';
-  // todo: opgp.config.ignore_mdc_error = true; // we manually check for missing MDC and show loud warning to user (no auto-decrypt)
+  opgp.config.allowUnauthenticatedMessages = true; // we manually check for missing MDC and show loud warning to user (no auto-decrypt)
   opgp.config.allowInsecureDecryptionWithSigningKeys = false; // may get later over-written using ClientConfiguration for some clients
   // openpgp.config.require_uid_self_cert = false;
 }
