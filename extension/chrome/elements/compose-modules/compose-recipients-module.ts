8d682f40536030e24e2e83d8fdb4f5a8c3aef301
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2023-01-10 22:14:35 +0200

    fix test

--- extension/chrome/elements/compose-modules/compose-recipients-module.ts
+++ extension/chrome/elements/compose-modules/compose-recipients-module.ts
@@ -334,6 +334,9 @@ export class ComposeRecipientsModule extends ViewModule<ComposeView> {
     Xss.sanitizeRender(container, '<span class="rest"><span id="rest_number"></span> more</span>');
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const maxWidth = container.parent().width()! - this.view.S.cached('container_cc_bcc_buttons').width()!;
+    if (maxWidth <= 0) {
+      return;
+    }
     const rest = container.find('.rest');
     let processed = 0;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
