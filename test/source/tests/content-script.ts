4d884256130dbc5aee7de4aba91c97b1c44f954f
Roman Shevchenko <roman@flowcrypt.com>
2023-03-24 07:25:30 +0000

    updated comment

--- test/source/tests/content-script.ts
+++ test/source/tests/content-script.ts
@@ -4,6 +4,7 @@ import test from 'ava';
 
 import { TestWithBrowser } from '../test';
 import { expect } from 'chai';
+import { Value } from '../core/common';
 
 export const defineContentScriptTests = (testWithBrowser: TestWithBrowser) => {
   test(
@@ -15,8 +16,8 @@ export const defineContentScriptTests = (testWithBrowser: TestWithBrowser) => {
       const gmailPage = await browser.newMockGmailPage(t, authorizationHeader);
       await gmailPage.waitAny('@content-script-test-result');
       const allValues = (await gmailPage.readAll('@content-script-test-result')).map(el => el.innerText);
-      // multiple result div may appear on exceptions. The test is successful only if there is exactly one div containing the word 'pass'
-      expect(allValues).to.eql(['pass']);
+      // multiple results appear in Firefox. The test is successful only if all divs have the word 'pass'
+      expect(Value.arr.unique(allValues)).to.eql(['pass']);
       await gmailPage.close();
     })
   );
