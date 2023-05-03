8af508c21a2c7842a45e0630f4d5fa39f587abbb
Roman Shevchenko <roman@flowcrypt.com>
2023-05-03 19:38:26 +0100

    Added OpenPGP_signature webmailNamePattern

--- extension/js/common/core/attachment.ts
+++ extension/js/common/core/attachment.ts
@@ -36,7 +36,7 @@ export type TransferableAttachment = (AttachmentId | { data: /* base64 see #2587
 
 export class Attachment {
   public static readonly webmailNamePattern =
-    /^(((cryptup|flowcrypt)-backup-[a-z0-9]+\.(key|asc))|(.+\.pgp)|(.+\.gpg)|(.+\.asc)|(noname)|(message)|(PGPMIME version identification)|(ATT[0-9]{5})|())$/m;
+    /^(((cryptup|flowcrypt)-backup-[a-z0-9]+\.(key|asc))|(.+\.pgp)|(.+\.gpg)|(.+\.asc)|(OpenPGP_signature(.asc)?)|(noname)|(message)|(PGPMIME version identification)|(ATT[0-9]{5})|())$/m;
   public static readonly encryptedMsgNames = ['msg.asc', 'message.asc', 'encrypted.asc', 'encrypted.eml.pgp', 'Message.pgp', 'openpgp-encrypted-message.asc'];
 
   public length = NaN;
