363eaac31d29eced58e30fcb571418a5f106943f
Ioan Moldovan <mobilestar108@outlook.com>
2023-05-23 05:34:46 -0400

    fix: get id token issue in settings page (#5174)

--- extension/js/common/platform/store/in-memory-store.ts
+++ extension/js/common/platform/store/in-memory-store.ts
@@ -2,6 +2,7 @@
 
 import { AbstractStore } from './abstract-store.js';
 import { BrowserMsg } from '../../browser/browser-msg.js';
+import { Time } from '../../browser/time.js';
 
 /**
  * Temporary In-Memory store for sensitive values, expiring after clientConfiguration.in_memory_pass_phrase_session_length (or default 4 hours)
@@ -14,5 +15,16 @@ export class InMemoryStore extends AbstractStore {
 
   public static get = async (acctEmail: string, key: string): Promise<string | undefined> => {
     return (await BrowserMsg.send.bg.await.inMemoryStoreGet({ acctEmail, key })) ?? undefined;
+  };
+
+  public static getUntilAvailable = async (acctEmail: string, key: string, retryCount = 20): Promise<string> => {
+    for (let i = 0; i < retryCount; i++) {
+      const value = await InMemoryStore.get(acctEmail, key);
+      if (value) {
+        return value;
+      }
+      await Time.sleep(300);
+    }
+    throw new Error('Value is not available');
   };
 }
