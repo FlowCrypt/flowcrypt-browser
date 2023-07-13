370b458eab55b0babab6a6d7b1036a061f2a9569
Mart <mart.robles.it@gmail.com>
2023-07-13 16:17:47 +0800

    add sample query string in ci_unit_test.ts

--- test/source/tests/browser-unit-tests/unit-Catch.js
+++ test/source/tests/browser-unit-tests/unit-Catch.js
@@ -63,8 +63,10 @@ BROWSER_UNIT_TEST_NAME(`Catcher does report sensitive infos`);
 BROWSER_UNIT_TEST_NAME(`Catcher does not include query string on report`);
 (async () => {
   const formatted = Catch.formatExceptionForReport({'name': 'Error'});
+  const expectedUrl = 'chrome-extension://extension-id/chrome/dev/ci_unit_test.htm';
   if (formatted.url.indexOf('?') !== -1) {
-    throw new Error(`The reported URL where the error occurred should not include query strings. Expecting ${expectedUrl} but got ${formatted.url}.`)
+    const url = formatted.url.replace(/(\w{32})/,'extension-id');
+    throw new Error(`The reported URL where the error occurred should not include query strings. Expecting ${expectedUrl} but got ${url}.`);
   }
   return 'pass';
 })();
