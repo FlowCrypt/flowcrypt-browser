23ae1ae66887aeeab65d8fbe8da1ae6275d8ea25
Tom J <6306961+tomholub@users.noreply.github.com>
2023-07-14 09:59:10 +0200

    Update catch.ts

--- extension/js/common/platform/catch.ts
+++ extension/js/common/platform/catch.ts
@@ -43,7 +43,7 @@ export class Catch {
     // benign error https://github.com/WICG/ResizeObserver/issues/38#issuecomment-422126006 https://stackoverflow.com/questions/49384120/resizeobserver-loop-limit-exceeded
     'ResizeObserver loop limit exceeded',
     // https://github.com/FlowCrypt/flowcrypt-browser/issues/5280
-    'Could not find valid self-signature',
+    '400 when POST-ing https://flowcrypt.com/attester/welcome-message string: email,pubkey -> This key does not appear valid',
   ];
 
   public static rewrapErr = (e: unknown, message: string): Error => {
