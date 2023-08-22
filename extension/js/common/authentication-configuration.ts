20c81ef9d773c5c9b434621865e65d09aadf3a6b
martgil <mart.robles.it@gmail.com>
2023-08-22 10:40:15 +0800

    update AuthenticationConfiguration type

--- extension/js/common/authentication-configuration.ts
+++ extension/js/common/authentication-configuration.ts
@@ -1,5 +1,5 @@
 /* ©️ 2016 - present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com */
 
 export type AuthenticationConfiguration = {
-  oauth: { clientId: string; clientSecret: string | undefined; redirectUrl: string; authCodeUrl: string; tokensUrl: string };
+  oauth: { clientId: string; clientSecret: string; redirectUrl: string; authCodeUrl: string; tokensUrl: string };
 };
