5d9df45b430faaa15ab3bf6f8af72af251ea9204
Mart G <46025304+martgil@users.noreply.github.com>
2023-10-03 18:21:52 +0800

    #5318 Add ConfiguredIdPOAuth class (#5421)
    * add ConfiguredIdPOAuth class
    
    * optional checkForConfiguredIdPOAuth
    
    * correct condition checking
    
    * wip: update ConfiguredIdPOAuth class
    
    * re-arrange build script pathing
    
    * update directory name

--- test/source/tests/tooling/browser-recipe.ts
+++ test/source/tests/tooling/browser-recipe.ts
@@ -42,10 +42,16 @@ export class BrowserRecipe {
     return settingsPage;
   };
 
-  public static openSettingsLoginApprove = async (t: AvaContext, browser: BrowserHandle, acctEmail: string) => {
+  public static openSettingsLoginApprove = async (t: AvaContext, browser: BrowserHandle, acctEmail: string, checkForConfiguredIdPOAuth?: boolean) => {
     const settingsPage = await browser.newExtensionSettingsPage(t, acctEmail);
     const oauthPopup = await browser.newPageTriggeredBy(t, () => settingsPage.waitAndClick('@action-connect-to-gmail'));
     await OauthPageRecipe.google(t, oauthPopup, acctEmail, 'approve');
+    if (checkForConfiguredIdPOAuth)
+      await settingsPage.waitAndRespondToModal(
+        'warning',
+        'confirm',
+        'Custom IdP is configured on this domain, but it is not supported on browser extension yet.'
+      );
     return settingsPage;
   };
 
