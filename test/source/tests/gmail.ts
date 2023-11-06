92e567da5e4f8a9ccb7c8ec95a8e23bdb52a8a06
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2023-11-06 15:05:59 +0200

    live test fix

--- test/source/tests/gmail.ts
+++ test/source/tests/gmail.ts
@@ -317,8 +317,7 @@ export const defineGmailTests = (testVariant: TestVariant, testWithBrowser: Test
           await gmailPage.reload({ timeout: TIMEOUT_PAGE_LOAD * 1000, waitUntil: 'load' }, true);
           replyBox = await pageHasSecureDraft(gmailPage, 'offline reply draft');
           await Util.sleep(2);
-          await replyBox.waitAndClick('@action-send');
-          await replyBox.waitTillGone('@action-send');
+          await replyBox.waitAndClick('@action-send', { confirmGone: true });
           await Util.sleep(2);
           await gmailPage.reload({ timeout: TIMEOUT_PAGE_LOAD * 1000, waitUntil: 'load' }, true);
           await gmailPage.waitAndClick('.h7:last-child .ajz', { delay: 1 }); // the small triangle which toggles the message details
