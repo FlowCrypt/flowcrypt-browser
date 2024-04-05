41b858653732f7e26981e3449f7987bd44bca419
Ioan Moldovan <mobilestar108@outlook.com>
2024-04-04 21:29:23 -0400

    fix: remove rejected promise from cache test

--- test/source/tests/browser-unit-tests/unit-ExpirationCache.js
+++ test/source/tests/browser-unit-tests/unit-ExpirationCache.js
@@ -15,7 +15,31 @@ BROWSER_UNIT_TEST_NAME(`[unit][ExpirationCache] entry expires after configured i
   await sleep(2);
   cacheValue = await cache.get('test-key');
   if (cacheValue) {
-    throw Error(`Expected cache value to be undefined to "test-value" but got ${cacheValue}`);
+    throw Error(`Expected cache value to be undefined but got ${cacheValue}`);
+  }
+  return 'pass';
+})();
+
+BROWSER_UNIT_TEST_NAME(`[unit][ExpirationCache.await] removes rejected promises from cache`);
+(async () => {
+  const cache = new ExpirationCache(24 * 60 * 60 * 1000); // 24 hours
+  const rejectionPromise = Promise.reject(Error('test-error'));
+  cache.set('test-key', rejectionPromise);
+  let cacheValue = cache.get('test-key');
+  if (cacheValue?.length) {
+    throw Error(`Expected cache value to be undefined but got ${JSON.stringify(cacheValue)}`);
+  } // next call simply returns undefined
+  const fulfilledPromise = Promise.resolve('new-test-value');
+  cache.set('test-key', fulfilledPromise);
+  cacheValue = await cache.await('test-key', fulfilledPromise);
+  // good value is returned indefinitely
+  if (cacheValue !== 'new-test-value') {
+    throw Error(`Expected cache value to be "new-test-value" but got ${cacheValue}`);
+  }
+  cacheValue = await cache.await('test-key', fulfilledPromise);
+  // good value is returned indefinitely
+  if (cacheValue !== 'new-test-value') {
+    throw Error(`Expected cache value to be "new-test-value" but got ${cacheValue}`);
   }
   return 'pass';
 })();
