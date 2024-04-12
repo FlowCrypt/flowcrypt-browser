8645ee14eb1899ffe91a7a3fe0ac5db4eeed75c9
martgil <mart.robles.it@gmail.com>
2024-04-12 17:40:50 +0800

    update toast message

--- test/source/tests/compose.ts
+++ test/source/tests/compose.ts
@@ -363,8 +363,8 @@ export const defineComposeTests = (testVariant: TestVariant, testWithBrowser: Te
         await composePage.waitAndFocus('@input-body');
         await composePage.waitTillGone('@spinner');
         await Util.sleep(3); // allow some time to search for messages
-        await composePage.waitForContent(
-          '@container-ui-toast-text',
+        await ComposePageRecipe.waitForToastToAppearAndDisappear(
+          composePage,
           "We couldn't find your public key on the server. We've included it for you so that they can use it to reply back to you encrypted."
         );
         expect(await composePage.hasClass('@action-include-pubkey', 'active')).to.be.true;
@@ -3300,7 +3300,10 @@ export const defineComposeTests = (testVariant: TestVariant, testWithBrowser: Te
         const composePage = await ComposePageRecipe.openStandalone(t, browser, acct);
         await Promise.all([
           ComposePageRecipe.fillMsg(composePage, { to: 'mock.only.pubkey@flowcrypt.com' }, 'no valid key'),
-          ComposePageRecipe.waitForToastToAppearAndDisappear(composePage, 'Draft not saved: Error: Your account keys are revoked'),
+          ComposePageRecipe.waitForToastToAppearAndDisappear(
+            composePage,
+            "We couldn't find your public key on the server. We've included it for you so that they can use it to reply back to you encrypted."
+          ),
         ]);
         await composePage.waitAndClick('@action-send', { delay: 1 });
         await PageRecipe.waitForModalAndRespond(composePage, 'warning', {
