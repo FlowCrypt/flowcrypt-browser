318e3d01174244280aff72f6b477e5594250d503
Mart <46025304+martgil@users.noreply.github.com>
2024-06-28 21:26:49 +0800

    #5780 Remove service_worker for firefox's manifest.json build process (#5781)
    * Remove service_worker for firefox manifest.json build process
    
    * Pr review: cleanup

--- tooling/build-types-and-manifests.ts
+++ tooling/build-types-and-manifests.ts
@@ -45,7 +45,7 @@ addManifest('firefox-consumer', manifest => {
     },
   };
   manifest.background = {
-    ...manifest.background,
+    type: 'module',
     scripts: ['/js/service_worker/background.js'],
   };
   // eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars
