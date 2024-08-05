0e61428b9b6d30340a2876985440d32dcb916886
Ioan Moldovan <mobilestar108@outlook.com>
2024-08-05 09:29:13 +0300

    fix: simplify

--- test/source/mock/fes/customer-url-fes-endpoints.ts
+++ test/source/mock/fes/customer-url-fes-endpoints.ts
@@ -17,6 +17,7 @@ export const issuedCustomIDPIdTokens: string[] = [];
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const standardSubDomainFesClientConfiguration = { flags: [], disallow_attester_search_for_domains: ['got.this@fromstandardfes.com'] };
 export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): HandlersDefinition => {
+  const isCustomIDPUsed = !!config?.authenticationConfiguration;
   return {
     // standard fes location at https://fes.domain.com
     '/api/': async ({}, req) => {
@@ -58,7 +59,7 @@ export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): H
     },
     '/api/v1/message/new-reply-token': async ({}, req) => {
       if (parseAuthority(req) === standardFesUrl(parsePort(req)) && req.method === 'POST') {
-        authenticate(req, !!config?.authenticationConfiguration);
+        authenticate(req, isCustomIDPUsed);
         return { replyToken: 'mock-fes-reply-token' };
       }
       throw new HttpClientErr('Not Found', 404);
@@ -68,7 +69,7 @@ export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): H
       const fesUrl = standardFesUrl(port);
       // body is a mime-multipart string, we're doing a few smoke checks here without parsing it
       if (parseAuthority(req) === fesUrl && req.method === 'POST' && typeof body === 'string') {
-        authenticate(req, !!config?.authenticationConfiguration);
+        authenticate(req, isCustomIDPUsed);
         if (config?.messagePostValidator) {
           return await config.messagePostValidator(body, fesUrl);
         }
@@ -80,7 +81,7 @@ export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): H
       const port = parsePort(req);
       if (parseAuthority(req) === standardFesUrl(port) && req.method === 'POST') {
         // test: `compose - user@standardsubdomainfes.localhost:8001 - PWD encrypted message with FES web portal`
-        authenticate(req, !!config?.authenticationConfiguration);
+        authenticate(req, isCustomIDPUsed);
         expect(body).to.match(messageIdRegex(port));
         return {};
       }
@@ -90,7 +91,7 @@ export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): H
       const port = parsePort(req);
       if (parseAuthority(req) === standardFesUrl(port) && req.method === 'POST') {
         // test: `compose - user2@standardsubdomainfes.localhost:8001 - PWD encrypted message with FES - Reply rendering`
-        authenticate(req, !!config?.authenticationConfiguration);
+        authenticate(req, isCustomIDPUsed);
         expect(body).to.match(messageIdRegex(port));
         return {};
       }
@@ -103,7 +104,7 @@ export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): H
         // test: `compose - user2@standardsubdomainfes.localhost:8001 - PWD encrypted message with FES - Reply rendering`
         // test: `compose - user3@standardsubdomainfes.localhost:8001 - PWD encrypted message with FES web portal - pubkey recipient in bcc`
         // test: `compose - user4@standardsubdomainfes.localhost:8001 - PWD encrypted message with FES web portal - some sends fail with BadRequest error`
-        authenticate(req, !!config?.authenticationConfiguration);
+        authenticate(req, isCustomIDPUsed);
         expect(body).to.match(messageIdRegex(port));
         return {};
       }
@@ -113,7 +114,7 @@ export const getMockCustomerUrlFesEndpoints = (config: FesConfig | undefined): H
       const port = parsePort(req);
       if (parseAuthority(req) === standardFesUrl(port) && req.method === 'POST') {
         // test: `compose - user@standardsubdomainfes.localhost:8001 - PWD encrypted message with FES web portal`
-        authenticate(req, !!config?.authenticationConfiguration);
+        authenticate(req, isCustomIDPUsed);
         expect(body).to.match(messageIdRegex(port));
         return {};
       }
