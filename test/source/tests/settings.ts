7e4a2b2f4b128d2ed228b305055c6a23c2328a52
Ioan Moldovan <mobilestar108@outlook.com>
2024-09-13 03:36:43 +0300

    fix: ui test

--- test/source/tests/settings.ts
+++ test/source/tests/settings.ts
@@ -552,6 +552,7 @@ export const defineSettingsTests = (testVariant: TestVariant, testWithBrowser: T
         // Uncheck backup_inbox to check if backup view correctly displayed
         await addKeyPopup.waitAndClick('@input-step2bmanualcreate-backup-inbox');
         await addKeyPopup.waitAndClick('@input-step2bmanualcreate-create-and-save');
+        await addKeyPopup.waitAndClick('@input-backup-step3manual-no-backup'); // choose no_backup so that it doesn't affect other tests.
         await addKeyPopup.waitAndClick('@action-backup-step3manual-continue');
         await SettingsPageRecipe.ready(settingsPage);
         await settingsPage.waitAndClick('@action-remove-key-2'); // Delete newly generated key
