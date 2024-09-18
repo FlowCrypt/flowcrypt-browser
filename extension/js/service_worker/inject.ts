7375f7bc1df0314d8cd47ed1e7e06de54b43e418
Ioan Moldovan <mobilestar108@outlook.com>
2024-09-18 12:44:39 +0300

    fix: remove chrome.tabs.onUpdated listener in firefox

--- extension/js/service_worker/inject.ts
+++ extension/js/service_worker/inject.ts
@@ -2,8 +2,6 @@
 
 'use strict';
 
-import { Catch } from '../common/platform/catch.js';
-
 export const injectFcIntoWebmail = () => {
   const contentScriptGroups = chrome.runtime.getManifest().content_scripts ?? []; // we know it's in the manifest
   // one time when extension installed or on browser start - go through all matching tabs and inject
@@ -11,21 +9,6 @@ export const injectFcIntoWebmail = () => {
     getContentScriptTabIds(group.matches || [], tabIds => {
       for (const tabId of tabIds) {
         injectContentScriptIntoTabIfNeeded(tabId, group.js || []);
-      }
-    });
-  }
-  // on Firefox, standard way of loading content scripts stopped working. We have to listen to tab loaded events, and inject then
-  // basically here we do what normally the browser is supposed to do (inject content scripts when page is done loading)
-  if (Catch.isFirefox()) {
-    chrome.tabs.onUpdated.addListener((tabId, changed, tab) => {
-      if (changed.status === 'complete' && tab.active && tab.url) {
-        for (const group of contentScriptGroups) {
-          for (const groupMatchUrl of group.matches || []) {
-            if (tab.url.startsWith(groupMatchUrl.replace(/\*$/, ''))) {
-              injectContentScriptIntoTabIfNeeded(tabId, group.js || []);
-            }
-          }
-        }
       }
     });
   }
