cd3acf638b0cfecf5ffca81d43e564b6164fa098
Ioan Moldovan <mobilestar108@outlook.com>
2025-01-06 16:05:07 +0200

    fix: pr reviews

--- scripts/config-mock-build.sh
+++ scripts/config-mock-build.sh
@@ -3,6 +3,4 @@ BUILD_PATH=./build/test/mock-builds/port-$2-$(openssl rand -hex 12)
 mkdir -p $BUILD_PATH
 cp -r ./$1/* $BUILD_PATH
 grep "\[TEST_REPLACEABLE_MOCK_PORT\]" $BUILD_PATH -lr | xargs sed -i.bak -e "s/\[TEST_REPLACEABLE_MOCK_PORT\]/$2/g"
-# Replace export const MOCK_PORT line with the provided mock port
-grep "export const MOCK_PORT = '\[TEST_REPLACEABLE_MOCK_PORT\]';" $BUILD_PATH -lr | xargs sed -i.bak -e "s/export const MOCK_PORT = '\[TEST_REPLACEABLE_MOCK_PORT\]';/export const MOCK_PORT = '$2';/"
 echo $BUILD_PATH
