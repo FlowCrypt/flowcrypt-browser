07628f43b18e2f391705867c52ce0115ec99e841
Mart <46025304+martgil@users.noreply.github.com>
2025-01-17 20:53:19 +0800

    #5896 Replace accented characters when sending password messages (#5897)
    * add reference comment
    
    * revert changes
    
    * add working hotfix
    
    * pr reviews: apply fix with appropriate approach

--- extension/js/common/core/common.ts
+++ extension/js/common/core/common.ts
@@ -121,6 +121,13 @@ export class Str {
     return str.replace(/[.~!$%^*=?]/gi, '');
   };
 
+  public static replaceAccentedChars = (str: string) => {
+    /*
+     * hotfix for issue https://github.com/FlowCrypt/enterprise-server/issues/6264
+     */
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
+
   public static formatEmailWithOptionalName = (emailParts: EmailParts): string => {
     return Str.formatEmailWithOptionalNameEx(emailParts);
   };
@@ -281,6 +288,7 @@ export class Str {
   public static stripPgpOrGpgExtensionIfPresent = (filename: string) => {
     return filename.replace(/\.(pgp|gpg)$/i, '');
   };
+
   private static formatEmailWithOptionalNameEx = ({ email, name }: EmailParts, forceBrackets?: boolean): string => {
     if (name) {
       return `${Str.rmSpecialCharsKeepUtf(name, 'ALLOW-SOME')} <${email}>`;
