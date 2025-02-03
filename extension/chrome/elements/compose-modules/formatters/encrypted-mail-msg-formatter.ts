cbc16870e7aa3765f79ff10c7c1c99e6d62849d3
Ioan Moldovan <mobilestar108@outlook.com>
2025-02-03 12:20:24 +0200

    #5913 Add encrypted attachment to password protected message (#5919)
    * feat: add encrypted attachment to password protected message
    
    * feat: added ui test
    
    * fix: ui test
    
    * feat: add attachment in encrypt.asc
    
    * fix: send message strategy
    
    * Revert "fix: send message strategy"
    
    This reverts commit ff49e47ca0882ee0836d143973297c555f3963ee.

--- extension/chrome/elements/compose-modules/formatters/encrypted-mail-msg-formatter.ts
+++ extension/chrome/elements/compose-modules/formatters/encrypted-mail-msg-formatter.ts
@@ -181,10 +181,11 @@ export class EncryptedMsgMailFormatter extends BaseMailFormatter {
     { msgUrl, externalId }: { msgUrl: string; externalId?: string },
     signingPrv?: Key
   ) => {
-    // encoded as: PGP/MIME-like structure but with attachments as external files due to email size limit (encrypted for pubkeys only)
+    // encoded as: PGP/MIME-like structure
     const msgBody = this.richtext ? { 'text/plain': newMsg.plaintext, 'text/html': newMsg.plainhtml } : { 'text/plain': newMsg.plaintext };
+    const attachments = await this.view.attachmentsModule.attachment.collectEncryptAttachments(pubs);
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    const pgpMimeNoAttachments = await Mime.encode(msgBody, { Subject: newMsg.subject }, []); // no attachments, attached to email separately
+    const pgpMimeNoAttachments = await Mime.encode(msgBody, { Subject: newMsg.subject }, attachments);
     const { data: pubEncryptedNoAttachments } = await this.encryptDataArmor(Buf.fromUtfStr(pgpMimeNoAttachments), undefined, pubs, signingPrv); // encrypted only for pubs
     const emailIntroAndLinkBody = await this.formatPwdEncryptedMsgBodyLink(msgUrl);
     return await SendableMsg.createPwdMsg(
