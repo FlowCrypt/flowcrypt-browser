4aa53a18ba506561a09df4a7647d39ac2e1c9e14
Ioan Moldovan <mobilestar108@outlook.com>
2025-05-12 05:54:10 -0700

    #5610: Migrate build script to Node.js for cross-platform compatibility (#5974)
    * feat: migrate build script to node.js
    
    * fix: eslint and github advanced security
    
    * fix: remove core
    
    * fix: test_ci_consumer
    
    * feat: added cross env
    
    * fix: pr reviews
    
    * fix: conflict
    
    * feat: add build to eslint
    
    * fix: patterns

--- scripts/config-mock-build.sh
+++
@@ -1,6 +0,0 @@
-# copy build to new folder and replace all usages of :8001 with appropriate mock port
-BUILD_PATH=./build/test/mock-builds/port-$2-$(openssl rand -hex 12)
-mkdir -p $BUILD_PATH
-cp -r ./$1/* $BUILD_PATH
-grep "\[TEST_REPLACEABLE_MOCK_PORT\]" $BUILD_PATH -lr | xargs sed -i.bak -e "s/\[TEST_REPLACEABLE_MOCK_PORT\]/$2/g"
-echo $BUILD_PATH
