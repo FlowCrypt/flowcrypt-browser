e4f7c9cad8e0c333f6f14c46a4901bcaa477de11
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2025-07-07 12:21:06 +0300

    fix

--- extension/chrome/elements/pgp_block_modules/pgp-block-attachments-module.ts
+++ extension/chrome/elements/pgp_block_modules/pgp-block-attachments-module.ts
@@ -27,7 +27,6 @@ export class PgpBlockViewAttachmentsModule {
     for (const i of attachments.keys()) {
       const name = attachments[i].name ? Str.stripPgpOrGpgExtensionIfPresent(attachments[i].name) : 'noname';
       const nameVisible = name.length > 100 ? name.slice(0, 100) + '…' : name;
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
       const size = filesize(attachments[i].length);
 
       const htmlContent = `<b>${Xss.escape(nameVisible)}</b>&nbsp;&nbsp;&nbsp;${size}<span class="progress"><span class="percent"></span></span>`;
