06ec303f12bb8af3f6a5bab07f3cbed25f53a544
martgil <martroblesit@gmail.com>
2026-05-13 13:15:14 +0800

    fix: refine content script matches for chrome-enterprise manifest

--- tooling/build-types-and-manifests.ts
+++ tooling/build-types-and-manifests.ts
@@ -102,7 +102,7 @@ addManifest('chrome-enterprise', manifest => {
     'https://flowcrypt.com/*',
   ];
   for (const csDef of manifest.content_scripts ?? []) {
-    csDef.matches = csDef.matches?.filter(host => host === 'https://mail.google.com/*' || host === 'https://www.google.com/robots.txt*');
+    csDef.matches = csDef.matches?.filter(host => host === 'https://mail.google.com/*');
   }
   manifest.content_scripts = (manifest.content_scripts ?? []).filter(csDef => csDef.matches?.length); // remove empty defs
   if (!manifest.content_scripts.length) {
