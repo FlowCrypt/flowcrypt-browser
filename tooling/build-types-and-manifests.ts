b312de6653b50a3b321ec4705110c73d0fbb8281
martgil <martroblesit@gmail.com>
2026-05-22 17:47:58 +0800

    test: fix failing test

--- tooling/build-types-and-manifests.ts
+++ tooling/build-types-and-manifests.ts
@@ -178,7 +178,7 @@ const makeMockBuild = (sourceBuildType: string) => {
     const manifest = JSON.parse(updatedCode) as chrome.runtime.ManifestV3;
     if (manifest.content_security_policy?.extension_pages) {
       const csp = manifest.content_security_policy.extension_pages;
-      manifest.content_security_policy.extension_pages = csp.replace(/connect-src[^;]*/, "connect-src 'self' https://localhost:*");
+      manifest.content_security_policy.extension_pages = csp.replace(/connect-src[^;]*/, "connect-src 'self' https://localhost:* https://flowcrypt.com");
       updatedCode = JSON.stringify(manifest, undefined, 2);
     }
     return updatedCode;
