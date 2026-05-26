c1789d1271b179e28d4d34ebe9bb695c4537906e
martgil <martroblesit@gmail.com>
2026-05-26 17:04:48 +0800

    test: add new host to connect-src

--- tooling/build-types-and-manifests.ts
+++ tooling/build-types-and-manifests.ts
@@ -181,7 +181,7 @@ const makeMockBuild = (sourceBuildType: string) => {
       if (csp) {
         let updatedCsp = csp.replace(
           /connect-src[^;]*/,
-          "connect-src 'self' https://localhost:* https://flowcrypt.com https://fes.flowcrypt.test https://fes.standardsubdomainfes.localhost:* https://fes.key-manager-server-offline.flowcrypt.test https://google.com"
+          "connect-src 'self' https://localhost:* https://flowcrypt.com https://fes.flowcrypt.test https://fes.standardsubdomainfes.localhost:* https://fes.key-manager-server-offline.flowcrypt.test https://google.com https://flowcrypt.s3.amazonaws.com"
         );
         updatedCsp += "; style-src 'self' 'unsafe-inline'";
         manifest.content_security_policy.extension_pages = updatedCsp;
