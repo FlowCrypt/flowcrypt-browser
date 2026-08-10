29b8523f670b64f06e757a8438f437b0e439e4f2
martgil <46025304+martgil@users.noreply.github.com>
2026-08-10 13:25:18 +0800

    feat: add srcset to forbidden HTML attribute

--- extension/js/common/platform/xss.ts
+++ extension/js/common/platform/xss.ts
@@ -47,7 +47,7 @@ export class Xss {
     'col',
   ];
   private static ADD_ATTR = ['email', 'page', 'addurltext', 'longid', 'index', 'target', 'fingerprint', 'cryptup-data'];
-  private static FORBID_ATTR = ['background'];
+  private static FORBID_ATTR = ['background', 'srcset'];
   private static HREF_REGEX_CACHE: RegExp | undefined;
   private static EMOJI_REGEX = /(?![*#0-9]+)[\p{Emoji}\p{Emoji_Modifier}\p{Emoji_Component}\p{Emoji_Modifier_Base}\p{Emoji_Presentation}]/gu;
   /* eslint-disable @typescript-eslint/naming-convention */
